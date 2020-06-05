2a09394c0fc8f6afe0670c02046a1a8deac6a1d1
nicholas <nicholas.spong@adyen.com>
2020-06-05 14:11:43 +0200

    BinLookupFny - comments added

--- src/components/Card/components/CardInput/utils.ts
+++ src/components/Card/components/CardInput/utils.ts
@@ -21,7 +21,12 @@ export const getCardImageUrl = (brand: string, loadingContext: string) => {
  */
 const capitaliseFirstLetter = (string: string): string => string.charAt(0).toUpperCase() + string.slice(1);
 
-export const createCardVariantSwitcher = (types: string[], switcherType: 'brandSwitcher' | 'cardTypeSwitcher') => {
+/**
+ * Creates an object used for setting state - that will trigger the rendering of a select element to allow a choice between 2 different card variants
+ * @param types - array containing 2 card brands or types
+ * @param switcherType - type of switcher ('brandSwitcher' or 'cardTypeSwitcher' - the latter would switch between 'debit' & 'credit' varieties)
+ */
+export const createCardVariantSwitcher = (types: string[], switcherType: string) => {
     const leadType = types[0];
     let displayName = cardType.getDisplayName(leadType); // Works for when types are card brands e.g. 'visa', 'mc' NOT when types are 'credit'/'debit'
     const leadDisplayName = displayName || capitaliseFirstLetter(leadType);
@@ -32,7 +37,10 @@ export const createCardVariantSwitcher = (types: string[], switcherType: 'brandS
 
     return {
         stateObject: {
-            additionalSelectElements: [{ id: leadType, name: leadDisplayName }, { id: subType, name: subDisplayName }],
+            additionalSelectElements: [
+                { id: leadType, name: leadDisplayName },
+                { id: subType, name: subDisplayName }
+            ],
             additionalSelectValue: leadType,
             additionalSelectType: switcherType
         },
