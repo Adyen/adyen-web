8a31a34ddbc3a075d02c56eb07636b48d38a6d2f
nicholas <nicholas.spong@adyen.com>
2020-06-08 11:04:37 +0200

    Use css to capitalize items in the Select element that results from BinLookup call

--- src/components/Card/components/CardInput/utils.ts
+++ src/components/Card/components/CardInput/utils.ts
@@ -12,16 +12,6 @@ export const getCardImageUrl = (brand: string, loadingContext: string) => {
 };
 
 /**
- * Takes a string and returns the same string with the first letter capitalised.
- * @param {string} string
- * @return {string}
- * @example
- *
- * capitaliseFirstLetter('test'); // => 'Test'
- */
-const capitaliseFirstLetter = (string: string): string => string.charAt(0).toUpperCase() + string.slice(1);
-
-/**
  * Creates an object used for setting state - that will trigger the rendering of a select element to allow a choice between 2 different card variants
  * @param types - array containing 2 card brands or types
  * @param switcherType - type of switcher ('brandSwitcher' or 'cardTypeSwitcher' - the latter would switch between 'debit' & 'credit' varieties)
@@ -29,11 +19,11 @@ const capitaliseFirstLetter = (string: string): string => string.charAt(0).toUpp
 export const createCardVariantSwitcher = (types: string[], switcherType: string) => {
     const leadType = types[0];
     let displayName = cardType.getDisplayName(leadType); // Works for when types are card brands e.g. 'visa', 'mc' NOT when types are 'credit'/'debit'
-    const leadDisplayName = displayName || capitaliseFirstLetter(leadType);
+    const leadDisplayName = displayName || leadType;
 
     const subType = types[1];
     displayName = cardType.getDisplayName(subType);
-    const subDisplayName = displayName || capitaliseFirstLetter(subType);
+    const subDisplayName = displayName || subType;
 
     return {
         stateObject: {
