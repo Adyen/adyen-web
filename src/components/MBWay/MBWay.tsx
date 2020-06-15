918c55d1683635990550851b24e5429583cd71f1
nicholas <nicholas.spong@adyen.com>
2020-06-15 15:30:34 +0200

    MBWay shopper data - expect it to be inputted the same way it is outputted

--- src/components/MBWay/MBWay.tsx
+++ src/components/MBWay/MBWay.tsx
@@ -9,6 +9,10 @@ export class MBWayElement extends UIElement {
     private static type = 'mbway';
 
     formatProps(props: UIElementProps): UIElementProps {
+        if (props.data) {
+            props.data.email = props.data.shopperEmail || '';
+            props.data.phoneNumber = props.data.telephoneNumber || '';
+        }
         return {
             ...props
         };
