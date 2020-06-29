accbb873cd09ad5744cc5c7a6c6fdf7c01113923
pablo <pablo.igarzabal@adyen.com>
2020-06-29 13:45:37 +0200

    Add interfaces for the AmazonPay button.

--- src/components/AmazonPay/types.ts
+++ src/components/AmazonPay/types.ts
@@ -15,3 +15,46 @@ export interface AmazonElementProps {
     sessionUrl?: string;
     showPayButton: boolean;
 }
+
+interface CreateCheckoutSessionConfig {
+    payloadJSON: string;
+    signature: string;
+    publicKeyId: string;
+}
+
+export interface AmazonPayButtonSettings {
+    /**
+     * Amazon Pay merchant account identifier
+     */
+    merchantId: string;
+
+    /**
+     * Sets button to Sandbox environment
+     */
+    sandbox: boolean;
+
+    /**
+     * Product type selected for checkout
+     */
+    productType: 'PayAndShip' | 'PayOnly';
+
+    /**
+     * Placement of the Amazon Pay button on your website
+     */
+    placement: 'Home' | 'Product' | 'Cart' | 'Checkout' | 'Other';
+
+    /**
+     * Language used to render the button and text on Amazon Pay hosted pages. Please note that supported language(s) is dependent on the region that your Amazon Pay account was registered for
+     */
+    checkoutLanguage: 'en_US' | 'en_GB' | 'de_DE' | 'fr_FR' | 'it_IT' | 'es_ES';
+
+    /**
+     * Ledger currency provided during registration for the given merchant identifier
+     */
+    ledgerCurrency: 'USD' | 'EUR' | 'GBP';
+
+    /**
+     * Create Checkout Session configuration
+     */
+    createCheckoutSessionConfig: CreateCheckoutSessionConfig;
+}
