d517a3ced1ce8c31a53c03df4ca5f7cabcca5648
Marc Pérez <marc.pereziribas@adyen.com>
2020-07-01 09:36:03 +0200

    Remove unused file-loader (#61)

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -175,21 +175,6 @@ module.exports = merge(webpackConfig, {
                                 loader: 'sass-loader'
                             }
                         ]
-                    },
-                    // "file" loader makes sure assets end up in the `build` folder.
-                    // When you `import` an asset, you get its filename.
-                    // This loader doesn't use a "test" so it will catch all modules
-                    // that fall through the other loaders.
-                    {
-                        loader: 'file-loader',
-                        // Exclude `js` files to keep "css" loader working as it injects
-                        // it's runtime that would otherwise be processed through "file" loader.
-                        // Also exclude `html` and `json` extensions so they get processed
-                        // by webpacks internal loaders.
-                        exclude: [/\.(js|jsx|ts|tsx|mjs)$/, /\.html$/, /\.json$/],
-                        options: {
-                            name: 'static/media/[name].[hash:8].[ext]'
-                        }
                     }
                 ]
             }
