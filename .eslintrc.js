c45612f1e0aa095042b31ab1b52e0411038b9a63
marcp <marc.pereziribas@adyen.com>
2020-07-02 11:21:47 +0200

    Add more a11y linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -100,7 +100,13 @@ module.exports = {
         'tsdoc/syntax': 'warn',
 
         // a11y
-        'jsx-a11y/alt-text': 'error'
+        'jsx-a11y/alt-text': 'error',
+        'jsx-a11y/aria-role': 'error',
+        'jsx-a11y/aria-props': 'error',
+        'jsx-a11y/aria-unsupported-elements': 'error',
+        'jsx-a11y/role-has-required-aria-props': 'error',
+        'jsx-a11y/role-supports-aria-props': 'error',
+        'jsx-a11y/tabindex-no-positive': 'error'
     },
     overrides: [
         {
