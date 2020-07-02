611c4e603ee6c3552bd4faf823397078b0d39458
pablo <pablo.igarzabal@adyen.com>
2020-07-02 10:24:33 +0200

    Add constants in AmazonPay config file.

--- src/components/AmazonPay/config.ts
+++ src/components/AmazonPay/config.ts
@@ -1,6 +1,13 @@
+const AMAZON_SIGNATURE_ENDPOINT = 'services/PaymentInitiation/v1/getAmazonSignature';
+
+const FALLBACK_LOCALE_PER_REGION = {
+    eu: 'en_GB',
+    us: 'en_US'
+};
+
 const SUPPORTED_LOCALES_PER_REGION = {
     eu: ['en_GB', 'de_DE', 'fr_FR', 'it_IT', 'es_ES'],
     us: ['en_US']
 };
 
-export { SUPPORTED_LOCALES_PER_REGION };
+export { AMAZON_SIGNATURE_ENDPOINT, FALLBACK_LOCALE_PER_REGION, SUPPORTED_LOCALES_PER_REGION };
