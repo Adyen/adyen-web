36213fdc8c6c8cc7d4d3916b61cd0f50b1c5e6d3
pablo <pablo.igarzabal@adyen.com>
2020-07-08 18:45:30 +0200

    Update the AmazonPay config file to update the signature endpoint and refactor constants formatting to improve consistency.

--- src/components/AmazonPay/config.ts
+++ src/components/AmazonPay/config.ts
@@ -1,13 +1,20 @@
-const AMAZON_SIGNATURE_ENDPOINT = 'services/PaymentInitiation/v1/getAmazonSignature';
+const AMAZONPAY_SIGNATURE_ENDPOINT = 'v1/AmazonPayUtility/signString';
 
-const FALLBACK_LOCALE_PER_REGION = {
-    eu: 'en_GB',
-    us: 'en_US'
+const AMAZONPAY_URL_EU = 'https://static-eu.payments-amazon.com/checkout.js';
+const AMAZONPAY_URL_US = 'https://static-na.payments-amazon.com/checkout.js';
+
+const FALLBACK_LOCALE_EU = 'en_GB';
+const FALLBACK_LOCALE_US = 'en_US';
+
+const SUPPORTED_LOCALES_EU = ['en_GB', 'de_DE', 'fr_FR', 'it_IT', 'es_ES'] as const;
+const SUPPORTED_LOCALES_US = ['en_US'] as const;
+
+export {
+    AMAZONPAY_SIGNATURE_ENDPOINT,
+    AMAZONPAY_URL_EU,
+    AMAZONPAY_URL_US,
+    FALLBACK_LOCALE_EU,
+    FALLBACK_LOCALE_US,
+    SUPPORTED_LOCALES_EU,
+    SUPPORTED_LOCALES_US
 };
-
-const SUPPORTED_LOCALES_PER_REGION = {
-    eu: ['en_GB', 'de_DE', 'fr_FR', 'it_IT', 'es_ES'],
-    us: ['en_US']
-};
-
-export { AMAZON_SIGNATURE_ENDPOINT, FALLBACK_LOCALE_PER_REGION, SUPPORTED_LOCALES_PER_REGION };
