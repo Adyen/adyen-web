5dc128522cce9b06417949989bad2ba880fdf912
nicholas <nicholas.spong@adyen.com>
2020-07-14 16:17:31 +0200

    Set vars in jest.config.js to allow only specific batches of tests to run

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,3 +1,24 @@
+// Run all tests
+const pathToRootDir = '../';
+const pathToFileMock = '/config/testMocks/fileMock.js';
+const pathToStyleMock = '/config/testMocks/styleMock.js';
+const pathToFileSrc$1 = '/src$1';
+const pathToSetupTests = '/config/setupTests.js';
+
+// Run SecuredFields test only
+//const pathToRootDir = '../src/components/internal/SecuredFields/';
+//const pathToFileMock = '../../../../config/testMocks/fileMock.js';
+//const pathToStyleMock = '../../../../config/testMocks/styleMock.js';
+//const pathToFileSrc$1 = '../../../../src$1';
+//const pathToSetupTests = '../../../../config/setupTests.js';
+
+// Run Card tests only
+//const pathToRootDir = '../src/components/Card/';
+//const pathToFileMock = '../../../config/testMocks/fileMock.js';
+//const pathToStyleMock = '../../../config/testMocks/styleMock.js';
+//const pathToFileSrc$1 = '../../../src$1';
+//const pathToSetupTests = '../../../config/setupTests.js';
+
 module.exports = {
     transformIgnorePatterns: ['node_modules'],
     transform: {
@@ -12,35 +33,13 @@ module.exports = {
     coveragePathIgnorePatterns: ['node_modules/', 'config/'],
     moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx'],
     testPathIgnorePatterns: ['node_modules'],
-    testURL: 'https://localhost:3030'
+    testURL: 'https://localhost:3030',
+    rootDir: pathToRootDir,
+    moduleNameMapper: {
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>${pathToFileMock}`,
+        '\\.css$': `<rootDir>${pathToStyleMock}`,
+        '\\.scss$': `<rootDir>${pathToStyleMock}`,
+        '~(.*)$': `<rootDir>${pathToFileSrc$1}`
+    },
+    setupFilesAfterEnv: [`<rootDir>${pathToSetupTests}`]
 };
-
-// Top all tests
-/*module.exports.rootDir = '../';
-module.exports.moduleNameMapper = {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/config/testMocks/fileMock.js',
-    '\\.css$': '<rootDir>/config/testMocks/styleMock.js',
-    '\\.scss$': '<rootDir>/config/testMocks/styleMock.js',
-    '~(.*)$': '<rootDir>/src$1'
-};
-module.exports.setupFilesAfterEnv = ['<rootDir>/config/setupTests.js'];*/
-
-// Run SecuredFields test only
-/*module.exports.rootDir = '../src/components/internal/SecuredFields/';
-module.exports.moduleNameMapper = {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>../../../../config/testMocks/fileMock.js',
-    '\\.css$': '<rootDir>../../../../config/testMocks/styleMock.js',
-    '\\.scss$': '<rootDir>../../../../config/testMocks/styleMock.js',
-    '~(.*)$': '<rootDir>../../../../src$1'
-};
-module.exports.setupFilesAfterEnv = ['<rootDir>../../../../config/setupTests.js'];*/
-
-// Run Card tests only
-module.exports.rootDir = '../src/components/Card/';
-module.exports.moduleNameMapper = {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>../../../config/testMocks/fileMock.js',
-    '\\.css$': '<rootDir>../../../config/testMocks/styleMock.js',
-    '\\.scss$': '<rootDir>../../../config/testMocks/styleMock.js',
-    '~(.*)$': '<rootDir>../../../src$1'
-};
-module.exports.setupFilesAfterEnv = ['<rootDir>../../../config/setupTests.js'];
