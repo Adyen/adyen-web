29b5399ed87cec171ff8a2467706321140105d5f
nicholas <nicholas.spong@adyen.com>
2020-07-15 11:10:23 +0200

    Revert jest.config.js

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,24 +1,3 @@
-// Run all tests
-const pathToRootDir = '../';
-const pathToFileMock = '/config/testMocks/fileMock.js';
-const pathToStyleMock = '/config/testMocks/styleMock.js';
-const pathToFileSrc$1 = '/src$1';
-const pathToSetupTests = '/config/setupTests.js';
-
-// Run SecuredFields test only
-//const pathToRootDir = '../src/components/internal/SecuredFields/';
-//const pathToFileMock = '../../../../config/testMocks/fileMock.js';
-//const pathToStyleMock = '../../../../config/testMocks/styleMock.js';
-//const pathToFileSrc$1 = '../../../../src$1';
-//const pathToSetupTests = '../../../../config/setupTests.js';
-
-// Run Card tests only
-//const pathToRootDir = '../src/components/Card/';
-//const pathToFileMock = '../../../config/testMocks/fileMock.js';
-//const pathToStyleMock = '../../../config/testMocks/styleMock.js';
-//const pathToFileSrc$1 = '../../../src$1';
-//const pathToSetupTests = '../../../config/setupTests.js';
-
 module.exports = {
     transformIgnorePatterns: ['node_modules'],
     transform: {
@@ -26,20 +5,20 @@ module.exports = {
         '^.+\\.tsx?$': 'ts-jest',
         '^.+\\.js?$': 'ts-jest'
     },
-    verbose: false,
+    moduleNameMapper: {
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/config/testMocks/fileMock.js',
+        '\\.css$': '<rootDir>/config/testMocks/styleMock.js',
+        '\\.scss$': '<rootDir>/config/testMocks/styleMock.js',
+        '~(.*)$': '<rootDir>/src$1'
+    },
+    verbose: true,
     globals: {
         NODE_ENV: 'test'
     },
     coveragePathIgnorePatterns: ['node_modules/', 'config/'],
     moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx'],
     testPathIgnorePatterns: ['node_modules'],
+    rootDir: '../',
     testURL: 'https://localhost:3030',
-    rootDir: pathToRootDir,
-    moduleNameMapper: {
-        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>${pathToFileMock}`,
-        '\\.css$': `<rootDir>${pathToStyleMock}`,
-        '\\.scss$': `<rootDir>${pathToStyleMock}`,
-        '~(.*)$': `<rootDir>${pathToFileSrc$1}`
-    },
-    setupFilesAfterEnv: [`<rootDir>${pathToSetupTests}`]
+    setupFilesAfterEnv: ['<rootDir>/config/setupTests.js']
 };
