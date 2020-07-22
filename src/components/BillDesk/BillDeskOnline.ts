60493c09098618ecfaae7fe52aecaccce2ddb3a4
marcp <marc.pereziribas@adyen.com>
2020-07-22 17:02:55 +0200

    Disable images in some IssuerLists

--- src/components/BillDesk/BillDeskOnline.ts
+++ src/components/BillDesk/BillDeskOnline.ts
@@ -2,6 +2,13 @@ import IssuerListContainer from '../helpers/IssuerListContainer';
 
 class BillDeskOnlineElement extends IssuerListContainer {
     public static type = 'billdesk_online';
+
+    formatProps(props) {
+        return {
+            ...super.formatProps(props),
+            showImage: false
+        };
+    }
 }
 
 export default BillDeskOnlineElement;
--- src/components/EPS/index.ts
+++ src/components/EPS/index.ts
@@ -2,6 +2,13 @@ import IssuerListContainer from '../helpers/IssuerListContainer';
 
 class EPSElement extends IssuerListContainer {
     public static type = 'eps';
+
+    formatProps(props) {
+        return {
+            ...super.formatProps(props),
+            showImage: false
+        };
+    }
 }
 
 export default EPSElement;
