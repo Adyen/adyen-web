ce1c5794f8eb0f3ba87b7cc11694c4bceef59e37
shernaz <shernaz.syedsaleem@adyen.com>
2020-08-20 13:11:43 +0200

    create types file

---
+++ src/components/internal/PhoneInput/types.ts
@@ -0,0 +1,32 @@
+import Language from '../../../language/Language';
+
+export interface PhoneInputComponentProps {
+    onChange: (state) => void;
+    onValid: () => void;
+    payButton: () => void;
+
+    selected: string;
+    items: [];
+    minLength: number;
+    prefixName: string;
+    phoneName: string;
+    showPayButton: boolean;
+    isValid: boolean;
+    i18n?: Language;
+    data?: {
+        phonePrefix: string;
+        phoneNumber: string;
+    };
+}
+
+export interface PhoneInputState {
+    data?: {
+        phonePrefix: string;
+        phoneNumber: string;
+    };
+    errors?: {
+        phoneNumber?: boolean;
+        phonePrefix?: boolean;
+    };
+    isValid?: boolean;
+}
