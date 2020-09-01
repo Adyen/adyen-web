c6a629ac050eb43938855ea913690592267d5f45
marcp <marc.pereziribas@adyen.com>
2020-09-01 14:15:40 +0200

    Fix an issue where Safari could not resolve the initial isReadyToPay promise on Drop-in when automatically loading the Google Pay API JavaScript library

--- src/components/GooglePay/GooglePayService.ts
+++ src/components/GooglePay/GooglePayService.ts
@@ -2,6 +2,7 @@ import { isReadyToPayRequest, initiatePaymentRequest } from './requests';
 import { resolveEnvironment } from './utils';
 import Script from '../../utils/Script';
 import config from './config';
+import wait from '../../utils/wait';
 
 class GooglePayService {
     public readonly paymentsClient: Promise<google.payments.api.PaymentsClient>;
@@ -28,6 +29,7 @@ class GooglePayService {
         if (!window.google?.payments) {
             const script = new Script(config.URL);
             await script.load();
+            await wait(100);
         }
 
         return new google.payments.api.PaymentsClient(paymentOptions);
