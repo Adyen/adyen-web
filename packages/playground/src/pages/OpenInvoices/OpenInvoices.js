f66d682502c0601281fa3872750ad62121b6146e
marcp <marc.pereziribas@adyen.com>
2020-09-07 15:09:12 +0200

    Separate codebase into packages

---
+++ packages/playground/src/pages/OpenInvoices/OpenInvoices.js
@@ -0,0 +1,94 @@
+import AdyenCheckout from '@adyen/adyen-web';
+import { getPaymentMethods } from '../../services';
+import { handleChange, handleSubmit } from '../../handlers';
+import { amount, shopperLocale } from '../../config/commonConfig';
+import '../../../config/polyfills';
+import '../../style.scss';
+
+window.paymentData = {};
+
+getPaymentMethods({ amount, shopperLocale }).then(paymentMethodsData => {
+    window.checkout = new AdyenCheckout({
+        locale: shopperLocale,
+        paymentMethodsResponse: paymentMethodsData,
+        environment: 'test',
+        onChange: handleChange,
+        onSubmit: handleSubmit,
+        onError: console.error,
+        showPayButton: true,
+        amount // Optional. Used to display the amount in the Pay Button.
+    });
+
+    // AFTERPAY
+    window.afterpay = checkout
+        .create('afterpay_default', {
+            countryCode: 'NL', // 'NL' / 'BE'
+            visibility: {
+                personalDetails: 'editable', // editable [default] / readOnly / hidden
+                billingAddress: 'readOnly',
+                deliveryAddress: 'hidden'
+            },
+            data: {
+                billingAddress: {
+                    city: 'Gravenhage',
+                    country: 'NL',
+                    houseNumberOrName: '1',
+                    postalCode: '2521VA',
+                    street: 'Neherkade'
+                }
+            }
+        })
+        .mount('.afterpay-field');
+
+    // AFFIRM
+    window.affirm = checkout
+        .create('affirm', {
+            countryCode: 'US', // 'US' / 'CA'
+            visibility: {
+                personalDetails: 'editable', // editable [default] / readOnly / hidden
+                billingAddress: 'editable',
+                deliveryAddress: 'editable'
+            },
+            data: {
+                personalDetails: {
+                    firstName: 'Jan',
+                    lastName: 'Jansen',
+                    shopperEmail: 'shopper@testemail.com',
+                    telephoneNumber: '+17203977880'
+                },
+                billingAddress: {
+                    city: 'Boulder',
+                    country: 'US',
+                    houseNumberOrName: '242',
+                    postalCode: '80302',
+                    stateOrProvince: 'CO',
+                    street: 'Silver Cloud Lane'
+                }
+            }
+        })
+        .mount('.affirm-field');
+
+    // FACILYPAY_3x
+    window.facilypay_3x = checkout
+        .create('facilypay_3x', {
+            countryCode: 'ES', // 'ES' / 'FR'
+            visibility: {
+                personalDetails: 'editable', // editable [default] / readOnly / hidden
+                billingAddress: 'editable',
+                deliveryAddress: 'editable'
+            }
+        })
+        .mount('.facilypay_3x-field');
+
+    // RATEPAY
+    window.ratepay = checkout
+        .create('ratepay', {
+            countryCode: 'DE', // 'DE' / 'AT' / 'CH'
+            visibility: {
+                personalDetails: 'editable', // editable [default] / readOnly / hidden
+                billingAddress: 'editable',
+                deliveryAddress: 'editable'
+            }
+        })
+        .mount('.ratepay-field');
+});
