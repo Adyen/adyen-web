266f6044aa4be9f1b255e23f19303f3e940cdead
pablo <pablo.igarzabal@adyen.com>
2020-09-10 11:39:26 +0200

    Update server files to only send the merchantAccount when necessary.

--- server/api/details.js
+++ server/api/details.js
@@ -1,9 +1,10 @@
 const { post } = require('request');
 const getPostParameters = require('../utils/getPostParameters');
 const handleCallback = require('../utils/handleCallback');
+const { MERCHANT_ACCOUNT: merchantAccount } = require('../utils/config');
 
 module.exports = (res, request) => {
-    const params = getPostParameters('/payments/details', request);
+    const params = getPostParameters('/payments/details', { merchantAccount, ...request });
 
     post(params, (err, response, body) => handleCallback({ err, response, body }, res));
 };
