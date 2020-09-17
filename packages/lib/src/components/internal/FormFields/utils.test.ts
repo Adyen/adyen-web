0fc9e56c478ea0113e8198b2238d6dfec87a103b
pablo <pablo.igarzabal@adyen.com>
2020-09-17 09:35:49 +0200

    Add util function to convert full width characters to half width characters. Remove unused functionality. Add tests.

---
+++ packages/lib/src/components/internal/FormFields/utils.test.ts
@@ -0,0 +1,19 @@
+import { convertFullToHalf } from './utils';
+
+describe('convertFullToHalf util', () => {
+    test('convert full width characters to half width characters', () => {
+        expect(convertFullToHalf('ｔｅｓｔ１２３＠ｅｍａｉｌ．com')).toBe('test123@email.com');
+    });
+
+    test('does not convert other characters', () => {
+        const katakana = 'パブロ';
+        const hiragana = '平仮名';
+        const kanji = '漢字';
+        const hangul = '훈민정음';
+
+        expect(convertFullToHalf(katakana)).toBe(katakana);
+        expect(convertFullToHalf(hiragana)).toBe(kanji);
+        expect(convertFullToHalf(kanji)).toBe(kanji);
+        expect(convertFullToHalf(hangul)).toBe(hangul);
+    });
+});
