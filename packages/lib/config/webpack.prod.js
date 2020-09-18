bea5fabaa622b3cde1b1af0b7d6652714558c33f
marcp <marc.pereziribas@adyen.com>
2020-09-18 14:02:18 +0200

    Remove unused require

--- packages/lib/config/webpack.prod.js
+++ packages/lib/config/webpack.prod.js
@@ -2,7 +2,6 @@ const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpackConfig = require('./webpack.config');
 const currentVersion = require('./version')();
