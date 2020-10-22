b3bb51ba69d183de62af047bff81272b2b503e9c
marcp <marc.pereziribas@adyen.com>
2020-10-22 10:45:22 +0200

    Add env variables to rollup build

--- packages/lib/config/rollup.config.js
+++ packages/lib/config/rollup.config.js
@@ -3,7 +3,16 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import json from '@rollup/plugin-json';
 import postcss from 'rollup-plugin-postcss';
+import replace from '@rollup/plugin-replace';
 import pkg from '../package.json';
+const currentVersion = require('./version')();
+
+if (process.env.CI !== 'true') {
+    console.warn(
+        '\x1b[33m%s\x1b[0m',
+        'Warning: Building custom bundle. We recommend using one of the official builds served by our servers or NPM. Check https://docs.adyen.com/checkout for more information.'
+    );
+}
 
 let cache;
 const isProduction = process.env.NODE_ENV === 'production';
@@ -23,6 +32,13 @@ export default async () => [
         input: 'src/index.ts',
         cache,
         plugins: [
+            replace({
+                'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+                'process.env.VERSION': JSON.stringify(currentVersion.ADYEN_WEB_VERSION),
+                'process.env.COMMIT_HASH': JSON.stringify(currentVersion.COMMIT_HASH),
+                'process.env.COMMIT_BRANCH': JSON.stringify(currentVersion.COMMIT_BRANCH),
+                'process.env.ADYEN_BUILD_ID': JSON.stringify(currentVersion.ADYEN_BUILD_ID)
+            }),
             resolve(),
             commonjs(),
             typescript({
