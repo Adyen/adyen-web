dd5cd20e82d9912de20a231699f43d817d8db563
marcp <marc.pereziribas@adyen.com>
2020-10-26 12:54:44 +0100

    Fix tests

--- packages/lib/src/components/ApplePay/ApplePay.test.ts
+++ packages/lib/src/components/ApplePay/ApplePay.test.ts
@@ -7,20 +7,13 @@ import defaultProps from './defaultProps';
 
 describe('ApplePay', () => {
     describe('formatProps', () => {
-        test('normalizes an amount in a legacy format', () => {
-            // @ts-ignore
-            const applepay = new ApplePay({ ...defaultProps, currencyCode: 'EUR', amount: 1000 });
-            expect(applepay.props.amount.currency).toEqual('EUR');
-            expect(applepay.props.amount.value).toEqual(1000);
-        });
-
-        test('normalizes an amount in a regular format', () => {
+        test('accepts an amount in a regular format', () => {
             const applepay = new ApplePay({ ...defaultProps, currencyCode: 'EUR', amount: { currency: 'EUR', value: 2000 } });
             expect(applepay.props.amount.value).toEqual(2000);
             expect(applepay.props.amount.currency).toEqual('EUR');
         });
 
-        test('normalizes an amount with default values', () => {
+        test('accepts an amount with default values', () => {
             const applepay = new ApplePay(defaultProps);
             expect(applepay.props.amount.value).toEqual(0);
             expect(applepay.props.amount.currency).toEqual('USD');
