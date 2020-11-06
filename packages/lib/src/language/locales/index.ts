21e082350a0b690d059ec79ba473af49daab12bc
pablo <pablo.igarzabal@adyen.com>
2020-11-06 09:15:05 +0100

    Add new languages and update translation files.
    Added languages:
    - cs-CZ
    - el-GR
    - hr-HR
    - hu-HU
    - ro-RO
    - sk-SK
    - sl-SI

--- packages/lib/src/language/locales/index.ts
+++ packages/lib/src/language/locales/index.ts
@@ -1,9 +1,13 @@
+import csCZ from './cs-CZ.json';
 import daDK from './da-DK.json';
 import deDE from './de-DE.json';
+import elGR from './el-GR.json';
 import enUS from './en-US.json';
 import esES from './es-ES.json';
 import fiFI from './fi-FI.json';
 import frFR from './fr-FR.json';
+import hrHR from './hr-HR.json';
+import huHU from './hu-HU.json';
 import itIT from './it-IT.json';
 import jaJP from './ja-JP.json';
 import koKR from './ko-KR.json';
@@ -11,18 +15,25 @@ import nlNL from './nl-NL.json';
 import noNO from './no-NO.json';
 import plPL from './pl-PL.json';
 import ptBR from './pt-BR.json';
+import roRO from './ro-RO.json';
 import ruRU from './ru-RU.json';
+import skSK from './sk-SK.json';
+import slSI from './sl-SI.json';
 import svSE from './sv-SE.json';
 import zhCN from './zh-CN.json';
 import zhTW from './zh-TW.json';
 
 export default {
+    'cs-CZ': csCZ,
     'da-DK': daDK,
     'de-DE': deDE,
+    'el-GR': elGR,
     'en-US': enUS,
     'es-ES': esES,
     'fi-FI': fiFI,
     'fr-FR': frFR,
+    'hr-HR': hrHR,
+    'hu-HU': huHU,
     'it-IT': itIT,
     'ja-JP': jaJP,
     'ko-KR': koKR,
@@ -30,7 +41,10 @@ export default {
     'no-NO': noNO,
     'pl-PL': plPL,
     'pt-BR': ptBR,
+    'ro-RO': roRO,
     'ru-RU': ruRU,
+    'sk-SK': skSK,
+    'sl-SI': slSI,
     'sv-SE': svSE,
     'zh-CN': zhCN,
     'zh-TW': zhTW
