133e505a5b3e604a9f8774ffe649a8ad3df61381
marcp <marc.pereziribas@adyen.com>
2020-11-09 13:36:34 +0100

    Filter core props sent to components/dropin

--- packages/lib/src/components/Dropin/components/utils.ts
+++ packages/lib/src/components/Dropin/components/utils.ts
@@ -1,26 +1,7 @@
 export function getCommonProps(props) {
     return {
-        // amount: props.amount,
-        // countryCode: props.countryCode,
         elementRef: props.elementRef,
-        // environment: props.environment,
-        // loadingContext: props.loadingContext,
-        // i18n: props.i18n,
-        // modules: props.modules,
-        // order: props.order,
-        // clientKey: props.clientKey,
         showPayButton: props.showPayButton,
         isDropin: true
-
-        // installmentOptions: props.installmentOptions,
-
-        // generic events
-        // onSubmit: props.onSubmit
-        // onAdditionalDetails: props.onAdditionalDetails,
-        // onCancel: props.onCancel,
-        // onChange: props.onChange,
-        // onError: props.onError,
-        // onBalanceCheck: props.onBalanceCheck,
-        // onOrderRequest: props.onOrderRequest
     };
 }
--- packages/lib/src/core/config.ts
+++ packages/lib/src/core/config.ts
@@ -3,6 +3,27 @@
  */
 export const FALLBACK_CONTEXT = 'https://checkoutshopper-live.adyen.com/checkoutshopper/';
 
+export const GENERIC_OPTIONS = [
+    'amount',
+    'countryCode',
+    'environment',
+    'loadingContext',
+    'i18n',
+    'modules',
+    'order',
+    'clientKey',
+    'showPayButton',
+    'installmentOptions',
+    'onSubmit',
+    'onAdditionalDetails',
+    'onCancel',
+    'onChange',
+    'onError',
+    'onBalanceCheck',
+    'onOrderRequest'
+];
+
 export default {
-    FALLBACK_CONTEXT
+    FALLBACK_CONTEXT,
+    GENERIC_OPTIONS
 };
