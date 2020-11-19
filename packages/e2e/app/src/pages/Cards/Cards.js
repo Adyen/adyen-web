69beaaa32dd709c2d89cc9c58320de91ec0a5ed4
marcp <marc.pereziribas@adyen.com>
2020-11-19 14:32:11 +0100

    Create e2e package

---
+++ packages/e2e/app/src/pages/Cards/Cards.js
@@ -0,0 +1,26 @@
+import AdyenCheckout from '@adyen/adyen-web';
+import '@adyen/adyen-web/dist/adyen.css';
+import { handleSubmit, handleAdditionalDetails, handleError } from '../../handlers';
+import { amount, shopperLocale, countryCode } from '../../services/commonConfig';
+import '../../style.scss';
+
+window.checkout = new AdyenCheckout({
+    amount,
+    clientKey: process.env.__CLIENT_KEY__,
+    locale: shopperLocale,
+    countryCode,
+    environment: 'test',
+    showPayButton: true,
+    onSubmit: handleSubmit,
+    onAdditionalDetails: handleAdditionalDetails,
+    onError: handleError,
+    ...window.mainConfiguration
+});
+
+// Credit card with installments
+window.card = checkout
+    .create('card', {
+        brands: ['mc', 'visa', 'amex'],
+        ...window.cardConfigObj
+    })
+    .mount('.card-field');
