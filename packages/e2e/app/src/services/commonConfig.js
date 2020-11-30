1267a62d852ca589f5aa6d1962338847cb9557d5
nicholas <nicholas.spong@adyen.com>
2020-11-30 13:41:25 +0100

    Added test for redirect flow

--- packages/e2e/app/src/services/commonConfig.js
+++ packages/e2e/app/src/services/commonConfig.js
@@ -1,12 +1,14 @@
 import getCurrency from '../utils/getCurrency';
+import { getSearchParameters } from '../utils/utils';
 
 const DEFAULT_LOCALE = 'en-US';
 const DEFAULT_COUNTRY = 'US';
 
+const urlParams = getSearchParameters(window.location.search);
 export const shopperLocale = DEFAULT_LOCALE;
 export const countryCode = DEFAULT_COUNTRY;
 export const currency = getCurrency(countryCode);
-export const amountValue = 25900;
+export const amountValue = urlParams.amount ?? 25900;
 export const amount = {
     currency,
     value: Number(amountValue)
