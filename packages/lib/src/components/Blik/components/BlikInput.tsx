44296a22e7216bd68bc51319d62505c6a9d8e69c
marcp <marc.pereziribas@adyen.com>
2020-12-03 15:49:50 +0100

    Migrate Blik to useForm

--- packages/lib/src/components/Blik/components/BlikInput.tsx
+++ packages/lib/src/components/Blik/components/BlikInput.tsx
@@ -6,6 +6,7 @@ import getImage from '../../../utils/get-image';
 import { renderFormField } from '../../internal/FormFields';
 import { UIElementProps } from '../../UIElement';
 import './BlikInput.scss';
+import useForm from '../../../utils/useForm';
 
 interface BlikInputProps extends UIElementProps {
     data?: BlikInputDataState;
@@ -25,31 +26,40 @@ interface BlikInputErrorState {
 
 function BlikInput(props: BlikInputProps) {
     const { i18n, loadingContext } = useCoreContext();
+    const { handleChangeFor, triggerValidation, data, valid, errors, isValid } = useForm({
+        schema: ['blikCode'],
+        rules: {
+            blur: {
+                blikCode: code => code?.length === 6
+            },
+            input: {
+                blikCode: code => code?.length === 6
+            }
+        }
+    });
+    // const [data, setData] = useState<BlikInputDataState>(props.data);
+    // const [errors, setErrors] = useState<BlikInputErrorState>({ blikCode: false });
+    // const [valid, setValid] = useState<BlikInputValidState>({ blikCode: false });
 
-    const [data, setData] = useState<BlikInputDataState>(props.data);
-    const [errors, setErrors] = useState<BlikInputErrorState>({ blikCode: false });
-    const [valid, setValid] = useState<BlikInputValidState>({ blikCode: false });
-
-    const handleBlikCode = type => (e: Event): void => {
-        e.preventDefault();
-
-        const value: string = (e.target as HTMLInputElement).value;
-        const isBlikCodeValid: boolean = value.length === 6;
-
-        setData({ blikCode: value });
-        setErrors({ ...valid, blikCode: type === 'blur' && !isBlikCodeValid });
-        setValid({ ...valid, blikCode: isBlikCodeValid });
-    };
+    // const handleBlikCode = type => (e: Event): void => {
+    //     e.preventDefault();
+    //
+    //     const value: string = (e.target as HTMLInputElement).value;
+    //     const isBlikCodeValid: boolean = value.length === 6;
+    //
+    //     setData({ blikCode: value });
+    //     setErrors({ ...valid, blikCode: type === 'blur' && !isBlikCodeValid });
+    //     setValid({ ...valid, blikCode: isBlikCodeValid });
+    // };
 
     useEffect(() => {
-        props.onChange({ data, isValid: valid.blikCode }, this);
-    }, [data, valid, errors]);
+        console.log(valid, errors);
+        props.onChange({ data, errors, valid, isValid }, this);
+    }, [data, valid, errors, isValid]);
 
     const [status, setStatus] = useState('ready');
-
-    this.setStatus = newStatus => {
-        setStatus(newStatus);
-    };
+    this.setStatus = setStatus;
+    this.showValidation = triggerValidation;
 
     return (
         <div className="adyen-checkout__blik">
@@ -66,8 +76,8 @@ function BlikInput(props: BlikInputProps) {
                     spellcheck: false,
                     required: true,
                     autocorrect: 'off',
-                    onInput: handleBlikCode('input'),
-                    onChange: handleBlikCode('blur'),
+                    onInput: handleChangeFor('blikCode', 'input'),
+                    onChange: handleChangeFor('blikCode', 'blur'),
                     placeholder: '123456',
                     maxLength: 6
                 })}
