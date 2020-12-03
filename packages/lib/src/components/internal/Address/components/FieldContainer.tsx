45e284d6abedd49bc1b65fcfb5394d5eb1825472
pablo <pablo.igarzabal@adyen.com>
2020-12-03 15:48:49 +0100

    Add key property to the field mapper. Improve how we handle dropdown changes.

--- packages/lib/src/components/internal/Address/components/FieldContainer.tsx
+++ packages/lib/src/components/internal/Address/components/FieldContainer.tsx
@@ -11,7 +11,7 @@ function FieldContainer(props: FieldContainerProps) {
     const { i18n } = useCoreContext();
     const { classNameModifiers = [], data, errors, fieldName, onInput } = props;
     const errorMessage = !!errors[fieldName];
-    const value = data[fieldName] || null;
+    const value = data[fieldName];
     const labelKey = getKeyForField(fieldName, data.country);
 
     switch (fieldName) {
@@ -21,7 +21,7 @@ function FieldContainer(props: FieldContainerProps) {
                     classNameModifiers={classNameModifiers}
                     allowedCountries={props.allowedCountries}
                     errorMessage={errorMessage}
-                    onDropdownChange={props.onCountryChange}
+                    onDropdownChange={props.onDropdownChange}
                     value={value}
                 />
             );
@@ -31,7 +31,7 @@ function FieldContainer(props: FieldContainerProps) {
                     classNameModifiers={classNameModifiers}
                     selectedCountry={data.country}
                     errorMessage={errorMessage}
-                    onDropdownChange={props.onStateChange}
+                    onDropdownChange={props.onDropdownChange}
                     value={value}
                 />
             );
