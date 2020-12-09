7cb87983ab38fb0058cc2c92d38da198d987bf87
nicholas <nicholas.spong@adyen.com>
2020-12-09 09:06:45 +0100

    Restoring BacsResult.scss and using .scss vars

--- packages/lib/src/components/BacsDD/components/BacsResult.tsx
+++ packages/lib/src/components/BacsDD/components/BacsResult.tsx
@@ -3,6 +3,7 @@ import getImage from '../../../utils/get-image';
 import useCoreContext from '../../../core/Context/useCoreContext';
 import Voucher from '../../internal/Voucher';
 import '../../internal/Voucher/Voucher.scss';
+import './BacsResult.scss';
 
 const BacsResult = props => {
     const { i18n, loadingContext } = useCoreContext();
