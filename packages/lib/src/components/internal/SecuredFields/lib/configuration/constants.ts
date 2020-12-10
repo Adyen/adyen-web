72bf94cc21f00484430e0a89a26f116656103e77
nicholas <nicholas.spong@adyen.com>
2020-12-10 17:36:09 +0100

    Increasing securedFields version to 3.2.7

--- packages/lib/src/components/internal/SecuredFields/lib/configuration/constants.ts
+++ packages/lib/src/components/internal/SecuredFields/lib/configuration/constants.ts
@@ -10,7 +10,7 @@ export const ENCRYPTED_PIN_FIELD = 'encryptedPin';
 export const ENCRYPTED_BANK_ACCNT_NUMBER_FIELD = 'encryptedBankAccountNumber';
 export const ENCRYPTED_BANK_LOCATION_FIELD = 'encryptedBankLocationId';
 
-export const SF_VERSION = '3.2.6';
+export const SF_VERSION = '3.2.7';
 
 export const DEFAULT_CARD_GROUP_TYPES = ['amex', 'mc', 'visa'];
 
