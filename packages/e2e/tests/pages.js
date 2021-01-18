5d7331acddf8a2bcf8d842e4d6823e594f4c1efb
marcp <marc.pereziribas@adyen.com>
2021-01-18 12:42:51 +0100

    Add iDeal e2e tests

--- packages/e2e/tests/pages.js
+++ packages/e2e/tests/pages.js
@@ -3,3 +3,5 @@ export const BASE_URL = 'http://localhost:3024';
 export const CARDS_URL = `${BASE_URL}/cards/`;
 
 export const GIFTCARDS_URL = `${BASE_URL}/giftcards/`;
+
+export const ISSUERLISTS_URL = `${BASE_URL}/issuerlists/`;
