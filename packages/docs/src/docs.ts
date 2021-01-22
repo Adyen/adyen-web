e62c6ddb74fff18cad344eee78651e2c64d2cb91
marcp <marc.pereziribas@adyen.com>
2021-01-22 10:17:30 +0100

    Add AmazonPay props to docs export

--- packages/docs/src/docs.ts
+++ packages/docs/src/docs.ts
@@ -1,4 +1,5 @@
 import { ApplePayElementProps as ApplePayProps } from '@adyen/adyen-web/dist/types/components/ApplePay/types.d';
 import { GooglePayProps } from '@adyen/adyen-web/dist/types/components/GooglePay/types.d';
+import { AmazonPayElementProps as AmazonPayProps } from '@adyen/adyen-web/dist/types/components/AmazonPay/types.d';
 
-export { ApplePayProps, GooglePayProps };
+export { ApplePayProps, GooglePayProps, AmazonPayProps };
