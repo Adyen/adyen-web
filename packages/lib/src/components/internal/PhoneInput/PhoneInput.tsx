0ad43a8093bc488e404c3f6e8bca900dad84ef99
marcp <marc.pereziribas@adyen.com>
2021-01-22 16:20:20 +0100

    Migrate PhoneInput to hooks

--- packages/lib/src/components/internal/PhoneInput/PhoneInput.tsx
+++ packages/lib/src/components/internal/PhoneInput/PhoneInput.tsx
@@ -1,141 +1,84 @@
-import { Component, h } from 'preact';
+import { h } from 'preact';
+import { useEffect, useState } from 'preact/hooks';
 import classNames from 'classnames';
+import renderFormField from '../FormFields';
 import Field from '../FormFields/Field';
-import { renderFormField } from '../FormFields';
-import { validatePhoneNumber } from './validate';
-import './PhoneInput.scss';
-import { PhoneInputComponentProps, PhoneInputState } from './types';
+import useForm from '../../../utils/useForm';
+import useCoreContext from '../../../core/Context/useCoreContext';
 
-class PhoneInput extends Component<PhoneInputComponentProps, PhoneInputState> {
-    constructor(props) {
-        super(props);
-        this.handlePrefixChange = this.handlePrefixChange.bind(this);
-        this.handlePhoneInput = this.handlePhoneInput.bind(this);
-        this.onChange = this.onChange.bind(this);
-
-        this.state = {
-            data: {
-                phonePrefix: this.props.selected || this.props.data?.phonePrefix,
-                phoneNumber: this.props.data?.phoneNumber || ''
+export function PhoneInput(props) {
+    const { i18n } = useCoreContext();
+    const [status, setStatus] = useState('ready');
+    const showPrefix = !!props?.items?.length;
+    const { handleChangeFor, triggerValidation, data, valid, errors, isValid } = useForm({
+        schema: [...(showPrefix ? ['phonePrefix'] : []), 'phoneNumber'],
+        defaultData: { ...(showPrefix ? { phonePrefix: props.selected } : {}) },
+        rules: {
+            blur: {
+                phoneNumber: phone => phone?.length > 6 && phone?.length > 20
             },
-            isValid: !!this.props.data?.phonePrefix && !!this.props.data?.phoneNumber, // check what is the default value
-            errors: {}
-        };
-    }
-
-    public static defaultProps = {
-        onChange: () => {},
-        onValid: () => {},
-        phoneName: 'phoneNumber',
-        prefixName: 'phonePrefix',
-        selected: null,
-        minLength: 3
-    };
-
-    public onChange() {
-        const isPrefixValid = this.props.items ? !!this.state.data.phonePrefix : true;
-        const isPhoneNumberValid = validatePhoneNumber(this.state.data.phoneNumber, this.props.minLength);
-        const isValid = isPrefixValid && isPhoneNumberValid;
-
-        this.setState({ isValid }, () => {
-            this.props.onChange(this.state);
-        });
-    }
-
-    public handlePhoneInput = e => {
-        e.preventDefault();
-        const phoneNumber = e.target.value;
-        const isPhoneNumberValid = validatePhoneNumber(phoneNumber, this.props.minLength);
-
-        this.setState(
-            prevState => ({
-                data: { ...prevState.data, phoneNumber },
-                errors: { ...prevState.errors, phoneNumber: !isPhoneNumberValid }
-            }),
-
-            this.onChange
-        );
-    };
-
-    public handlePrefixChange = e => {
-        e.preventDefault();
-        const phonePrefix = e.currentTarget.getAttribute('data-value');
-        const isPrefixValid = !!phonePrefix;
-
-        this.setState(
-            prevState => ({
-                data: { ...prevState.data, phonePrefix },
-                ...(isPrefixValid && { errors: { ...prevState.errors, phonePrefix: false } })
-            }),
-            this.onChange
-        );
-    };
-
-    public showValidation = () => {
-        const isPrefixValid = this.props.items && this.props.items.length ? !!this.state.data.phonePrefix : true;
-        const isPhoneNumberValid = validatePhoneNumber(this.state.data.phoneNumber, this.props.minLength);
-
-        this.setState({
-            errors: {
-                phoneNumber: !isPhoneNumberValid,
-                phonePrefix: !isPrefixValid
+            input: {
+                phoneNumber: code => code?.length < 10
             }
-        });
-    };
+        }
+    });
 
-    render() {
-        const showPrefix = !!this.props.items && this.props.items.length;
+    useEffect(() => {
+        props.onChange({ data, valid, errors, isValid });
+    }, [data, valid, errors, isValid]);
 
-        return (
-            <div className="adyen-checkout__phone-input">
-                <Field
-                    errorMessage={!!this.state.errors.phoneNumber}
-                    label={this.props.i18n.get('telephoneNumber')}
-                    className={classNames({
-                        'adyen-checkout__input--phone-number': true
-                    })}
-                    inputWrapperModifiers={['phoneInput']}
-                >
-                    <div className="adyen-checkout__input-wrapper">
-                        <div
-                            className={classNames({
-                                'adyen-checkout__input': true,
-                                'adyen-checkout__input--invalid': !!this.state.errors.phoneNumber
-                            })}
-                        >
-                            {!!showPrefix && (
-                                <Field inputWrapperModifiers={['phoneInput']}>
-                                    {renderFormField('select', {
-                                        className: 'adyen-checkout__dropdown--small adyen-checkout__countryFlag',
-                                        items: this.props.items,
-                                        name: this.props.prefixName,
-                                        onChange: this.handlePrefixChange,
-                                        placeholder: this.props.i18n.get('infix'),
-                                        selected: this.state.data.phonePrefix
-                                    })}
+    this.showValidation = triggerValidation;
+    this.setStatus = setStatus;
+    return (
+        <div className="adyen-checkout__phone-input">
+            <Field
+                errorMessage={!!errors.phoneNumber}
+                label={this.props.i18n.get('telephoneNumber')}
+                className={classNames({
+                    'adyen-checkout__input--phone-number': true
+                })}
+                inputWrapperModifiers={['phoneInput']}
+            >
+                <div className="adyen-checkout__input-wrapper">
+                    <div
+                        className={classNames({
+                            'adyen-checkout__input': true,
+                            'adyen-checkout__input--invalid': !!errors.phoneNumber
+                        })}
+                    >
+                        {!!showPrefix && (
+                            <Field inputWrapperModifiers={['phoneInput']}>
+                                {renderFormField('select', {
+                                    className: 'adyen-checkout__dropdown--small adyen-checkout__countryFlag',
+                                    items: props.items,
+                                    name: props.prefixName,
+                                    onChange: handleChangeFor('phonePrefix'),
+                                    placeholder: i18n.get('infix'),
+                                    selected: data.phonePrefix
+                                })}
 
-                                    <div className="adyen-checkout__phoneNumber">
-                                        <div>{this.state.data.phonePrefix}</div>
+                                <div className="adyen-checkout__phoneNumber">
+                                    <div>{data.phonePrefix}</div>
 
-                                        <input
-                                            type="tel"
-                                            name={this.props.phoneName}
-                                            value={this.state.data.phoneNumber}
-                                            onInput={this.handlePhoneInput}
-                                            placeholder="123 456 789"
-                                            className="adyen-checkout__input adyen-checkout__input--phoneNumber"
-                                            autoCorrect="off"
-                                        />
-                                    </div>
-                                </Field>
-                            )}
-                        </div>
+                                    <input
+                                        type="tel"
+                                        name={props.phoneName}
+                                        value={data.phoneNumber}
+                                        onInput={handleChangeFor('phoneNumber', 'input')}
+                                        onChange={handleChangeFor('phoneNumber', 'blur')}
+                                        placeholder="123 456 789"
+                                        className="adyen-checkout__input adyen-checkout__input--phoneNumber"
+                                        autoCorrect="off"
+                                    />
+                                </div>
+                            </Field>
+                        )}
                     </div>
-                </Field>
-                {this.props.showPayButton && this.props.payButton()}
-            </div>
-        );
-    }
+                </div>
+            </Field>
+            {this.props.showPayButton && this.props.payButton({ status })}
+        </div>
+    );
 }
 
 export default PhoneInput;
