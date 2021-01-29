090f40a7b9276bca19e9d9347a247cecaa432365
marcp <marc.pereziribas@adyen.com>
2021-01-29 15:38:24 +0100

    Add copyBtn and introduction to PIX component

--- packages/lib/src/components/Pix/Pix.ts
+++ packages/lib/src/components/Pix/Pix.ts
@@ -7,6 +7,8 @@ class PixElement extends QRLoaderContainer {
         return {
             delay: 2000, // ms
             countdownTime: 15, // min
+            copyBtn: true,
+            introduction: 'Abra o app com sua chave PIX cadastrada, escolha Pagar com Pix e escaneie o QR Code ou copie e cole o código',
             ...super.formatProps(props)
         };
     }
