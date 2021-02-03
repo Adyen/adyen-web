74d708f4a380b07da4ec63876a97316318d64aba
marcp <marc.pereziribas@adyen.com>
2021-02-03 16:30:56 +0100

    Migrate BankTransfer component to useHook

--- packages/lib/src/components/BankTransfer/components/BankTransferInput/BankTransferInput.tsx
+++ packages/lib/src/components/BankTransfer/components/BankTransferInput/BankTransferInput.tsx
@@ -3,41 +3,29 @@ import useCoreContext from '../../../../core/Context/useCoreContext';
 import './BankTransferInput.scss';
 import SendCopyToEmail from '../../../internal/SendCopyToEmail/SendCopyToEmail';
 import { useEffect, useState } from 'preact/hooks';
-import Validator from '../../../../utils/Validator';
-
-const validator = new Validator({});
+import useForm from '../../../../utils/useForm';
 
 function BankTransferInput(props) {
     const { i18n } = useCoreContext();
-    const [errors, setErrors] = useState({ shopperEmail: false });
-    const [data, setData] = useState({ shopperEmail: null });
-    const [valid, setValid] = useState({ shopperEmail: false });
     const [showingEmail, setShowingEmail] = useState(false);
+
+    const { handleChangeFor, triggerValidation, data, valid, errors, isValid, setSchema } = useForm({
+        schema: [],
+        defaultData: props.data
+    });
 
     const toggleEmailField = () => setShowingEmail(!showingEmail);
 
-    const handleChangeFor = (key, validationMode) => e => {
-        const { value } = e.target;
-        const isValid = validator.validate(key, validationMode)(value);
+    useEffect(() => {
+        const newSchema = showingEmail ? ['shopperEmail'] : [];
+        setSchema(newSchema);
+    }, [showingEmail]);
 
-        setData({ ...data, [key]: value });
-        setValid({ ...valid, [key]: isValid });
-        setErrors({ ...errors, [key]: !isValid });
-    };
-
-    this.showValidation = () => {
-        setErrors({
-            ...(showingEmail && { shopperEmail: !validator.validate('shopperEmail')(data.shopperEmail) })
-        });
-    };
+    this.showValidation = triggerValidation;
 
     useEffect(() => {
-        const emailRequired = showingEmail && props.showEmailAddress;
-        const emailAddressValid = emailRequired ? Boolean(validator.validate('shopperEmail', 'blur')(data.shopperEmail)) : true;
-        const shopperEmail = emailRequired ? data.shopperEmail : null;
-
-        props.onChange({ data: { ...data, shopperEmail }, isValid: emailAddressValid });
-    }, [data, valid, errors, showingEmail]);
+        props.onChange({ data, errors, valid, isValid });
+    }, [data, valid, errors, showingEmail, isValid]);
 
     return (
         <div className="adyen-checkout__bankTransfer">
