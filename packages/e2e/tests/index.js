58a3e115b690507e27003c6810cd2c439f530fe9
nicholas <nicholas.spong@adyen.com>
2021-02-16 14:43:31 +0100

    Fixing paths for tests

--- packages/e2e/tests/index.js
+++ packages/e2e/tests/index.js
@@ -16,8 +16,7 @@ const remote = process.argv.indexOf('--remote') > -1 || process.argv.indexOf('-r
             browser = remoteConnection;
         }
         const failedCount = await runner
-            //            .src(`${PATH}**/*.test.js`)
-            .src(`${PATH}**/threeDS2.**.test.js`)
+            .src(`${PATH}**/*.test.js`)
             .browsers(browser)
             .run();
 
