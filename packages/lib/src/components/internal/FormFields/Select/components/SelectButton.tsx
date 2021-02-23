3c54529f79aff65aa95e1db5bca6fb9d5b641726
pablo <pablo.igarzabal@adyen.com>
2021-02-23 10:53:53 +0100

    Improve accessibility and reduce duplicated code.

--- packages/lib/src/components/internal/FormFields/Select/components/SelectButton.tsx
+++ packages/lib/src/components/internal/FormFields/Select/components/SelectButton.tsx
@@ -4,50 +4,18 @@ import useCoreContext from '../../../../../core/Context/useCoreContext';
 import { SelectButtonProps } from '../types';
 import styles from '../Select.module.scss';
 
+function SelectButtonElement({ filterable, ...props }) {
+    if (filterable) return <div {...props} />;
+
+    return <button {...props} />;
+}
+
 function SelectButton(props: SelectButtonProps) {
     const { i18n } = useCoreContext();
     const { active, readonly, showList } = props;
 
-    if (props.filterable) {
-        return (
-            <div
-                aria-autocomplete="list"
-                aria-controls={props.selectListId}
-                aria-disabled={readonly}
-                aria-expanded={showList}
-                aria-haspopup="listbox"
-                className={cx({
-                    'adyen-checkout__dropdown__button': true,
-                    [styles['adyen-checkout__dropdown__button']]: true,
-                    'adyen-checkout__dropdown__button--readonly': readonly,
-                    'adyen-checkout__dropdown__button--active': showList,
-                    [styles['adyen-checkout__dropdown__button--active']]: showList,
-                    'adyen-checkout__dropdown__button--invalid': props.isInvalid
-                })}
-                onClick={!readonly ? props.toggleList : null}
-                onKeyDown={!readonly ? props.onButtonKeyDown : null}
-                ref={props.toggleButtonRef}
-                role="combobox"
-                tabIndex={0}
-                title={active.name || props.placeholder}
-            >
-                {!showList ? (
-                    <Fragment>{active.name || props.placeholder}</Fragment>
-                ) : (
-                    <input
-                        className={cx('adyen-checkout__filter-input', [styles['adyen-checkout__filter-input']])}
-                        onInput={props.onInput}
-                        placeholder={i18n.get('select.filter.placeholder')}
-                        ref={props.filterInputRef}
-                        type="text"
-                    />
-                )}
-            </div>
-        );
-    }
-
     return (
-        <button
+        <SelectButtonElement
             aria-disabled={readonly}
             aria-expanded={showList}
             aria-haspopup="listbox"
@@ -59,15 +27,36 @@ function SelectButton(props: SelectButtonProps) {
                 [styles['adyen-checkout__dropdown__button--active']]: showList,
                 'adyen-checkout__dropdown__button--invalid': props.isInvalid
             })}
+            filterable={props.filterable}
             onClick={!readonly ? props.toggleList : null}
             onKeyDown={!readonly ? props.onButtonKeyDown : null}
             ref={props.toggleButtonRef}
+            role={props.filterable ? 'button' : null}
             tabIndex={0}
             title={active.name || props.placeholder}
+            type={!props.filterable ? 'button' : null}
         >
-            <span className="adyen-checkout__dropdown__button__text">{active.selectedOptionName || active.name || props.placeholder}</span>
-            {active.icon && <img className="adyen-checkout__dropdown__button__icon" src={active.icon} alt={active.name} />}
-        </button>
+            {!showList || !props.filterable ? (
+                <Fragment>
+                    <span className="adyen-checkout__dropdown__button__text">{active.selectedOptionName || active.name || props.placeholder}</span>
+                    {active.icon && <img className="adyen-checkout__dropdown__button__icon" src={active.icon} alt={active.name} />}
+                </Fragment>
+            ) : (
+                <input
+                    aria-autocomplete="list"
+                    aria-controls={props.selectListId}
+                    aria-expanded={showList}
+                    aria-owns={props.selectListId}
+                    autoComplete="off"
+                    className={cx('adyen-checkout__filter-input', [styles['adyen-checkout__filter-input']])}
+                    onInput={props.onInput}
+                    placeholder={i18n.get('select.filter.placeholder')}
+                    ref={props.filterInputRef}
+                    role="combobox"
+                    type="text"
+                />
+            )}
+        </SelectButtonElement>
     );
 }
 export default SelectButton;
