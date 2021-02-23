61463a8b3463296a49765a6847814da927bd53e3
marcp <marc.pereziribas@adyen.com>
2021-02-23 11:47:24 +0100

    Delay conversionId fetching until first telemetry call

--- packages/lib/src/core/Services/collect-id.ts
+++ packages/lib/src/core/Services/collect-id.ts
@@ -3,18 +3,27 @@ import { httpPost } from './http';
 /**
  * Log event to Adyen
  * @param config - ready to be serialized and included in the body of request
- * @returns a promise containing the response of the call
+ * @returns a function returning a promise containing the response of the call
  */
-const collectId = config => {
-    if (!config.clientKey) return Promise.reject();
+const collectId = ({ loadingContext, clientKey }) => {
+    let promise;
 
     const options = {
         errorLevel: 'silent' as const,
-        loadingContext: config.loadingContext,
-        path: `v1/analytics/id?clientKey=${config.clientKey}`
+        loadingContext: loadingContext,
+        path: `v1/analytics/id?clientKey=${clientKey}`
     };
 
-    return httpPost(options).then(conversion => conversion.id);
+    return () => {
+        if (promise) return promise;
+        if (!clientKey) return Promise.reject();
+
+        promise = httpPost(options)
+            .then(conversion => conversion?.id)
+            .catch(() => {});
+
+        return promise;
+    };
 };
 
 export default collectId;
