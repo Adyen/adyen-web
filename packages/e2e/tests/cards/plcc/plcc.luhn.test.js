9d9ae2a7e815a707e800e86a9aa03dc468597e42
nicholas <nicholas.spong@adyen.com>
2021-02-25 12:42:30 +0100

    Added e2e test for card not needing luhn check

--- packages/e2e/tests/cards/plcc/plcc.luhn.test.js
+++ packages/e2e/tests/cards/plcc/plcc.luhn.test.js
@@ -5,11 +5,9 @@ import { Selector, ClientFunction, RequestMock } from 'testcafe';
 import { start, getIframeSelector, getIsValid } from '../../utils/commonUtils';
 import cu from '../utils/cardUtils';
 import { BASE_URL, CARDS_URL } from '../../pages';
-import { TEST_CVC_VALUE, UNKNOWN_BIN_CARD } from '../utils/constants';
+import { FAILS_LUHN_CARD } from '../utils/constants';
 
 const brandingIcon = Selector('.card-field .adyen-checkout__card__cardNumber__brandIcon');
-
-const dateSpan = Selector('.card-field .adyen-checkout__card__exp-date__input');
 
 const requestURL = `https://checkoutshopper-test.adyen.com/checkoutshopper/v2/bin/binLookup?token=${process.env.CLIENT_KEY}`;
 
@@ -18,8 +16,8 @@ const mockedResponse = {
         {
             brand: 'bcmc', // keep as a recognised card brand (bcmc) until we have a genuine plcc - to avoid logo loading errors
             cvcPolicy: 'required',
-            enableLuhnCheck: true,
-            showExpiryDate: false,
+            enableLuhnCheck: false,
+            showExpiryDate: true,
             supported: true
         }
     ],
@@ -47,35 +45,28 @@ const iframeSelector = getIframeSelector('.card-field iframe');
 
 const cardUtils = cu(iframeSelector);
 
-fixture`Testing a PLCC, as detected by a mock/binLookup, for a response that should indicate hidden expiryDate field)`
+fixture`Testing a PLCC, as detected by a mock/binLookup, for a response that should indicate luhn check is not required)`
     .page(CARDS_URL)
     .clientScripts('plcc.clientScripts.js')
     .requestHooks(mock);
 
-test('Test plcc card hides date field ', async t => {
+test('Test plcc card becomes valid with number that fails luhn check ', async t => {
     // Start, allow time for iframes to load
     await start(t, 2000, TEST_SPEED);
 
-    // Generic card
+    // generic card
     await t.expect(brandingIcon.getAttribute('src')).contains('nocard.svg');
 
     // Unknown card
-    await cardUtils.fillCardNumber(t, UNKNOWN_BIN_CARD);
+    await cardUtils.fillCardNumber(t, FAILS_LUHN_CARD);
 
     await t
         // bcmc card icon
         .expect(brandingIcon.getAttribute('src'))
-        .contains('bcmc.svg')
-
-        // hidden date field
-        .expect(dateSpan.filterHidden().exists)
-        .ok();
-
-    // Is not valid
-    await t.expect(getIsValid('card')).eql(false);
+        .contains('bcmc.svg');
 
     // Fill cvc
-    await cardUtils.fillCVC(t, TEST_CVC_VALUE);
+    await cardUtils.fillDateAndCVC(t);
 
     // Is valid
     await t.expect(getIsValid('card')).eql(true);
@@ -87,11 +78,7 @@ test('Test plcc card hides date field ', async t => {
     await t
         // generic card icon
         .expect(brandingIcon.getAttribute('src'))
-        .contains('nocard.svg')
-
-        // visible date field
-        .expect(dateSpan.filterVisible().exists)
-        .ok();
+        .contains('nocard.svg');
 
     await t.expect(getIsValid('card')).eql(false);
 });
