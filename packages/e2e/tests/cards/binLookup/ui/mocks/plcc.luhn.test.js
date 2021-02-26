a9a1de2879c87a986889c0fdc8629e03e2daeb5c
nicholas <nicholas.spong@adyen.com>
2021-02-26 08:50:33 +0100

    fixed failing e2e tests that used RequestMock

--- packages/e2e/tests/cards/binLookup/ui/mocks/plcc.luhn.test.js
+++ packages/e2e/tests/cards/binLookup/ui/mocks/plcc.luhn.test.js
@@ -30,7 +30,9 @@ const mockedResponse = {
 };
 
 const mock = RequestMock()
-    .onRequestTo(requestURL)
+    .onRequestTo(request => {
+        return request.url === requestURL && request.method === 'post';
+    })
     .respond(
         (req, res) => {
             const body = JSON.parse(req.body);
