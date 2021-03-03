5729f9a657da78e4a40215af1833f50d5bec8299
pablo <pablo.igarzabal@adyen.com>
2021-03-03 10:58:22 +0100

    Update address validation rules.

--- packages/lib/src/components/internal/Address/validate.ts
+++ packages/lib/src/components/internal/Address/validate.ts
@@ -1,8 +1,17 @@
 import { ValidatorRules } from '../../../utils/Validator/Validator';
 
-export const addressValidationRules: ValidatorRules = {
+export const getAddressValidationRules = (specifications): ValidatorRules => ({
+    houseNumberOrName: {
+        validate: (value, context) => {
+            const selectedCountry = context.state?.data?.country;
+            const isOptional = selectedCountry && specifications.countryHasOptionalField(selectedCountry, 'houseNumberOrName');
+            if (isOptional && !value) return true;
+            return value && value.length > 0;
+        },
+        modes: ['blur']
+    },
     default: {
         validate: value => value && value.length > 0,
         modes: ['blur']
     }
-};
+});
