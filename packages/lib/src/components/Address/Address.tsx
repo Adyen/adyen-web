e0c030b329e0fd67e20288704de7a77666c5247f
pablo <pablo.igarzabal@adyen.com>
2021-03-03 10:28:17 +0100

    Add Address and Personal Details elements.

---
+++ packages/lib/src/components/Address/Address.tsx
@@ -0,0 +1,30 @@
+import { h } from 'preact';
+import UIElement from '../UIElement';
+import Address from '../internal/Address';
+import CoreProvider from '../../core/Context/CoreProvider';
+
+export class AddressElement extends UIElement {
+    get data() {
+        return { ...this.state.data };
+    }
+
+    get isValid() {
+        return !!this.state.isValid;
+    }
+
+    render() {
+        return (
+            <CoreProvider i18n={this.props.i18n} loadingContext={this.props.loadingContext}>
+                <Address
+                    ref={ref => {
+                        this.componentRef = ref;
+                    }}
+                    {...this.props}
+                    onChange={this.setState}
+                />
+            </CoreProvider>
+        );
+    }
+}
+
+export default AddressElement;
