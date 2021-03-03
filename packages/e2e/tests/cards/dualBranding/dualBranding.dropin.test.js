a39b92e65b11ef45f8b6b3241accdd16870834b1
nicholas <nicholas.spong@adyen.com>
2021-03-03 14:41:09 +0100

    Added tests to Dropin scenario

--- packages/e2e/tests/cards/dualBranding/dualBranding.dropin.test.js
+++ packages/e2e/tests/cards/dualBranding/dualBranding.dropin.test.js
@@ -1,7 +1,7 @@
 import { Selector, ClientFunction } from 'testcafe';
 import { start, getIframeSelector, getIsValid } from '../../utils/commonUtils';
 import cu from '../utils/cardUtils';
-import { DUAL_BRANDED_CARD } from '../utils/constants';
+import { DUAL_BRANDED_CARD, REGULAR_TEST_CARD } from '../utils/constants';
 import { BASE_URL } from '../../pages';
 
 const dualBrandingIconHolder = Selector('.adyen-checkout__payment-method--card .adyen-checkout__card__dual-branding__buttons');
@@ -171,6 +171,175 @@ test(
             )
             .eql(false)
             // first icon should have the "not selected" class
+            .expect(
+                dualBrandingIconHolderActive
+                    .find('img')
+                    .nth(0)
+                    .hasClass(NOT_SELECTED_CLASS)
+            )
+            .eql(true);
+    }
+);
+
+test(
+    'Fill in card number that will get single branding result from binLookup, ' +
+        'then enter a dual branded card number,' +
+        'check both brand icons are at full alpha,' +
+        'then click icons and make sure they go to the expected alpha',
+    async t => {
+        // Start, allow time for iframes to load
+        await start(t, 2000, TEST_SPEED);
+
+        await cardUtils.fillCardNumber(t, REGULAR_TEST_CARD);
+
+        // Fill card field with dual branded card (visa/cb)
+        await cardUtils.fillCardNumber(t, DUAL_BRANDED_CARD, true);
+
+        // Check buttons are active
+        await t.expect(dualBrandingIconHolderActive.exists).ok();
+
+        // Check icon opacities (should be 100%)
+        await t
+            // first icon SHOULDN'T have the "not selected" class
+            .expect(
+                dualBrandingIconHolderActive
+                    .find('img')
+                    .nth(0)
+                    .hasClass(NOT_SELECTED_CLASS)
+            )
+            .eql(false)
+            // second icon SHOULDN'T have the "not selected" class
+            .expect(
+                dualBrandingIconHolderActive
+                    .find('img')
+                    .nth(1)
+                    .hasClass(NOT_SELECTED_CLASS)
+            )
+            .eql(false);
+
+        // Click brand icons
+        await t
+            // click first icon
+            .click(dualBrandingIconHolderActive.find('img').nth(0))
+            // first icon SHOULDN'T have the "not selected" class
+            .expect(
+                dualBrandingIconHolderActive
+                    .find('img')
+                    .nth(0)
+                    .hasClass(NOT_SELECTED_CLASS)
+            )
+            .eql(false)
+            // second icon SHOULD have the "not selected" class
+            .expect(
+                dualBrandingIconHolderActive
+                    .find('img')
+                    .nth(1)
+                    .hasClass(NOT_SELECTED_CLASS)
+            )
+            .eql(true)
+            // click second icon
+            .click(dualBrandingIconHolderActive.find('img').nth(1))
+            // second icon SHOULDN'T have the "not selected" class
+            .expect(
+                dualBrandingIconHolderActive
+                    .find('img')
+                    .nth(1)
+                    .hasClass(NOT_SELECTED_CLASS)
+            )
+            .eql(false)
+            // first icon SHOULD have the "not selected" class
+            .expect(
+                dualBrandingIconHolderActive
+                    .find('img')
+                    .nth(0)
+                    .hasClass(NOT_SELECTED_CLASS)
+            )
+            .eql(true);
+    }
+);
+
+test.only(
+    'Fill in card number that will get single branding result from binLookup, ' +
+        'then enter a partial dual branded card number,' +
+        'check both brand icons are at reduced alpha,' +
+        'complete card number,' +
+        'check both brand icons are at full alpha,' +
+        'then click icons and make sure they go to the expected alpha',
+    async t => {
+        // Start, allow time for iframes to load
+        await start(t, 2000, TEST_SPEED);
+
+        await cardUtils.fillCardNumber(t, REGULAR_TEST_CARD);
+
+        const firstDigits = DUAL_BRANDED_CARD.substring(0, 11);
+        const lastDigits = DUAL_BRANDED_CARD.substring(11, 16);
+
+        // Partially fill card field with dual branded card (visa/cb)
+        await cardUtils.fillCardNumber(t, firstDigits, true);
+
+        // Check buttons are present but NOT active (which will mean the holding element is at 25% opacity)
+        await t
+            .expect(dualBrandingIconHolder.exists)
+            .ok()
+            .expect(dualBrandingIconHolderActive.exists)
+            .notOk();
+
+        // Complete field
+        await cardUtils.fillCardNumber(t, lastDigits);
+
+        // Check buttons are active
+        await t.expect(dualBrandingIconHolderActive.exists).ok();
+
+        // Check icon opacities (should be 100%)
+        await t
+            // first icon SHOULDN'T have the "not selected" class
+            .expect(
+                dualBrandingIconHolderActive
+                    .find('img')
+                    .nth(0)
+                    .hasClass(NOT_SELECTED_CLASS)
+            )
+            .eql(false)
+            // second icon SHOULDN'T have the "not selected" class
+            .expect(
+                dualBrandingIconHolderActive
+                    .find('img')
+                    .nth(1)
+                    .hasClass(NOT_SELECTED_CLASS)
+            )
+            .eql(false);
+
+        // Click brand icons
+        await t
+            // click first icon
+            .click(dualBrandingIconHolderActive.find('img').nth(0))
+            // first icon SHOULDN'T have the "not selected" class
+            .expect(
+                dualBrandingIconHolderActive
+                    .find('img')
+                    .nth(0)
+                    .hasClass(NOT_SELECTED_CLASS)
+            )
+            .eql(false)
+            // second icon SHOULD have the "not selected" class
+            .expect(
+                dualBrandingIconHolderActive
+                    .find('img')
+                    .nth(1)
+                    .hasClass(NOT_SELECTED_CLASS)
+            )
+            .eql(true)
+            // click second icon
+            .click(dualBrandingIconHolderActive.find('img').nth(1))
+            // second icon SHOULDN'T have the "not selected" class
+            .expect(
+                dualBrandingIconHolderActive
+                    .find('img')
+                    .nth(1)
+                    .hasClass(NOT_SELECTED_CLASS)
+            )
+            .eql(false)
+            // first icon SHOULD have the "not selected" class
             .expect(
                 dualBrandingIconHolderActive
                     .find('img')
