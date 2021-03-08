e2819e6e501a336763c76f52dfbcc39c3f54ecd1
marcp <marc.pereziribas@adyen.com>
2021-03-08 14:09:45 +0100

    Exclude external dependencies from ES/CJS bundle

---
+++ packages/lib/config/terser.config.js
@@ -0,0 +1,11 @@
+const terserConfig = {
+    output: {
+        ecma: 5,
+        comments: false,
+        // Turned on because emoji and regex is not minified properly using default
+        // https://github.com/facebook/create-react-app/issues/2488
+        ascii_only: true
+    }
+};
+
+export default terserConfig;
