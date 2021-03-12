249cfe9506b265f9acc10c623f2e72f4e077dd5d
nicholas <nicholas.spong@adyen.com>
2021-03-12 09:53:03 +0100

    Ensure the Card prop hasCVC is set to false for all Bcmc/Bancontact instances not just stored cards

--- packages/lib/src/components/Card/Card.tsx
+++ packages/lib/src/components/Card/Card.tsx
@@ -15,7 +15,7 @@ export class CardElement extends UIElement<CardElementProps> {
             ...props,
             // Mismatch between hasHolderName & holderNameRequired which can mean card can never be valid
             holderNameRequired: !props.hasHolderName ? false : props.holderNameRequired,
-            // Special catch for recurring bcmc (i.e. card with no cvc field). Scenario?? - Dropin - One click with no details
+            // False for Bcmc cards & if merchant explicitly wants to hide the CVC field
             hasCVC: !((props.brand && props.brand === 'bcmc') || props.hideCVC),
             // billingAddressRequired only available for non-stored cards
             billingAddressRequired: props.storedPaymentMethodId ? false : props.billingAddressRequired,
