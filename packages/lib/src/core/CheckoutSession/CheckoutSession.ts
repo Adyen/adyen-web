a357d696f9a7466998acf2e8440e11031f4c2d7c
marcp <marc.pereziribas@adyen.com>
2021-03-17 14:23:41 +0100

    Move CheckoutSession file

---
+++ packages/lib/src/core/CheckoutSession/CheckoutSession.ts
@@ -0,0 +1,83 @@
+import { CheckoutSession } from '../../types';
+import makePayment from '../Services/makePayment';
+import submitDetails from '../Services/submitDetails';
+import { getStoredSession, storeSession } from './storage';
+import setupSession from '../Services/setupSession';
+
+class Session {
+    private readonly session: CheckoutSession;
+    public clientKey: any;
+    public loadingContext: any;
+
+    constructor(session, clientKey, loadingContext) {
+        if (!session.id) throw new Error('No Session ID');
+        if (!clientKey) throw new Error('No clientKey available');
+
+        this.session = session;
+
+        if (!this.session.data) {
+            this.session = this.getStoredSession();
+        } else {
+            this.storeSession();
+        }
+
+        this.clientKey = clientKey;
+        this.loadingContext = loadingContext;
+    }
+
+    get id() {
+        return this.session.id;
+    }
+
+    get data() {
+        return this.session.data;
+    }
+
+    /**
+     * Updates the session.data with the latest data blob
+     */
+    updateSessionData(latestData: string) {
+        this.session.data = latestData;
+        this.storeSession();
+    }
+
+    setupSession() {
+        return setupSession(this.session, { clientKey: this.clientKey, loadingContext: this.loadingContext });
+    }
+
+    /**
+     * Submits a session payment
+     */
+    makePayment(data) {
+        return makePayment(data, this).then(response => {
+            if (response.sessionData) {
+                this.updateSessionData(response.sessionData);
+            }
+
+            return response;
+        });
+    }
+
+    /**
+     * Submits session payment additional details
+     */
+    submitDetails(data) {
+        return submitDetails(data, this).then(response => {
+            if (response.sessionData) {
+                this.updateSessionData(response.sessionData);
+            }
+
+            return response;
+        });
+    }
+
+    getStoredSession(): CheckoutSession {
+        const storedSession = getStoredSession();
+        return this.id === storedSession?.id ? storedSession : this.session;
+    }
+
+    storeSession() {
+        storeSession(this.session);
+    }
+}
+export default Session;
