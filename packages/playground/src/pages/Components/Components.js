51f8a4866450a4cca8f7f39aa1651b6e077d45e5
marcp <marc.pereziribas@adyen.com>
2021-04-08 15:54:55 +0200

    Add Klarna Widget in Components playground

--- packages/playground/src/pages/Components/Components.js
+++ packages/playground/src/pages/Components/Components.js
@@ -41,6 +41,19 @@ getPaymentMethods({ amount, shopperLocale }).then(paymentMethodsResponse => {
     // MBWay
     window.mbway = checkout.create('mbway').mount('.mbway-field');
 
+    window.klarnaButton = checkout.create('klarna').mount('.klarna-field');
+    window.klarnaWidget = checkout
+        .createFromAction({
+            type: 'sdk',
+            paymentMethodType: 'klarna',
+            paymentData: '...',
+            sdkData: {
+                client_token:
+                    'eyJhbGciOiJSUzI1NiIsImtpZCI6IjgyMzA1ZWJjLWI4MTEtMzYzNy1hYTRjLTY2ZWNhMTg3NGYzZCJ9.eyJzZXNzaW9uX2lkIjoiNmViYTBjZDktMzVkMy0zNzBiLTk5NzUtNzg1MzA0ZGIwNjAzIiwiYmFzZV91cmwiOiJodHRwczovL2pzLnBsYXlncm91bmQua2xhcm5hLmNvbS9ldS9rcCIsImRlc2lnbiI6ImtsYXJuYSIsImxhbmd1YWdlIjoibmwiLCJwdXJjaGFzZV9jb3VudHJ5IjoiTkwiLCJlbnZpcm9ubWVudCI6InBsYXlncm91bmQiLCJtZXJjaGFudF9uYW1lIjoiQWR5ZW4iLCJzZXNzaW9uX3R5cGUiOiJQQVlNRU5UUyIsImNsaWVudF9ldmVudF9iYXNlX3VybCI6Imh0dHBzOi8vZXUucGxheWdyb3VuZC5rbGFybmFldnQuY29tIiwic2NoZW1lIjp0cnVlLCJleHBlcmltZW50cyI6W3sibmFtZSI6ImluLWFwcC1zZGstbmV3LWludGVybmFsLWJyb3dzZXIiLCJ2YXJpYXRlIjoibmV3LWludGVybmFsLWJyb3dzZXItZW5hYmxlIiwicGFyYW1ldGVycyI6eyJ2YXJpYXRlX2lkIjoibmV3LWludGVybmFsLWJyb3dzZXItZW5hYmxlIn19LHsibmFtZSI6ImluLWFwcC1zZGstY2FyZC1zY2FubmluZyIsInZhcmlhdGUiOiJjYXJkLXNjYW5uaW5nLWVuYWJsZSIsInBhcmFtZXRlcnMiOnsidmFyaWF0ZV9pZCI6ImNhcmQtc2Nhbm5pbmctZW5hYmxlIn19XSwicmVnaW9uIjoiZXUifQ.KtcKFP_FGtCEiGrVBm54iz5FpLUDBPHRt8C26PIvo1MjawxquBi7Lm2y_m4wtNTeu229zdKArZ2DFdnzebCDJJ-_PAyoiWv1v85MKrs-R0I1Axz9gmzlEQkjltDTXHFPUj5QNe-2y-l8jBV50xSHXUVPfHI5L9XrjvKuuIcoFla3n1fE6yhBVfiYarty7oca782C5oGNcJcJNRzsPdLD1LzWOQ1HIVVDDeEUsKImPpRbSHM83JrH-uyDbO3rlzEQi8OjeYcvuhGHilsf0RCFjWQYpDMXfu4v4uvcJUofzB7j54sfoq5K9E5dO_sgvENSI7tgw2wBN3uQT5hFD1PeGQ'
+            }
+        })
+        .mount('.klarna-result-field');
+
     // ACH
     window.ach = checkout
         .create('ach', {
