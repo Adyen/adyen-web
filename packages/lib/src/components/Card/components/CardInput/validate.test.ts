dd51cb2d6318de2aac09f6d6c9ffaccd99dbe20c
nicholas <nicholas.spong@adyen.com>
2021-04-23 11:00:23 +0200

    Added taxNumber validation tests

--- packages/lib/src/components/Card/components/CardInput/validate.test.ts
+++ packages/lib/src/components/Card/components/CardInput/validate.test.ts
@@ -1,12 +1,24 @@
 import { getRuleByNameAndMode } from './validate';
 
 const holderNameValidationFn = getRuleByNameAndMode('holderName', 'blur');
+const taxNumValidationFn = getRuleByNameAndMode('taxNumber', 'blur');
 
-describe('validateHolderName', () => {
-    test('validates a required card holder name', () => {
+describe('validate holder name', () => {
+    test('validates a card holder name', () => {
         expect(holderNameValidationFn('')).toBe(false);
         expect(holderNameValidationFn('John Smith')).toBe(true);
+        expect(holderNameValidationFn('   ')).toBe(false);
         // expect(validateHolderName(undefined)).toBe(false);
         // expect(validateHolderName(null)).toBe(false);
+    });
+});
+
+describe('validate tax number', () => {
+    test('validates a tax number is 6 or 10 digits', () => {
+        expect(taxNumValidationFn('')).toBe(false);
+        expect(taxNumValidationFn('12345')).toBe(false);
+        expect(taxNumValidationFn('123456')).toBe(true);
+        expect(taxNumValidationFn('1234567')).toBe(false);
+        expect(taxNumValidationFn('1234567890')).toBe(true);
     });
 });
