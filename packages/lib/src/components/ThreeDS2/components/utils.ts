b04b7484f00c9983d4c7691a2727e2ba2f2a86fe
sponglord <nicholas.spong@adyen.com>
2021-05-20 11:56:18 +0200

    handleAction accepts & propagates challengeWindowSize option (#1084)
    * handleAction accepts & propagates challengeWindowSize option
    
    * propagates challengeWindowSize when handleAction called from Dropin
    
    * Added tests for challengeWindowSize being read from object sent to handleAction function
    
    * always set challengeWindowSize in threeDS2 action
    
    * removing commented out line

--- packages/lib/src/components/ThreeDS2/components/utils.ts
+++ packages/lib/src/components/ThreeDS2/components/utils.ts
@@ -183,9 +183,7 @@ const fingerprintFlowPropsDropin = ['elementRef'];
  *  Must contain all props needed for the challenge stage since, in the new 3DS2 flow, the fingerprint component will be the "component" reference
  *  if the /submitThreeDS2Fingerprint response dictates we "handleAction" to create a challenge
  */
-const fingerprintFlowProps = ['createFromAction', 'onAdditionalDetails', 'challengeWindowSize'];
-
-const challengeFlowProps = ['challengeWindowSize'];
+const fingerprintFlowProps = ['createFromAction', 'onAdditionalDetails'];
 
 /**
  * Add props specifically needed for the type of 3DS2 flow: fingerprint or challenge
@@ -204,7 +202,7 @@ export const get3DS2FlowProps = (actionSubtype, props) => {
     }
 
     // Challenge
-    const rtnObj = pick(challengeFlowProps).from(props);
-    rtnObj.statusType = 'custom';
-    return rtnObj;
+    return {
+        statusType: 'custom'
+    };
 };
