bc3f74b3eb11f33651266adc5f8c2b75d29ce7e1
marcp <marc.pereziribas@adyen.com>
2021-06-03 09:36:39 +0200

    Migrate PaymentMethodList to hooks

--- packages/lib/src/components/Dropin/components/PaymentMethod/PaymentMethodList.tsx
+++ packages/lib/src/components/Dropin/components/PaymentMethod/PaymentMethodList.tsx
@@ -1,4 +1,5 @@
-import { Component, Fragment, h } from 'preact';
+import { Fragment, h } from 'preact';
+import { useEffect } from 'preact/hooks';
 import classNames from 'classnames';
 import PaymentMethodItem from './PaymentMethodItem';
 import getProp from '../../../../utils/getProp';
@@ -25,75 +26,71 @@ interface PaymentMethodListProps {
     isLoading: boolean;
 }
 
-class PaymentMethodList extends Component<PaymentMethodListProps> {
-    public static defaultProps: PaymentMethodListProps = {
-        paymentMethods: [],
-        activePaymentMethod: null,
-        cachedPaymentMethods: {},
-        orderStatus: null,
-        onSelect: () => {},
-        onDisableStoredPaymentMethod: () => {},
-        isDisabling: false,
-        isLoading: false
-    };
+function PaymentMethodList(props: PaymentMethodListProps) {
+    const onSelect = paymentMethod => () => props.onSelect(paymentMethod);
 
-    componentDidMount() {
+    useEffect(() => {
         // Open first PaymentMethodItem
-        if (this.props.paymentMethods[0]) {
-            const firstPaymentMethod = this.props.paymentMethods[0];
-            const shouldOpenFirstStored = this.props.openFirstStoredPaymentMethod && getProp(firstPaymentMethod, 'props.oneClick') === true;
-            const shouldOpenFirstPaymentMethod = shouldOpenFirstStored || this.props.openFirstPaymentMethod;
+        if (props.paymentMethods[0]) {
+            const firstPaymentMethod = props.paymentMethods[0];
+            const shouldOpenFirstStored = props.openFirstStoredPaymentMethod && getProp(firstPaymentMethod, 'props.oneClick') === true;
+            const shouldOpenFirstPaymentMethod = shouldOpenFirstStored || props.openFirstPaymentMethod;
 
             if (shouldOpenFirstPaymentMethod) {
-                this.onSelect(firstPaymentMethod)();
+                onSelect(firstPaymentMethod)();
             }
         }
-    }
+    }, []);
 
-    public onSelect = paymentMethod => () => this.props.onSelect(paymentMethod);
+    const paymentMethodListClassnames = classNames({
+        [styles['adyen-checkout__payment-methods-list']]: true,
+        'adyen-checkout__payment-methods-list': true,
+        'adyen-checkout__payment-methods-list--loading': props.isLoading
+    });
 
-    render({ paymentMethods, activePaymentMethod, cachedPaymentMethods, isLoading }) {
-        const paymentMethodListClassnames = classNames({
-            [styles['adyen-checkout__payment-methods-list']]: true,
-            'adyen-checkout__payment-methods-list': true,
-            'adyen-checkout__payment-methods-list--loading': isLoading
-        });
+    return (
+        <Fragment>
+            {props.orderStatus && <OrderPaymentMethods order={props.order} orderStatus={props.orderStatus} onOrderCancel={props.onOrderCancel} />}
 
-        return (
-            <Fragment>
-                {this.props.orderStatus && (
-                    <OrderPaymentMethods order={this.props.order} orderStatus={this.props.orderStatus} onOrderCancel={this.props.onOrderCancel} />
-                )}
+            <ul className={paymentMethodListClassnames}>
+                {props.paymentMethods.map((paymentMethod, index, paymentMethodsCollection) => {
+                    const isSelected = props.activePaymentMethod && props.activePaymentMethod._id === paymentMethod._id;
+                    const isLoaded = paymentMethod._id in props.cachedPaymentMethods;
+                    const isNextOneSelected =
+                        props.activePaymentMethod &&
+                        paymentMethodsCollection[index + 1] &&
+                        props.activePaymentMethod._id === paymentMethodsCollection[index + 1]._id;
 
-                <ul className={paymentMethodListClassnames}>
-                    {paymentMethods.map((paymentMethod, index, paymentMethodsCollection) => {
-                        const isSelected = activePaymentMethod && activePaymentMethod._id === paymentMethod._id;
-                        const isLoaded = paymentMethod._id in cachedPaymentMethods;
-                        const isNextOneSelected =
-                            activePaymentMethod &&
-                            paymentMethodsCollection[index + 1] &&
-                            activePaymentMethod._id === paymentMethodsCollection[index + 1]._id;
-
-                        return (
-                            <PaymentMethodItem
-                                className={classNames({ 'adyen-checkout__payment-method--next-selected': isNextOneSelected })}
-                                standalone={paymentMethods.length === 1}
-                                paymentMethod={paymentMethod}
-                                isSelected={isSelected}
-                                isDisabling={isSelected && this.props.isDisabling}
-                                isLoaded={isLoaded}
-                                isLoading={isLoading}
-                                onSelect={this.onSelect(paymentMethod)}
-                                key={paymentMethod._id}
-                                showRemovePaymentMethodButton={this.props.showRemovePaymentMethodButton}
-                                onDisableStoredPaymentMethod={this.props.onDisableStoredPaymentMethod}
-                            />
-                        );
-                    })}
-                </ul>
-            </Fragment>
-        );
-    }
+                    return (
+                        <PaymentMethodItem
+                            className={classNames({ 'adyen-checkout__payment-method--next-selected': isNextOneSelected })}
+                            standalone={props.paymentMethods.length === 1}
+                            paymentMethod={paymentMethod}
+                            isSelected={isSelected}
+                            isDisabling={isSelected && props.isDisabling}
+                            isLoaded={isLoaded}
+                            isLoading={props.isLoading}
+                            onSelect={onSelect(paymentMethod)}
+                            key={paymentMethod._id}
+                            showRemovePaymentMethodButton={props.showRemovePaymentMethodButton}
+                            onDisableStoredPaymentMethod={props.onDisableStoredPaymentMethod}
+                        />
+                    );
+                })}
+            </ul>
+        </Fragment>
+    );
 }
+
+PaymentMethodList.defaultProps = {
+    paymentMethods: [],
+    activePaymentMethod: null,
+    cachedPaymentMethods: {},
+    orderStatus: null,
+    onSelect: () => {},
+    onDisableStoredPaymentMethod: () => {},
+    isDisabling: false,
+    isLoading: false
+} as PaymentMethodListProps;
 
 export default PaymentMethodList;
