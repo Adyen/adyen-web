60166ec09e672ad558609944cab309514c589258
nicholas <nicholas.spong@adyen.com>
2021-10-28 18:25:29 +0200

    onChange function for Cards that maps state.errors to remove DOM nodes - moved to Cards.js (it's just not always picked up from the expiryDate.clientScript.js for some reason)

--- packages/e2e/app/src/pages/Cards/Cards.js
+++ packages/e2e/app/src/pages/Cards/Cards.js
@@ -23,6 +23,18 @@ const initCheckout = async () => {
         .create('card', {
             brands: ['mc', 'visa', 'amex', 'maestro', 'bcmc'],
             //            onChange: state => console.log(state),
+            onChange: state => {
+                // Needed now that, for v5, we enhance the securedFields state.errors object with a rootNode prop
+                // - Testcafe doesn't like a ClientFunction retrieving an object with a DOM node in it!?
+                if (!!Object.keys(state.errors).length) {
+                    // Replace any rootNode values in the objects in state.errors with an empty string
+                    const nuErrors = Object.entries(state.errors).reduce((acc, [fieldType, error]) => {
+                        acc[fieldType] = error ? { ...error, rootNode: '' } : error;
+                        return acc;
+                    }, {});
+                    window.mappedStateErrors = nuErrors;
+                }
+            },
             ...window.cardConfig
         })
         .mount('.card-field');
--- packages/e2e/tests/cards/expiryDate/expiryDate.clientScripts.js
+++ packages/e2e/tests/cards/expiryDate/expiryDate.clientScripts.js
@@ -1,17 +1,17 @@
 window.cardConfig = {
     type: 'scheme',
     brands: ['mc', 'visa', 'amex'],
-    minimumExpiryDate: '09/22',
-    onChange: state => {
-        // Needed now that, for v5, we enhance the securedFields state.errors object with a rootNode prop
-        // - Testcafe doesn't like a ClientFunction retrieving an object with a DOM node in it!?
-        if (!!Object.keys(state.errors).length) {
-            // Replace any rootNode values in the objects in state.errors with an empty string
-            const nuErrors = Object.entries(state.errors).reduce((acc, [fieldType, error]) => {
-                acc[fieldType] = error ? { ...error, rootNode: '' } : error;
-                return acc;
-            }, {});
-            window.mappedStateErrors = nuErrors;
-        }
-    }
+    minimumExpiryDate: '09/22'
+    //    onChange: state => {
+    //        // Needed now that, for v5, we enhance the securedFields state.errors object with a rootNode prop
+    //        // - Testcafe doesn't like a ClientFunction retrieving an object with a DOM node in it!?
+    //        if (!!Object.keys(state.errors).length) {
+    //            // Replace any rootNode values in the objects in state.errors with an empty string
+    //            const nuErrors = Object.entries(state.errors).reduce((acc, [fieldType, error]) => {
+    //                acc[fieldType] = error ? { ...error, rootNode: '' } : error;
+    //                return acc;
+    //            }, {});
+    //            window.mappedStateErrors = nuErrors;
+    //        }
+    //    }
 };
