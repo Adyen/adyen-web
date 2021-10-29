2c9466da9e3ad53b35fc98e855d46ab0b02c5dd5
nicholas <nicholas.spong@adyen.com>
2021-10-29 15:03:06 +0200

    removing unused code

--- packages/e2e/tests/cards/expiryDate/expiryDate.clientScripts.js
+++ packages/e2e/tests/cards/expiryDate/expiryDate.clientScripts.js
@@ -2,19 +2,4 @@ window.cardConfig = {
     type: 'scheme',
     brands: ['mc', 'visa', 'amex'],
     minimumExpiryDate: '09/22'
-    /**
-     * This function should work here, but it doesn't - see comment in e2e/app/src/pages/Cards/Cards.js
-     */
-    //    onChange: state => {
-    //        // Needed now that, for v5, we enhance the securedFields state.errors object with a rootNode prop
-    //        // - Testcafe doesn't like a ClientFunction retrieving an object with a DOM node in it!?
-    //        if (!!Object.keys(state.errors).length) {
-    //            // Replace any rootNode values in the objects in state.errors with an empty string
-    //            const nuErrors = Object.entries(state.errors).reduce((acc, [fieldType, error]) => {
-    //                acc[fieldType] = error ? { ...error, rootNode: '' } : error;
-    //                return acc;
-    //            }, {});
-    //            window.mappedStateErrors = nuErrors;
-    //        }
-    //    }
 };
