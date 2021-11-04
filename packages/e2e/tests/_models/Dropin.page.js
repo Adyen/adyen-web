a1d6a73d6ae584685582b82c12ae083bc2a1ee79
sponglord <nicholas.spong@adyen.com>
2021-11-04 14:10:40 +0100

    DropinPage model is instantiated with an object containing refs to other page models (#1351)
    * DropinPage model is instantiated with an object containing refs to other page models
    
    * Add prop to .env template
    
    * Corrected how Dropin page accesses state for the paymentMethod
    
    * Added tests for custom card and hidden expiry date

--- packages/e2e/tests/_models/Dropin.page.js
+++ packages/e2e/tests/_models/Dropin.page.js
@@ -1,71 +1,42 @@
 import { ClientFunction, Selector } from 'testcafe';
 import BasePage from './BasePage';
-import { getIframeSelector } from '../utils/commonUtils';
-import cu from '../cards/utils/cardUtils';
 
 /**
- * The Page Model Pattern is a test automation pattern that allows you to create an
- * abstraction of the tested page, and use it in test code to refer to page elements
+ * Expects a 'components' property in the object sent to the constructor containing instances of other Page models
+ * e.g.
+ * const dropinPage = new DropinPage({
+ *   components: {
+ *     cc: new CardComponentPage('.adyen-checkout__payment-method--bcmc')
+ *   }
+ * });
  */
 export default class DropinPage extends BasePage {
-    constructor(pmSelector) {
+    constructor({ components }) {
         super('');
+
+        Object.assign(this, components);
 
         const BASE_EL = '.adyen-checkout__dropin';
 
         /**
-         * ###################
-         * Credit card related
-         * ###################
+         * ###############################################
+         * Credit card related (but only appear in Dropin)
+         * ###############################################
          */
+        // Regular branding
         this.brandsHolder = Selector(`${BASE_EL} .adyen-checkout__payment-method__brands`);
         this.brandsImages = this.brandsHolder.find('img');
 
-        /**
-         * CardNumber
-         */
-        // Top level <div>
-        this.numHolder = Selector(`${BASE_EL} .adyen-checkout__field--cardNumber`);
-        // The <span> that holds the iframe
-        this.numSpan = Selector(`${BASE_EL} .adyen-checkout__card__cardNumber__input`);
-
-        // The <img> el that holds the card brand logo (actually a child of this.numSpan)
-        this.brandingIcon = Selector(`${BASE_EL} .adyen-checkout__card__cardNumber__brandIcon`);
-
-        /**
-         * ExpiryDate
-         */
-        // Top level <div>
-        this.dateHolder = Selector(`${BASE_EL} .adyen-checkout__field__exp-date`);
-        // The <span> that holds the iframe
-        this.dateSpan = Selector(`${BASE_EL} .adyen-checkout__card__exp-date__input`);
-
-        /**
-         * CVC
-         */
-        // Top level <div>
-        this.cvcHolder = Selector(`${BASE_EL} .adyen-checkout__field__cvc`);
-        // The <span> that holds the iframe
-        this.cvcSpan = Selector(`${BASE_EL} .adyen-checkout__card__cvc__input`);
-
-        /**
-         * Dual branding
-         */
+        // Dual branding
         this.dualBrandingIconHolder = Selector(`${BASE_EL} .adyen-checkout__card__dual-branding__buttons`);
         this.dualBrandingIconHolderActive = Selector(`${BASE_EL} .adyen-checkout__card__dual-branding__buttons--active`);
         this.dualBrandingImages = this.dualBrandingIconHolderActive.find('img');
-
-        /**
-         * iframe utils
-         */
-        this.iframeSelector = getIframeSelector(`${BASE_EL} ${pmSelector} iframe`);
-        this.cardUtils = cu(this.iframeSelector);
     }
 
     getFromState = ClientFunction(path => {
         const splitPath = path.split('.');
         const reducer = (xs, x) => (xs && xs[x] !== undefined ? xs[x] : undefined);
 
-        return splitPath.reduce(reducer, window.dropin.dropinRef.state);
+        return splitPath.reduce(reducer, window.dropin.dropinRef.state.activePaymentMethod.state);
     });
 }
