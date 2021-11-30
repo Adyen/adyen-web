b7d14c190f4c999bfb7ebb60909bd01487b41d8e
nicholas <nicholas.spong@adyen.com>
2021-11-30 14:06:20 +0100

    ErrorPanel - changed class names

--- packages/lib/src/core/Errors/ErrorPanel.tsx
+++ packages/lib/src/core/Errors/ErrorPanel.tsx
@@ -29,16 +29,16 @@ export function ErrorPanel({
     callbackFn?.(errors);
 
     return (
-        <div className={showPanel ? 'adyen-checkout-error-panel' : 'sr-only'} id={id} aria-live="polite">
+        <div className={showPanel ? 'adyen-checkout-error-panel' : 'adyen-checkout-error-panel--sr-only'} id={id} aria-live="polite">
             <div className="adl-alert adl-alert--error">
                 <div className="adl-alert__header">
                     <span className="adl-alert__icon">
                         <i className="adl-icon-triangle"></i>
                     </span>
-                    <span className="adl-alert__title">{heading}</span>
+                    <span className="adl-alert__title adyen-checkout-error-panel__title">{heading}</span>
                 </div>
                 {errorMessages.map(error => (
-                    <div key={error} className="adl-alert__explanation">
+                    <div key={error} className="adl-alert__explanation adyen-checkout-error-panel__error">
                         {error}
                     </div>
                 ))}
