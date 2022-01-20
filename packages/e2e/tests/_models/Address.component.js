4b46ba43cbd418ffbce1f3f56c3c2c470a457a0d
guilhermer <guilherme.ribeiro@adyen.com>
2022-01-20 01:12:48 +0100

    feat: e2e tests for postal code

--- packages/e2e/tests/_models/Address.component.js
+++ packages/e2e/tests/_models/Address.component.js
@@ -1,16 +1,26 @@
 import { t, ClientFunction, Selector } from 'testcafe';
 
 export default class AddressComponent {
-    countrySelector = null;
-    streetInput = null;
-
     constructor(baseEl = '.adyen-checkout__fieldset') {
-        this.countrySelector = Selector(`${baseEl} .adyen-checkout__dropdown__button`);
-        this.streetInput = Selector(`${baseEl} .adyen-checkout__input--street`);
+        this.baseEl = Selector(baseEl);
+
+        this.countrySelector = this.baseEl.find('.adyen-checkout__field--country .adyen-checkout__dropdown');
+
+        this.streetInput = this.baseEl.find('.adyen-checkout__input--street');
+
+        this.postalCodeLabel = this.baseEl.find('.adyen-checkout__field--postalCode .adyen-checkout__label__text');
+        this.postalCodeInput = this.baseEl.find('.adyen-checkout__input--postalCode');
+        this.postalCodeInputError = this.baseEl.find('.adyen-checkout__field--postalCode .adyen-checkout__error-text');
     }
 
-    async fillStreetInput(value = '') {
-        await t.typeText(this.streetInput, value);
+    async fillPostalCode(value = '') {
+        await t.typeText(this.postalCodeInput, value);
+    }
+
+    async selectCountry(value = '') {
+        await t.click(this.countrySelector);
+        const countryDropdownItem = this.countrySelector.find('.adyen-checkout__dropdown__element').withText(value);
+        await t.click(countryDropdownItem);
     }
 
     getFromState = ClientFunction(path => {
--- packages/e2e/tests/address/address.mocks.js
+++ packages/e2e/tests/address/address.mocks.js
@@ -1,23 +1,14 @@
 import { RequestMock, RequestLogger } from 'testcafe';
 import { BASE_URL } from '../pages';
 
-// const path = require('path');
-// require('dotenv').config({ path: path.resolve('../../', '.env') });
-//
-// const MOCK_SESSION_ID = 'CS616D08FC28573F9C';
-// const MOCK_SESSION_DATA = 'Ab02b4c0!BQABAgChW9EQ6U';
-//
-// const sessionsUrl = 'http://localhost:3024/sessions';
-// const setupUrl = `https://checkoutshopper-test.adyen.com/checkoutshopper/v1/sessions/${MOCK_SESSION_ID}/setup?clientKey=${process.env.CLIENT_KEY}`;
-// const paymentUrl = `https://checkoutshopper-test.adyen.com/checkoutshopper/v1/sessions/${MOCK_SESSION_ID}/payments?clientKey=${process.env.CLIENT_KEY}`;
-
 const countriesDatasetUrl = 'https://checkoutshopper-test.adyen.com/checkoutshopper/datasets/countries/en-US.json';
 const unitedStatesDatasetUrl = 'https://checkoutshopper-test.adyen.com/checkoutshopper/datasets/states/US/en-US.json';
 const brazilDatasetUrl = 'https://checkoutshopper-test.adyen.com/checkoutshopper/datasets/states/BR/en-US.json';
 
 const countriesDatasetResponse = [
     { id: 'BR', name: 'Brazil' },
-    { id: 'US', name: 'United States' }
+    { id: 'US', name: 'United States' },
+    { id: 'NL', name: 'Netherlands' }
 ];
 
 const unitedStatesDatasetResponse = [
@@ -29,11 +20,6 @@ const brazilDatasetResponse = [
     { id: 'MG', name: 'Minas Gerais' },
     { id: 'SP', name: 'São Paulo' }
 ];
-
-// const loggers = {
-//     setupLogger: RequestLogger({ url: setupUrl, method: 'post' }, { logRequestBody: true }),
-//     paymentLogger: RequestLogger({ url: paymentUrl, method: 'post' }, { logRequestBody: true })
-// };
 
 const mock = RequestMock()
     .onRequestTo(request => request.url === countriesDatasetUrl)
