66c698ab5e0feaaf154cffc4934beed132ecf7f5
guilhermer <guilherme.ribeiro@adyen.com>
2022-03-09 13:05:18 +0100

    feat: e2e test for drop-in

--- packages/e2e/tests/cards/availableBrands/dropin/availableBrands.compactView.test.js
+++ packages/e2e/tests/cards/availableBrands/dropin/availableBrands.compactView.test.js
@@ -1,37 +1,33 @@
 import { Selector } from 'testcafe';
 import { DROPIN_SESSIONS_URL } from '../../../pages';
-import { mock, loggers, MOCK_SESSION_DATA } from './availableBrands.mocks';
-import { getIframeSelector } from '../../../utils/commonUtils';
-import cu from '../../utils/cardUtils';
-import AddressComponent from '../../../_models/Address.component';
+import { mock, loggers } from './availableBrands.mocks';
 import DropinPage from '../../../_models/Dropin.page';
-// import { TEST_CVC_VALUE } from '../../utils/constants';
 
-const iframeSelector = getIframeSelector('.adyen-checkout__payment-method--card iframe');
-const cardUtils = cu(iframeSelector);
 const { setupLogger, paymentLogger } = loggers;
 
 let dropinPage = null;
 
-fixture.only`Cards - Available Brands on Drop-in`
+fixture`Cards - Available Brands (Compact view)`
     .page(DROPIN_SESSIONS_URL)
     .requestHooks([mock, setupLogger, paymentLogger])
+    .clientScripts('availableBrands.clientScripts.js')
     .beforeEach(() => {
         dropinPage = new DropinPage({});
     });
 
-test('Card brands show on the header of the payment method item', async t => {
+test('Not all brands show on the header of the payment method item', async t => {
     const paymentItem = dropinPage.getPaymentMethodItemSelector('Credit Card');
 
     await t.expect(paymentItem.hasBrands).ok();
     await t.expect(paymentItem.extraBrandsText).eql('+7');
     await t.expect(paymentItem.numberOfBrandImages).eql(3);
+});
 
+test('Clicking on the Payment method, brands disappear from header and show beneath Card Number', async t => {
+    const paymentItem = dropinPage.getPaymentMethodItemSelector('Credit Card');
     await paymentItem.click();
 
     await t.expect(paymentItem.hasBrands).notOk();
-
     const brandsInsidePaymentMethod = Selector('.adyen-checkout__card__brands');
     await t.expect(brandsInsidePaymentMethod.find('img').count).eql(10);
-    // await t.debug();
 });
