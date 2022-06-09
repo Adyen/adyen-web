9ed47396dd3d812264e685e6cae0be1d6b39787a
guilhermer <guilherme.ribeiro@adyen.com>
2022-06-09 14:06:33 +0200

    fix: removing ids

--- packages/playground/src/pages/Dropin/session.js
+++ packages/playground/src/pages/Dropin/session.js
@@ -46,44 +46,17 @@ export async function initSession() {
                     schemes: {
                         discovery: '',
                         visa: {
-                            srciTransactionId: 'adyen-id-290202020',
-                            srcInitiatorId: 'B9SECVKIQX2SOBQ6J9X721dVBBKHhJJl1nxxVbemHGn5oB6S8',
-                            srciDpaId: '8e6e347c-254e-863f-0e6a-196bf2d9df02',
-                            dpaData: {
-                                //  "srcDpaId": "8e6e347c-254e-863f-0e6a-196bf2d9df02",
-                                dpaPresentationName: 'Adyen Visa Click to Play Sandbox',
-                                dpaUri: 'https://www.adyen.com',
-                                dpaThreeDsPreference: 'UNKNOWN'
-                            },
+                            srciTransactionId: 'adyen-id-' + new Date().getTime(),
+                            srcInitiatorId: 'xxxxxxxx',
+                            srciDpaId: 'xxxxxxxx',
                             dpaTransactionOptions: {
-                                // Start
-                                // Mandatory to specify:
-                                payloadTypeIndicator: 'NON_PAYMENT',
-                                // End
                                 dpaLocale: 'en_US',
-                                dpaAcceptedBillingCountries: ['US', 'CA', 'NL'],
-                                dpaAcceptedShippingCountries: ['US', 'CA', 'NL'],
-                                dpaBillingPreference: 'ALL',
-                                dpaShippingPreference: 'ALL',
-                                consumerNameRequested: true,
-                                consumerEmailAddressRequested: true,
-                                consumerPhoneNumberRequested: true,
-                                paymentOptions: {
-                                    dynamicDataType: 'TAVV',
-                                    dpaPanRequested: false
-                                },
-                                reviewAction: 'continue',
-                                checkoutDescription: 'Sample checkout',
-                                transactionType: 'PURCHASE',
-                                orderType: 'REAUTHORIZATION',
-                                //  "merchantOrderId": "order-id-" + txId,
-                                merchantCategoryCode: '5734',
-                                merchantCountryCode: 'US'
+                                payloadTypeIndicator: 'NON_PAYMENT'
                             }
                         },
                         mc: {
-                            srcInitiatorId: '6d41d4d6-45b1-42c3-a5d0-a28c0e69d4b1',
-                            srciDpaId: '6d41d4d6-45b1-42c3-a5d0-a28c0e69d4b1_dpa2',
+                            srcInitiatorId: 'xxxxxxxx',
+                            srciDpaId: 'xxxxxxxx',
                             srciTransactionId: 'adyen-id-' + new Date().getTime(),
                             dpaTransactionOptions: {
                                 dpaLocale: 'en_US',
