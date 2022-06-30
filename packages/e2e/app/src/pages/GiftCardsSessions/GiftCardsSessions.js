4af176dabc8082af5eca21b30a0a805a79ef1020
antoniof <antonio.ferreira@adyen.com>
2022-06-30 17:04:19 +0200

    adds e2e for onOrderCreated callback

---
+++ packages/e2e/app/src/pages/GiftCardsSessions/GiftCardsSessions.js
@@ -0,0 +1,49 @@
+import AdyenCheckout from '@adyen/adyen-web';
+import '@adyen/adyen-web/dist/es/adyen.css';
+import { checkBalance, createOrder, createSession } from '../../services';
+import { amount, shopperLocale, countryCode, returnUrl, shopperReference } from '../../services/commonConfig';
+import '../../style.scss';
+
+const initCheckout = async () => {
+    const session = await createSession({
+        amount,
+        reference: 'ABC123',
+        returnUrl,
+        shopperLocale,
+        shopperReference,
+        countryCode,
+    });
+
+    const sessionCheckout = await AdyenCheckout({
+        environment: 'test',
+        clientKey: process.env.__CLIENT_KEY__,
+        showPayButton: true,
+        session,
+
+        // Events
+        beforeSubmit: (data, component, actions) => {
+            actions.resolve(data);
+        },
+        onPaymentCompleted: (result, component) => {
+            console.info(result, component);
+        },
+        onError: (error, component) => {
+            console.log('arg', error);
+            console.error(error.message, component);
+        },
+    });
+
+    window.card = sessionCheckout
+        .create('giftcard', {
+            type: 'giftcard',
+            brand: 'valuelink',
+            onOrderCreated: async (resolve, reject, data) => {
+                console.log(data);
+                window.onOrderCreatedTestData = data;
+                resolve();
+            }
+        })
+        .mount('.card-field');
+};
+
+initCheckout();
