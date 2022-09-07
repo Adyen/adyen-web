6c0984b160e4e280080f580bedd9b6a4d215a73f
Thomas Maurstad Larsson <thomas.m.larsson@gmail.com>
2022-09-07 13:52:15 +0200

    Adds support for changing shopperLocale via query params

--- packages/e2e/app/src/services/commonConfig.js
+++ packages/e2e/app/src/services/commonConfig.js
@@ -5,7 +5,7 @@ const DEFAULT_LOCALE = 'en-US';
 const DEFAULT_COUNTRY = 'US';
 
 const urlParams = getSearchParameters(window.location.search);
-export const shopperLocale = DEFAULT_LOCALE;
+export const shopperLocale = urlParams.shopperLocale || DEFAULT_LOCALE;
 export const countryCode = urlParams.countryCode || DEFAULT_COUNTRY;
 export const currency = getCurrency(countryCode);
 export const amountValue = urlParams.amount || 25900;
