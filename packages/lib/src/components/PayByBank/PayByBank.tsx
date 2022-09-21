fc7492011828e38defe4dc1c794d5bc1d92d5aac
antoniof <antonio.ferreira@adyen.com>
2022-09-21 15:53:49 +0200

    adds support for paybybank to issuer list

---
+++ packages/lib/src/components/PayByBank/PayByBank.tsx
@@ -0,0 +1,12 @@
+import IssuerListContainer from '../helpers/IssuerListContainer';
+
+class PayByBank extends IssuerListContainer {
+    public static type = 'paybybank';
+
+
+    constructor(props) {
+        super({ ...props, showPaymentMethodItemImages: true });
+    }
+}
+
+export default PayByBank;
