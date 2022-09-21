7b187fd2f74c9dbf0f75c7ce90e32a49b74dd1f6
nicholas <nicholas.spong@adyen.com>
2022-09-21 13:46:12 +0200

    Added e2e tests to ensure excludedBrands are removed from (card) PaymentMethodItem in Dropin; and from brands shown under Card Number

--- packages/e2e/tests/cards/availableBrands/dropin/availableBrands.compactView.excluded.test.js
+++ packages/e2e/tests/cards/availableBrands/dropin/availableBrands.compactView.excluded.test.js
@@ -5,26 +5,28 @@ import DropinPage from '../../../_models/Dropin.page';
 
 let dropinPage = null;
 
-fixture`Cards - Available Brands (Compact view)`
+fixture`Cards - Available Brands ("with" excluded brands) (Compact view)`
     .page(DROPIN_SESSIONS_URL)
-    .requestHooks([getMock()])
+    .requestHooks([getMock('setupResponseWithExcludedBrands')])
     .beforeEach(() => {
         dropinPage = new DropinPage({});
     });
 
-test('Not all brands show on the header of the payment method item', async t => {
+test('#1 Not all brands show on the header of the payment method item, and excluded items are also removed', async t => {
+    await dropinPage.brandsHolder();
     const paymentItem = dropinPage.getPaymentMethodItemSelector('Credit Card');
 
     await t.expect(paymentItem.hasBrands).ok();
-    await t.expect(paymentItem.extraBrandsText).eql('+7');
+    await t.expect(paymentItem.extraBrandsText).eql('+3');
     await t.expect(paymentItem.numberOfBrandImages).eql(3);
 });
 
-test('Clicking on the Payment method, brands disappear from header and show beneath Card Number', async t => {
+test('#2 Clicking on the Payment method, brands disappear from header and show beneath Card Number (with excluded brands absent)', async t => {
+    await dropinPage.brandsHolder();
     const paymentItem = dropinPage.getPaymentMethodItemSelector('Credit Card');
     await paymentItem.click();
 
     await t.expect(paymentItem.hasBrands).notOk();
     const brandsInsidePaymentMethod = Selector('.adyen-checkout__card__brands');
-    await t.expect(brandsInsidePaymentMethod.find('img').count).eql(10);
+    await t.expect(brandsInsidePaymentMethod.find('img').count).eql(6);
 });
