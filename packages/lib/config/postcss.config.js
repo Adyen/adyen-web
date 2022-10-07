9190a7f4dfeb330a8e2b6ee8d4d10b16ae055d5c
guilhermer <guilherme.ribeiro@adyen.com>
2022-10-07 18:24:10 +0200

    feat: moved file to root

--- packages/lib/config/postcss.config.js
+++ packages/lib/config/postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     plugins: [
-        require('stylelint')({ configFile: 'config/stylelint.config.js' }),
+        require('stylelint')({ configFile: '../stylelint.config.js' }),
         require('postcss-reporter'),
         require('autoprefixer'),
         require('cssnano')({ preset: ['default', { colormin: false }] })
--- packages/lib/stylelint.config.js
+++ packages/lib/stylelint.config.js
@@ -4,6 +4,7 @@ module.exports = {
         'color-function-notation': 'legacy',
         'no-descending-specificity': null,
         'selector-class-pattern': null,
-        'max-line-length': 180
+        'max-line-length': 180,
+        'rule-empty-line-before': 'always'
     }
 };
