9114a1b9f6f57a0cac3f634fa1e10d572e4713b4
jobsturm <job.sturm@adyen.com>
2022-11-01 10:48:22 +0100

    Added QR code component for Paynow - untested

---
+++ packages/lib/src/components/PayNow/PayNow.test.ts
@@ -0,0 +1,26 @@
+import PayNow from './PayNow';
+
+describe('PayNow', () => {
+    describe('formatProps', () => {});
+
+    describe('isValid', () => {
+        test('should be always true', () => {
+            const paynow = new PayNow({});
+            expect(paynow.isValid).toBe(true);
+        });
+    });
+
+    describe('get data', () => {
+        test('always returns a type', () => {
+            const paynow = new PayNow({});
+            expect(paynow.data.paymentMethod.type).toBe('paynow');
+        });
+    });
+
+    describe('render', () => {
+        test('does not render anything by default', () => {
+            const paynow = new PayNow({});
+            expect(paynow.render()).toBe(null);
+        });
+    });
+});
