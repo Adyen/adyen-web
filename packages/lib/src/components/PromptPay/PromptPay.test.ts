f104a6049ad6a3f2982f4a531f9e62b267e42c76
jobsturm <job.sturm@adyen.com>
2022-11-07 15:30:20 +0100

    Fixd test description, added right block back

--- packages/lib/src/components/PromptPay/PromptPay.test.ts
+++ packages/lib/src/components/PromptPay/PromptPay.test.ts
@@ -1,24 +1,24 @@
 import PromptPay from './PromptPay';
 
-describe('formatProps', () => {});
-
-describe('isValid', () => {
-    test('should always be true', () => {
-        const promptPay = new PromptPay({});
-        expect(promptPay.isValid).toBe(true);
+describe('PromptPay', () => {
+    describe('isValid', () => {
+        test('should always be true', () => {
+            const promptPay = new PromptPay({});
+            expect(promptPay.isValid).toBe(true);
+        });
     });
-});
 
-describe('get data', () => {
-    test('always returns a type', () => {
-        const promptPay = new PromptPay({});
-        expect(promptPay.data.paymentMethod.type).toBe('promptpay');
+    describe('get data', () => {
+        test('always returns a type', () => {
+            const promptPay = new PromptPay({});
+            expect(promptPay.data.paymentMethod.type).toBe('promptpay');
+        });
     });
-});
 
-describe('render', () => {
-    test('does not render anything by default', () => {
-        const promptPay = new PromptPay({});
-        expect(promptPay.render()).toBe(null);
+    describe('render', () => {
+        test('does not render anything by default', () => {
+            const promptPay = new PromptPay({});
+            expect(promptPay.render()).toBe(null);
+        });
     });
 });
