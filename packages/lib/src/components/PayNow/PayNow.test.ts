d1056f092704d3c06d5d96ad6a4aa475f405b2dc
jobsturm <job.sturm@adyen.com>
2022-11-07 15:28:24 +0100

    Fixed MR feedback for Paynow

--- packages/lib/src/components/PayNow/PayNow.test.ts
+++ packages/lib/src/components/PayNow/PayNow.test.ts
@@ -1,25 +1,25 @@
 import PayNow from './PayNow';
 
-describe('formatProps', () => {});
-
-describe('isValid', () => {
-    test('should be always true', () => {
-        const paynow = new PayNow({});
-        expect(paynow.isValid).toBe(true);
+describe('PayNow', () => {
+    describe('isValid', () => {
+        test('should be always true', () => {
+            const paynow = new PayNow({});
+            expect(paynow.isValid).toBe(true);
+        });
     });
-});
 
-describe('get data', () => {
-    test('always returns a type', () => {
-        const paynow = new PayNow({});
-        expect(paynow.data.paymentMethod.type).toBe('paynow');
+    describe('get data', () => {
+        test('always returns a type', () => {
+            const paynow = new PayNow({});
+            expect(paynow.data.paymentMethod.type).toBe('paynow');
+        });
     });
-});
 
-describe('render', () => {
-    test('does not render anything by default', () => {
-        const paynow = new PayNow({});
-        expect(paynow.render()).toBe(null);
+    describe('render', () => {
+        test('does not render anything by default', () => {
+            const paynow = new PayNow({});
+            expect(paynow.render()).toBe(null);
+        });
     });
 });
 
