63dbe2315b01c4f95f3bf7c281e3b3e4806c646e
Job Sturm <jobsturm@gmail.com>
2022-11-28 15:10:49 +0100

    Removed auto redirect from QR code loader

--- packages/lib/src/components/internal/QRLoader/QRLoader.tsx
+++ packages/lib/src/components/internal/QRLoader/QRLoader.tsx
@@ -54,24 +54,8 @@ class QRLoader extends Component<QRLoaderProps, QRLoaderState> {
     };
 
     componentDidMount() {
-        const { shouldRedirectOnMobile, url } = this.props;
-        const isMobile = window.matchMedia('(max-width: 768px)').matches && /Android|iPhone|iPod/.test(navigator.userAgent);
-
-        const startPolling = () => {
-            this.interval = setInterval(this.statusInterval, this.state.delay);
-        };
-
-        if (shouldRedirectOnMobile && url && isMobile) {
-            this.redirectToApp(url, startPolling);
-        } else {
-            startPolling();
-        }
+        this.interval = setInterval(this.statusInterval, this.state.delay);
     }
-
-    public redirectToApp = (url, fallback = () => {}) => {
-        setTimeout(fallback, 1000);
-        window.location.assign(url);
-    };
 
     componentDidUpdate(prevProps, prevState) {
         if (prevState.delay !== this.state.delay) {
@@ -219,7 +203,6 @@ class QRLoader extends Component<QRLoaderProps, QRLoaderState> {
                         />
                     </div>
                 )}
-
                 {url && (
                     <div className="adyen-checkout__qr-loader__app-link">
                         <ContentSeparator />
