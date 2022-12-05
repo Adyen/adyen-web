225d93bea4a8bac0853cbc2c492dfcd59bf9926e
nicholas <nicholas.spong@adyen.com>
2022-12-05 11:34:00 +0100

    PersonalDetails component - fixed validation & aria-required attributes

--- packages/lib/src/components/internal/FormFields/InputText.tsx
+++ packages/lib/src/components/internal/FormFields/InputText.tsx
@@ -2,5 +2,5 @@ import { h } from 'preact';
 import InputBase from './InputBase';
 
 export default function InputText(props) {
-    return <InputBase classNameModifiers={['large']} {...props} aria-required={props.required} type="text" />;
+    return <InputBase classNameModifiers={['large']} {...props} type="text" />;
 }
