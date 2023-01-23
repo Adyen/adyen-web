828dac48e28296963137687a82bb9e7f5a61d37f
nicholas <nicholas.spong@adyen.com>
2023-01-23 12:42:15 +0100

    SRPanel uses functional comp, SRMessages, to show/hide messages

--- packages/lib/src/core/Errors/SRPanel.tsx
+++ packages/lib/src/core/Errors/SRPanel.tsx
@@ -2,7 +2,6 @@ import { h } from 'preact';
 import './SRPanel.scss';
 import { SRPanelProps } from './types';
 import BaseElement from '../../components/BaseElement';
-// import { objectsDeepEqual } from '../../components/internal/SecuredFields/lib/utilities/commonUtils';
 import { SRMessages, SRMessagesRef } from './SRMessages';
 
 /**
@@ -21,8 +20,6 @@ export class SRPanel extends BaseElement<SRPanelProps> {
 
     private readonly id;
     private readonly showPanel;
-    // private msgPanel;
-    // private msgHolder;
 
     private componentRef: SRMessagesRef;
 
@@ -32,16 +29,12 @@ export class SRPanel extends BaseElement<SRPanelProps> {
         this.showPanel = process.env.NODE_ENV !== 'production' ? this.props.showPanel : false;
         this.state = { panelMessages: null };
 
-        console.log('### SRPanel::constructor:: this.props=', this.props);
-
         if (this.props.enabled) {
             if (document.querySelector(this.props.node)) {
                 this.srPanelContainer = document.createElement('div');
                 this.srPanelContainer.className = 'sr-panel-holder';
                 document.querySelector(this.props.node).appendChild(this.srPanelContainer);
                 this.mount(this.srPanelContainer);
-
-                // this.msgPanel = document.getElementById('ariaLiveSRPanel');
             } else {
                 throw new Error('Component could not mount. Root node was not found.');
             }
@@ -62,44 +55,10 @@ export class SRPanel extends BaseElement<SRPanelProps> {
             panelMessages = Array.isArray(messages) ? messages : [messages];
         }
 
-        // const oldMessages = this.state.panelMessages;
-        //
-        // this.setState({ panelMessages });
-        //
-        // // "re-render" if we have a new set of messages
-        // const hasNewMessages = !objectsDeepEqual(oldMessages, panelMessages);
-        // if (hasNewMessages) {
-        //     this.createMessageElements(); // TODO - don't need any of this, nor, probably, do we need to store panelMessages on this.state (check we no longer need to check if messages have changed to prevent too many renders)
-
-        this.componentRef.setMessages(panelMessages); // panelMessages
-        // }
+        this.componentRef.setMessages(panelMessages);
     };
 
-    // public createMessageElements() {
-    //     // Remove old holder
-    //     if (this.msgHolder) {
-    //         this.msgPanel.removeChild(this.msgHolder);
-    //         this.msgHolder = null;
-    //     }
-    //
-    //     // If we have new messages - create them and add them to the holder
-    //     if (this.state.panelMessages) {
-    //         this.msgHolder = document.createElement('div');
-    //         this.msgHolder.className = 'adyen-checkout-sr-panel__msg-holder';
-    //
-    //         this.state.panelMessages.forEach(msg => {
-    //             const msgDiv = document.createElement('div');
-    //             msgDiv.className = 'adyen-checkout-sr-panel__msg';
-    //             msgDiv.innerText = msg;
-    //             this.msgHolder.appendChild(msgDiv);
-    //         });
-    //
-    //         this.msgPanel.appendChild(this.msgHolder);
-    //     }
-    // }
-
     render() {
-        console.log('### SRPanel::render:: ');
         return (
             <div
                 className={this.showPanel ? 'adyen-checkout-sr-panel' : 'adyen-checkout-sr-panel--sr-only'}
