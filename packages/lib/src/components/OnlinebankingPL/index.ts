eb34009b878bed62ecc15252416117f16be6b24f
Guilherme Ribeiro <guilhermemrr@gmail.com>
2023-01-27 10:55:06 +0100

    Enabling issuer logos for Online Banking PL (#1962)

--- packages/lib/src/components/OnlinebankingPL/index.ts
+++ packages/lib/src/components/OnlinebankingPL/index.ts
@@ -2,13 +2,6 @@ import IssuerListContainer from '../helpers/IssuerListContainer';
 
 class OnlineBankingPL extends IssuerListContainer {
     public static type = 'onlineBanking_PL';
-
-    formatProps(props) {
-        return {
-            ...super.formatProps(props),
-            showImage: false
-        };
-    }
 }
 
 export default OnlineBankingPL;
