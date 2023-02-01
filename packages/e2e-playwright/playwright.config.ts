1e45ad17c278fef4a632c32bf5dfb50f7b3efc4a
Cenk Kucukiravul <cenkiravul35@gmail.com>
2023-02-01 13:01:00 +0100

    Hardcode 4 parallel runners for local runs

--- packages/e2e-playwright/playwright.config.ts
+++ packages/e2e-playwright/playwright.config.ts
@@ -32,7 +32,7 @@ const config: PlaywrightTestConfig = {
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 4,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [ ['list'] ],
