a3f9257759982a7d88c799af7659473431d3ddee
Cenk Kucukiravul <cenk.kucukiravul@adyen.com>
2023-02-07 13:13:31 +0100

    Use 2 workers on CI

--- packages/e2e-playwright/playwright.config.ts
+++ packages/e2e-playwright/playwright.config.ts
@@ -32,7 +32,7 @@ const config: PlaywrightTestConfig = {
   /* Retry on CI only */
   retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 4,
+  workers: process.env.CI ? 2 : 4,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [["html", { open: "never" }]],
