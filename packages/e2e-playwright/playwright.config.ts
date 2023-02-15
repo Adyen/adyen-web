7e34e6a463fe7f005291956f407b007930c745c6
guilhermer <guilherme.ribeiro@adyen.com>
2023-02-15 15:45:15 +0100

    feat: adjusted timeouts

--- packages/e2e-playwright/playwright.config.ts
+++ packages/e2e-playwright/playwright.config.ts
@@ -6,18 +6,12 @@ import * as path from 'path';
 dotenv.config({ path: path.resolve('../../', '.env') });
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 const config: PlaywrightTestConfig = {
     testDir: './tests',
     /* Maximum time one test can run for. */
-    timeout: 10 * 1500,
+    timeout: 10 * 2000,
     expect: {
         /**
          * Maximum time expect() should wait for the condition to be met.
@@ -40,7 +34,7 @@ const config: PlaywrightTestConfig = {
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-        actionTimeout: 20000,
+        actionTimeout: 15000,
         /* Base URL to use in actions like `await page.goto('/')`. */
         // baseURL: 'http://localhost:3000',
 
