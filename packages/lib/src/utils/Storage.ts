bfcafbebf33c07e976cac897558945ab0ff44b41
nicholas <nicholas.spong@adyen.com>
2023-03-08 17:27:50 +0100

    When local storage isn't available e.g. when window is in incognito mode, fall back to a non-persistent storage solution

--- packages/lib/src/utils/Storage.ts
+++ packages/lib/src/utils/Storage.ts
@@ -1,10 +1,42 @@
+class NonPersistentStorage {
+    private storage;
+
+    constructor() {
+        this.storage = {};
+    }
+
+    get length() {
+        return Object.keys(this.storage).length;
+    }
+
+    key(keyName) {
+        return Object.keys(this.storage).indexOf(keyName);
+    }
+    getItem(keyName) {
+        return this.storage[keyName] || null;
+    }
+    setItem(keyName, keyValue) {
+        return (this.storage[keyName] = keyValue);
+    }
+    removeItem(keyName) {
+        delete this.storage[keyName];
+    }
+    clear() {
+        this.storage = {};
+    }
+}
+
 class Storage<T> {
     private readonly prefix = 'adyen-checkout__';
     private readonly key: string;
-    private storage: globalThis.Storage;
+    private storage: globalThis.Storage | NonPersistentStorage;
 
-    constructor(key: string, storage = window.localStorage) {
-        this.storage = storage;
+    constructor(key: string, storageType: 'sessionStorage' | 'localStorage') {
+        try {
+            this.storage = storageType ? window[storageType] : window.localStorage;
+        } catch (e) {
+            this.storage = new NonPersistentStorage();
+        }
         this.key = this.prefix + key;
     }
 
