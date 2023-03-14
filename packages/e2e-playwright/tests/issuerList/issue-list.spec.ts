0094fc5c54f187ca9cfcb80897386e8d42e07b36
guilhermer <guilherme.ribeiro@adyen.com>
2023-03-14 13:56:17 +0100

    fix: test

--- packages/e2e-playwright/tests/issuerList/issue-list.spec.ts
+++ packages/e2e-playwright/tests/issuerList/issue-list.spec.ts
@@ -3,6 +3,8 @@ import { test, expect } from '../../pages/issuerList/issuer-list.fixture';
 test('should select issuers on dropdown and update pay button label', async ({ issuerListPage }) => {
     const { issuerList } = issuerListPage;
 
+    await expect(issuerList.submitButton).toHaveText('Continue');
+
     await issuerList.selectIssuerOnSelectorDropdown('Test Issuer 5');
     await expect(issuerList.submitButton).toHaveText('Continue to Test Issuer 5');
 
