a89890791ab24bfb5b9d8d2c2717d0ddb137acc8
guilhermer <guilherme.ribeiro@adyen.com>
2023-04-11 15:46:13 +0200

    feat: removed unused value

--- packages/e2e-playwright/tests/utils/constants.ts
+++ packages/e2e-playwright/tests/utils/constants.ts
@@ -8,4 +8,3 @@ export const TEST_CVC_VALUE = '737';
 export const BIN_LOOKUP_URL = `https://checkoutshopper-test.adyen.com/checkoutshopper/${BIN_LOOKUP_VERSION}/bin/binLookup?token=${process.env.CLIENT_KEY}`;
 
 export const USER_TYPE_DELAY = 150;
-export const SELECTOR_DELAY = 500;
