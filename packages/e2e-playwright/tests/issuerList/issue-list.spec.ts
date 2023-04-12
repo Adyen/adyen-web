63e85120bcefb5e8a5e3cceef0d23f9ed58257df
Guilherme Ribeiro <guilhermemrr@gmail.com>
2023-04-12 14:23:07 +0200

    E2E - Fixing issuer list flaky tests (#2108)
    * feat: attempt to fix flakyness
    
    * feat: removed unused value

--- packages/e2e-playwright/tests/issuerList/issue-list.spec.ts
+++ packages/e2e-playwright/tests/issuerList/issue-list.spec.ts
@@ -1,17 +1,5 @@
 import { test, expect } from '../../pages/issuerList/issuer-list.fixture';
 
-test('should select issuers on dropdown and update pay button label', async ({ issuerListPage }) => {
-    const { issuerList } = issuerListPage;
-
-    await expect(issuerList.submitButton).toHaveText('Continue');
-
-    await issuerList.selectIssuerOnSelectorDropdown('Test Issuer 5');
-    await expect(issuerList.submitButton).toHaveText('Continue to Test Issuer 5');
-
-    await issuerList.selectIssuerOnSelectorDropdown('Test Issuer');
-    await expect(issuerList.submitButton).toHaveText('Continue to Test Issuer');
-});
-
 test('should select highlighted issuer and update pay button label', async ({ issuerListPage }) => {
     const { issuerList } = issuerListPage;
 
@@ -28,15 +16,18 @@ test('should select highlighted issuer and update pay button label', async ({ is
 test('it should be able to filter and select using the keyboard', async ({ issuerListPage }) => {
     const { issuerList } = issuerListPage;
 
-    await issuerList.typeToFilterTerm('');
+    await expect(issuerList.submitButton).toHaveText('Continue');
+
+    await issuerList.clickOnSelector();
     await expect(issuerList.selectorList).toContainText('SNS');
 
-    await issuerList.typeToFilterTerm('Test');
+    await issuerList.typeOnSelectorField('Test');
     await expect(issuerList.selectorList).not.toContainText('SNS');
 
     await issuerList.pressKeyboardToNextItem();
     await issuerList.pressKeyboardToNextItem();
     await issuerList.pressKeyboardToSelectItem();
+
     await expect(issuerList.submitButton).toHaveText('Continue to Test Issuer 5');
 
     // 1st press opens the dropdown
@@ -44,13 +35,16 @@ test('it should be able to filter and select using the keyboard', async ({ issue
     // 2nd selects next items
     await issuerList.pressKeyboardToNextItem();
     await issuerList.pressKeyboardToSelectItem();
+
     await expect(issuerList.submitButton).toHaveText('Continue to Test Issuer 4');
 });
 
 test('it should load a default when pressing enter', async ({ issuerListPage }) => {
     const { issuerList } = issuerListPage;
 
-    await issuerList.typeToFilterTerm('Test');
+    await issuerList.clickOnSelector();
+    await issuerList.typeOnSelectorField('Test');
     await issuerList.pressKeyboardToSelectItem();
+
     await expect(issuerList.submitButton).toHaveText('Continue to Test Issuer');
 });
