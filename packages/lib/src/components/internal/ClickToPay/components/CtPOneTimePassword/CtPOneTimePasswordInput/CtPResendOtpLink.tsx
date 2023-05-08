79fe89597112ed5738ed15e619f6bd90545b81dc
guilhermer <guilherme.ribeiro@adyen.com>
2023-05-08 10:28:14 -0300

    feat: moved ctp to internal folder

---
+++ packages/lib/src/components/internal/ClickToPay/components/CtPOneTimePassword/CtPOneTimePasswordInput/CtPResendOtpLink.tsx
@@ -0,0 +1,89 @@
+import { h } from 'preact';
+import { useCallback, useEffect, useState } from 'preact/hooks';
+import useClickToPayContext from '../../../context/useClickToPayContext';
+import classnames from 'classnames';
+import useCoreContext from '../../../../../../core/Context/useCoreContext';
+import Icon from '../../../../Icon';
+
+const CONFIRMATION_SHOWING_TIME = 2000;
+
+interface CtPResendOtpLinkProps {
+    onError(errorCode: string): void;
+    onResendCode(): void;
+    disabled: boolean;
+}
+
+const CtPResendOtpLink = ({ onError, onResendCode, disabled }: CtPResendOtpLinkProps): h.JSX.Element => {
+    const [counter, setCounter] = useState<number>(null);
+    const [showConfirmation, setShowConfirmation] = useState<boolean>(false);
+    const { i18n } = useCoreContext();
+    const { startIdentityValidation } = useClickToPayContext();
+
+    useEffect(() => {
+        let timeout = null;
+        if (counter > 0) {
+            timeout = setTimeout(() => setCounter(counter - 1), 1000);
+        }
+        return () => clearTimeout(timeout);
+    }, [counter]);
+
+    useEffect(() => {
+        let timeout = null;
+
+        if (showConfirmation) {
+            timeout = setTimeout(() => {
+                setShowConfirmation(false);
+                setCounter(60);
+            }, CONFIRMATION_SHOWING_TIME);
+        }
+        return () => clearTimeout(timeout);
+    }, [showConfirmation]);
+
+    const handleResendCodeClick = useCallback(
+        async event => {
+            event.preventDefault();
+
+            try {
+                onResendCode();
+                setShowConfirmation(true);
+                await startIdentityValidation();
+            } catch (error) {
+                onError(error.reason);
+                setCounter(0);
+                setShowConfirmation(false);
+            }
+        },
+        [startIdentityValidation, onError, onResendCode]
+    );
+
+    if (showConfirmation) {
+        return (
+            <div className="adyen-checkout-ctp__otp-resend-code--confirmation">
+                {i18n.get('ctp.otp.codeResent')}
+                <Icon type="checkmark" height={14} width={14} />
+            </div>
+        );
+    }
+
+    if (counter > 0) {
+        return (
+            <div className="adyen-checkout-ctp__otp-resend-code--disabled">
+                {i18n.get('ctp.otp.resendCode')} -{' '}
+                <span className="adyen-checkout-ctp__otp-resend-code-counter"> {counter > 0 && `${counter}s`} </span>
+            </div>
+        );
+    }
+
+    return (
+        <div
+            role="link"
+            tabIndex={0}
+            className={classnames('adyen-checkout-ctp__otp-resend-code', { 'adyen-checkout-ctp__otp-resend-code--disabled': disabled })}
+            onClick={handleResendCodeClick}
+        >
+            {i18n.get('ctp.otp.resendCode')}
+        </div>
+    );
+};
+
+export default CtPResendOtpLink;
--- packages/playground/src/pages/Cards/Cards.js
+++ packages/playground/src/pages/Cards/Cards.js
@@ -174,12 +174,12 @@ getPaymentMethods({ amount, shopperLocale }).then(async paymentMethodsResponse =
                 type: 'scheme',
                 brands: ['mc', 'visa'],
                 configuration: {
-                    mcDpaId: '6d41d4d6-45b1-42c3-a5d0-a28c0e69d4b1_dpa2',
-                    mcSrcClientId: '6d41d4d6-45b1-42c3-a5d0-a28c0e69d4b1'
+                    visaSrciDpaId: '8e6e347c-254e-863f-0e6a-196bf2d9df02',
+                    visaSrcInitiatorId: 'B9SECVKIQX2SOBQ6J9X721dVBBKHhJJl1nxxVbemHGn5oB6S8'
                 },
                 clickToPayConfiguration: {
                     disableOtpAutoFocus: true,
-                    shopperEmail: 'gui.ctp@adyen.com',
+                    shopperEmail: 'guilherme.ribeiro-ctp1@adyen.com',
                     merchantDisplayName: 'Adyen Merchant Name '
                 }
             })
