99b39b0852cd79233ccbee863db890971e0caa49
guilhermer <guilherme.ribeiro@adyen.com>
2023-05-08 10:33:39 -0300

    feat: default card.js

--- packages/playground/src/pages/Cards/Cards.js
+++ packages/playground/src/pages/Cards/Cards.js
@@ -174,12 +174,12 @@ getPaymentMethods({ amount, shopperLocale }).then(async paymentMethodsResponse =
                 type: 'scheme',
                 brands: ['mc', 'visa'],
                 configuration: {
-                    visaSrciDpaId: '8e6e347c-254e-863f-0e6a-196bf2d9df02',
-                    visaSrcInitiatorId: 'B9SECVKIQX2SOBQ6J9X721dVBBKHhJJl1nxxVbemHGn5oB6S8'
+                    mcDpaId: '6d41d4d6-45b1-42c3-a5d0-a28c0e69d4b1_dpa2',
+                    mcSrcClientId: '6d41d4d6-45b1-42c3-a5d0-a28c0e69d4b1'
                 },
                 clickToPayConfiguration: {
                     disableOtpAutoFocus: true,
-                    shopperEmail: 'guilherme.ribeiro-ctp1@adyen.com',
+                    shopperEmail: 'gui.ctp@adyen.com',
                     merchantDisplayName: 'Adyen Merchant Name '
                 }
             })
