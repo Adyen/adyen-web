0ba298e2d9e713a37b734f8558537062b259b738
guilhermer <guilherme.ribeiro@adyen.com>
2023-06-21 17:09:26 +0200

    feat: attempt with different rollup

--- packages/lib/config/rollup.config.js
+++ packages/lib/config/rollup.config.js
@@ -1,8 +1,19 @@
 import babel from '@rollup/plugin-babel';
-import { modernTerserConfig, terserConfig } from './terser.config';
-import pkg from '../package.json';
+// import { modernTerserConfig, terserConfig } from './terser.config';
+// import pkg from '../package.json';
 import path from 'path';
 import { extensions, getExternals, getPlugins, input, polyfillPlugin, polyfillPreset, watchConfig } from './rollup.base.config';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import stylelint from 'rollup-plugin-stylelint';
+import eslint from '@rollup/plugin-eslint';
+import replace from '@rollup/plugin-replace';
+import json from '@rollup/plugin-json';
+import { swc, defineRollupSwcOption } from 'rollup-plugin-swc3';
+import postcss from 'rollup-plugin-postcss';
+
+const currentVersion = require('./version')();
+require('dotenv').config({ path: path.resolve('../../', '.env') });
 
 async function getProdPlugins({ modern }) {
     const babelPlugins = modern ? [] : [polyfillPlugin];
@@ -19,8 +30,8 @@ async function getProdPlugins({ modern }) {
             babelHelpers: modern ? 'bundled' : 'runtime',
             plugins: babelPlugins,
             presets: babelPreset
-        }),
-        (await import('rollup-plugin-terser')).terser(modern ? modernTerserConfig : terserConfig)
+        })
+        // (await import('rollup-plugin-terser')).terser(modern ? modernTerserConfig : terserConfig)
     ];
 }
 
@@ -34,49 +45,118 @@ export default async () => {
     });
 
     return [
+        // {
+        //     input,
+        //     external: getExternals(),
+        //     plugins,
+        //     output: [
+        //         {
+        //             dir: 'dist/es',
+        //             format: 'es',
+        //             chunkFileNames: '[name].js'
+        //         },
+        //         {
+        //             dir: 'dist/cjs',
+        //             format: 'cjs',
+        //             exports: 'auto',
+        //             inlineDynamicImports: true
+        //         }
+        //     ],
+        //     watch: watchConfig
+        // },
+        // {
+        //     input,
+        //     external: getExternals(),
+        //     plugins: modernPlugins,
+        //     output: [
+        //         {
+        //             dir: 'dist/es.modern',
+        //             format: 'esm',
+        //             chunkFileNames: '[name].js'
+        //         }
+        //     ],
+        //     watch: watchConfig
+        // },
+        //ES6 build
         {
-            input,
-            external: getExternals(),
-            plugins,
-            output: [
-                {
-                    dir: 'dist/es',
-                    format: 'es',
-                    chunkFileNames: '[name].js'
-                },
-                {
-                    dir: 'dist/cjs',
-                    format: 'cjs',
-                    exports: 'auto',
-                    inlineDynamicImports: true
-                }
+            input: 'src/index.ts',
+            external: ['preact', 'classnames'],
+            // plugins: modernPlugins,
+            plugins: [
+                resolve({ extensions: ['.js', '.jsx', '.ts', '.tsx'] }),
+                commonjs(),
+                stylelint({ include: 'src/**/*.scss' }),
+                eslint({
+                    include: ['./src/**'],
+                    exclude: ['./src/**/*.json', './src/**/*.scss']
+                }),
+                replace({
+                    values: {
+                        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+                        'process.env.VERSION': JSON.stringify(currentVersion.ADYEN_WEB_VERSION),
+                        'process.env.COMMIT_HASH': JSON.stringify(currentVersion.COMMIT_HASH),
+                        'process.env.COMMIT_BRANCH': JSON.stringify(currentVersion.COMMIT_BRANCH),
+                        'process.env.ADYEN_BUILD_ID': JSON.stringify(currentVersion.ADYEN_BUILD_ID),
+                        'process.env.__SF_ENV__': JSON.stringify(process.env.SF_ENV || 'build')
+                    },
+                    preventAssignment: true
+                }),
+                json({ namedExports: false, compact: true, preferConst: true }),
+                postcss({
+                    use: ['sass'],
+                    config: {
+                        path: 'config/postcss.config.js'
+                    },
+                    sourceMap: true,
+                    inject: false,
+                    extract: 'adyen.css'
+                }),
+                swc(
+                    defineRollupSwcOption({
+                        tsconfig: '../tsconfssig.json',
+                        jsc: {
+                            target: 'es2022',
+                            parser: {
+                                syntax: 'typescript'
+                            },
+                            transform: {
+                                react: {
+                                    pragma: 'h',
+                                    pragmaFrag: 'Fragment'
+                                }
+                            }
+                        },
+                        module: {
+                            type: 'es6'
+                        },
+                        sourceMaps: true
+                    })
+                )
             ],
-            watch: watchConfig
-        },
-        {
-            input,
-            external: getExternals(),
-            plugins: modernPlugins,
             output: [
                 {
-                    dir: 'dist/es.modern',
+                    dir: './dist/esm',
+                    chunkFileNames: 'chunks/[name].js',
+                    entryFileNames: '[name].js',
                     format: 'esm',
-                    chunkFileNames: '[name].js'
+                    indent: false,
+                    sourcemap: true
+                    // preserveModules: true,
                 }
             ],
-            watch: watchConfig
-        },
-        {
-            input,
-            plugins,
-            output: {
-                name: 'AdyenCheckout',
-                file: pkg['umd:main'],
-                format: 'umd',
-                inlineDynamicImports: true,
-                sourcemap: true
-            },
             watch: watchConfig
         }
+        // {
+        //     input,
+        //     plugins,
+        //     output: {
+        //         name: 'AdyenCheckout',
+        //         file: pkg['umd:main'],
+        //         format: 'umd',
+        //         inlineDynamicImports: true,
+        //         sourcemap: true
+        //     },
+        //     watch: watchConfig
+        // }
     ];
 };
--- packages/lib/config/version.js
+++ packages/lib/config/version.js
@@ -1,5 +1,5 @@
-const childProcess = require('child_process');
-const packageJson = require('../package.json');
+import * as childProcess from 'child_process';
+import packageJson from '../package.json';
 
 function uuidv4() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
@@ -9,30 +9,45 @@ function uuidv4() {
     });
 }
 
-module.exports = () => {
-    let COMMIT_HASH = null;
-    let COMMIT_BRANCH = null;
-    const ADYEN_BUILD_ID = `@adyen/adyen-web-${uuidv4()}`;
+let COMMIT_HASH = null;
+let COMMIT_BRANCH = null;
+const ADYEN_BUILD_ID = `@adyen/adyen-web-${uuidv4()}`;
 
-    try {
-        COMMIT_HASH = childProcess
-            .execSync('git rev-parse --short HEAD')
-            .toString()
-            .trim();
-        COMMIT_BRANCH = childProcess
-            .execSync('git rev-parse --abbrev-ref HEAD')
-            .toString()
-            .trim();
+try {
+    COMMIT_HASH = childProcess.execSync('git rev-parse --short HEAD').toString().trim();
+    COMMIT_BRANCH = childProcess.execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
 
-        console.log(`Building version ${packageJson.version} (revision ${COMMIT_HASH} from branch ${COMMIT_BRANCH}). Build id ${ADYEN_BUILD_ID}`);
-    } catch (e) {
-        console.warn(e.message);
-    }
+    console.log(`Building version ${packageJson.version} (revision ${COMMIT_HASH} from branch ${COMMIT_BRANCH}). Build id ${ADYEN_BUILD_ID}`);
+} catch (e) {
+    console.warn(e.message);
+}
 
-    return {
-        ADYEN_WEB_VERSION: packageJson.version,
-        COMMIT_BRANCH,
-        COMMIT_HASH,
-        ADYEN_BUILD_ID
-    };
+export default {
+    ADYEN_WEB_VERSION: packageJson.version,
+    COMMIT_BRANCH,
+    COMMIT_HASH,
+    ADYEN_BUILD_ID
 };
+
+//
+// module.exports = () => {
+//     let COMMIT_HASH = null;
+//     let COMMIT_BRANCH = null;
+//     const ADYEN_BUILD_ID = `@adyen/adyen-web-${uuidv4()}`;
+//
+//     try {
+//         COMMIT_HASH = childProcess.execSync('git rev-parse --short HEAD').toString().trim();
+//         COMMIT_BRANCH = childProcess.execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+//
+//         console.log(`Building version ${packageJson.version} (revision ${COMMIT_HASH} from branch ${COMMIT_BRANCH}). Build id ${ADYEN_BUILD_ID}`);
+//     } catch (e) {
+//         console.warn(e.message);
+//     }
+//
+//     return {
+//         ADYEN_WEB_VERSION: packageJson.version,
+//         COMMIT_BRANCH,
+//         COMMIT_HASH,
+//         ADYEN_BUILD_ID
+//     };
+// };
