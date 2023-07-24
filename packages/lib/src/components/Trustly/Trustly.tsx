5a50114f4097d4dd89037443d7885a31ef8d9e31
Yu Long <longyu901009@gmail.com>
2023-07-24 17:12:32 +0200

    feat(trustly): add description

---
+++ packages/lib/src/components/Trustly/Trustly.tsx
@@ -0,0 +1,42 @@
+import { h } from 'preact';
+import CoreProvider from '../../core/Context/CoreProvider';
+import RedirectElement from '../Redirect';
+import RedirectButton from '../internal/RedirectButton';
+import './Trustly.scss';
+
+class TrustlyElement extends RedirectElement {
+    public static type = 'trustly';
+
+    get displayName() {
+        return this.props.name || this.constructor['type'];
+    }
+
+    render() {
+        if (this.props.showPayButton) {
+            return (
+                <CoreProvider i18n={this.props.i18n} loadingContext={this.props.loadingContext} resources={this.resources}>
+                    <div className="adyen-checkout-trustly">
+                        <p className="adyen-checkout-trustly__descriptor">{this.props.i18n.get('trustly.descriptor')}</p>
+                        <ul className="adyen-checkout-trustly__description-list">
+                            <li>{this.props.i18n.get('trustly.description1')}</li>
+                            <li>{this.props.i18n.get('trustly.description2')}</li>
+                        </ul>
+                    </div>
+
+                    <RedirectButton
+                        {...this.props}
+                        name={this.displayName}
+                        onSubmit={this.submit}
+                        payButton={this.payButton}
+                        ref={ref => {
+                            this.componentRef = ref;
+                        }}
+                    />
+                </CoreProvider>
+            );
+        }
+        return null;
+    }
+}
+
+export default TrustlyElement;
