4fc5311ec21e6e2c9de1e0fc35900c448c51b1d4
nicholas <nicholas.spong@adyen.com>
2023-08-10 16:51:11 +0200

    Bancontact card working in Dropin

--- packages/playground/src/pages/Dropin/manual.js
+++ packages/playground/src/pages/Dropin/manual.js
@@ -20,7 +20,7 @@ export async function initManual() {
                 values: [1, 2, 3, 4]
             }
         },
-        allowPaymentMethods: ['scheme'],
+        allowPaymentMethods: ['scheme', 'bcmc'],
         onSubmit: async (state, component) => {
             const result = await makePayment(state.data);
 
