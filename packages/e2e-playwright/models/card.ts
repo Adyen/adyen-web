16fb1b04efaa497adfa1063b4a011199df9aeba5
nicholas <nicholas.spong@adyen.com>
2023-09-15 14:43:48 +0200

    Migrated card's general test and legacyInputMode tests to Playwright & removed original testCafe versions

--- packages/e2e-playwright/models/card.ts
+++ packages/e2e-playwright/models/card.ts
@@ -1,21 +1,70 @@
 import { Locator, Page } from '@playwright/test';
 import { USER_TYPE_DELAY } from '../tests/utils/constants';
+import LANG from '../../lib/src/language/locales/en-US.json';
+
+const CARD_IFRAME_TITLE = LANG['creditCard.encryptedCardNumber.aria.iframeTitle'];
+const EXPIRY_DATE_IFRAME_TITLE = LANG['creditCard.encryptedExpiryDate.aria.iframeTitle'];
+const CVC_IFRAME_TITLE = LANG['creditCard.encryptedSecurityCode.aria.iframeTitle'];
+
+const CARD_IFRAME_LABEL = LANG['creditCard.encryptedCardNumber.aria.label'];
+const EXPIRY_DATE_IFRAME_LABEL = LANG['creditCard.encryptedExpiryDate.aria.label'];
+const CVC_IFRAME_LABEL = LANG['creditCard.encryptedSecurityCode.aria.label'];
 
 class Card {
     readonly rootElement: Locator;
     readonly rootElementSelector: string;
 
+    readonly cardNumberField: Locator;
+    readonly cardNumberErrorElement: Locator;
     readonly cardNumberInput: Locator;
+
+    readonly expiryDateField: Locator;
+    readonly expiryDateErrorElement: Locator;
     readonly expiryDateInput: Locator;
+
+    readonly cvcField: Locator;
+    readonly cvcErrorElement: Locator;
     readonly cvcInput: Locator;
 
     constructor(page: Page, rootElementSelector = '.adyen-checkout__card-input') {
         this.rootElement = page.locator(rootElementSelector);
         this.rootElementSelector = rootElementSelector;
 
-        this.cardNumberInput = this.rootElement.frameLocator('[title="Iframe for card number"]').locator('input[aria-label="Card number"]');
-        this.expiryDateInput = this.rootElement.frameLocator('[title="Iframe for expiry date"]').locator('input[aria-label="Expiry date"]');
-        this.cvcInput = this.rootElement.frameLocator('[title="Iframe for security code"]').locator('input[aria-label="Security code"]');
+        /**
+         * Card Number elements, in Checkout
+         */
+        this.cardNumberField = this.rootElement.locator('.adyen-checkout__field--cardNumber'); // Holder
+        this.cardNumberErrorElement = this.cardNumberField.locator('.adyen-checkout__error-text');
+
+        /**
+         * Card Number elements, in iframe
+         */
+        const cardNumberIframe = this.rootElement.frameLocator(`[title="${CARD_IFRAME_TITLE}"]`);
+        this.cardNumberInput = cardNumberIframe.locator(`input[aria-label="${CARD_IFRAME_LABEL}"]`);
+
+        /**
+         * Expiry Date elements, in Checkout
+         */
+        this.expiryDateField = this.rootElement.locator('.adyen-checkout__field--expiryDate'); // Holder
+        this.expiryDateErrorElement = this.expiryDateField.locator('.adyen-checkout__error-text'); // Related error element
+
+        /**
+         * Expiry Date elements, in iframe
+         */
+        const expiryDateIframe = this.rootElement.frameLocator(`[title="${EXPIRY_DATE_IFRAME_TITLE}"]`);
+        this.expiryDateInput = expiryDateIframe.locator(`input[aria-label="${EXPIRY_DATE_IFRAME_LABEL}"]`);
+
+        /**
+         * Security code elements, in Checkout
+         */
+        this.cvcField = this.rootElement.locator('.adyen-checkout__field--securityCode'); // Holder
+        this.cvcErrorElement = this.cvcField.locator('.adyen-checkout__error-text'); // Related error element
+
+        /**
+         * Security code elements, in iframe
+         */
+        const cvcIframe = this.rootElement.frameLocator(`[title="${CVC_IFRAME_TITLE}"]`);
+        this.cvcInput = cvcIframe.locator(`input[aria-label="${CVC_IFRAME_LABEL}"]`);
     }
 
     async isComponentVisible() {
@@ -26,6 +75,10 @@ class Card {
 
     async typeCardNumber(cardNumber: string) {
         await this.cardNumberInput.type(cardNumber, { delay: USER_TYPE_DELAY });
+    }
+
+    async deleteCardNumber() {
+        await this.cardNumberInput.clear();
     }
 
     async typeExpiryDate(expiryDate: string) {
