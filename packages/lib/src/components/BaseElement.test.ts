a75ee9a6f289cfb42d1b148df02d693af75a3f25
guilhermer <guilherme.ribeiro@adyen.com>
2023-09-27 15:49:35 +0200

    test: fix baseelement

--- packages/lib/src/components/BaseElement.test.ts
+++ packages/lib/src/components/BaseElement.test.ts
@@ -1,15 +1,20 @@
 import BaseElement from './BaseElement';
+import { BaseElementProps } from './types';
+import { mock } from 'jest-mock-extended';
+import { ICore } from '../core/types';
 
 describe('BaseElement', () => {
     let MyElement;
+    let core;
 
     beforeAll(() => {
-        MyElement = class extends BaseElement<{}> {};
+        core = mock<ICore>();
+        MyElement = class extends BaseElement<BaseElementProps> {};
     });
 
     describe('formatProps', () => {
         test('should return props by default', () => {
-            const baseElement = new MyElement({});
+            const baseElement = new MyElement({ core });
             const props = { prop1: 'prop1' };
             expect(baseElement.formatProps(props)).toBe(props);
         });
@@ -17,19 +22,19 @@ describe('BaseElement', () => {
 
     describe('formatData', () => {
         test('should return an empty object by default', () => {
-            const baseElement = new MyElement();
+            const baseElement = new MyElement({ core });
             expect(baseElement.formatData()).toEqual({});
         });
     });
 
     describe('get data', () => {
         test('returns an empty object by default', () => {
-            const baseElement = new MyElement();
+            const baseElement = new MyElement({ core });
             expect(baseElement.data).toEqual({ clientStateDataIndicator: true });
         });
 
         test('calls formatData to get the specific component output', () => {
-            const baseElement = new MyElement();
+            const baseElement = new MyElement({ core });
             const spy = jest.spyOn(baseElement, 'formatData');
             expect(baseElement.data).toEqual({ clientStateDataIndicator: true });
             expect(spy).toHaveBeenCalled();
@@ -38,7 +43,7 @@ describe('BaseElement', () => {
 
     describe('render', () => {
         test('does not render anything by default', () => {
-            const baseElement = new MyElement();
+            const baseElement = new MyElement({ core });
             expect(() => baseElement.render()).toThrow();
         });
     });
