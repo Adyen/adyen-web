f816ec3d4ce615a98998032f7354a28ff321b49d
guilhermer <guilherme.ribeiro@adyen.com>
2023-10-27 15:11:37 +0200

    fix: checking checkbox + styles

--- packages/lib/src/components/internal/ClickToPay/components/CtPOneTimePassword/CtPSaveCookiesCheckbox/CtPSaveCookiesCheckbox.tsx
+++ packages/lib/src/components/internal/ClickToPay/components/CtPOneTimePassword/CtPSaveCookiesCheckbox/CtPSaveCookiesCheckbox.tsx
@@ -2,7 +2,7 @@ import { h, Fragment } from 'preact';
 import Field from '../../../../FormFields/Field';
 import Checkbox from '../../../../FormFields/Checkbox';
 import useCoreContext from '../../../../../../core/Context/useCoreContext';
-import { useCallback, useState } from 'preact/hooks';
+import { useState, useCallback } from 'preact/hooks';
 import useClickToPayContext from '../../../context/useClickToPayContext';
 import './CtPSaveCookiesCheckbox.scss';
 
@@ -11,23 +11,23 @@ function CtPSaveCookiesCheckbox() {
     const { updateStoreCookiesConsent, isStoringCookies } = useClickToPayContext();
     const [checked, setIsChecked] = useState(isStoringCookies);
 
-    const handleOnChange = useCallback(event => {
-        const { checked } = event.target;
-        setIsChecked(checked);
-        updateStoreCookiesConsent(checked);
-    }, []);
+    const handleOnChange = useCallback(() => {
+        const newChecked = !checked;
+        setIsChecked(newChecked);
+        updateStoreCookiesConsent(newChecked);
+    }, [updateStoreCookiesConsent, setIsChecked, checked]);
 
     return (
         <Fragment>
             <Field
                 className="adyen-checkout-ctp__otp-checkbox-container"
-                name={'click-to-pay-cookies-checkbox'}
+                name={'clickToPayCookiesCheckbox'}
                 addContextualElement={false}
                 useLabelElement={false}
                 i18n={i18n}
             >
                 <Checkbox
-                    name={'click-to-pay-cookies-checkbox'}
+                    name={'clickToPayCookiesCheckbox'}
                     onInput={handleOnChange}
                     label={i18n.get('ctp.otp.saveCookiesCheckbox')}
                     checked={checked}
