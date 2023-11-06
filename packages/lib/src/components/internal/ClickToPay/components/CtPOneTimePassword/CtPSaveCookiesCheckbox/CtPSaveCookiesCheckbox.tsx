d140714a1edb6620296255937d400d7c57c25927
guilhermer <guilherme.ribeiro@adyen.com>
2023-11-06 14:21:00 +0100

    feat: ui changes

--- packages/lib/src/components/internal/ClickToPay/components/CtPOneTimePassword/CtPSaveCookiesCheckbox/CtPSaveCookiesCheckbox.tsx
+++ packages/lib/src/components/internal/ClickToPay/components/CtPOneTimePassword/CtPSaveCookiesCheckbox/CtPSaveCookiesCheckbox.tsx
@@ -1,4 +1,5 @@
-import { h, Fragment } from 'preact';
+import { Fragment, h } from 'preact';
+import classnames from 'classnames';
 import Field from '../../../../FormFields/Field';
 import Checkbox from '../../../../FormFields/Checkbox';
 import useCoreContext from '../../../../../../core/Context/useCoreContext';
@@ -10,6 +11,7 @@ function CtPSaveCookiesCheckbox() {
     const { i18n } = useCoreContext();
     const { updateStoreCookiesConsent, isStoringCookies } = useClickToPayContext();
     const [checked, setIsChecked] = useState(isStoringCookies);
+    const [isTextTruncated, setIsTextTruncated] = useState<boolean>(true);
 
     const handleOnChange = useCallback(() => {
         const newChecked = !checked;
@@ -18,24 +20,34 @@ function CtPSaveCookiesCheckbox() {
     }, [updateStoreCookiesConsent, setIsChecked, checked]);
 
     return (
-        <Fragment>
-            <Field
-                className="adyen-checkout-ctp__otp-checkbox-container"
-                name={'clickToPayCookiesCheckbox'}
-                addContextualElement={false}
-                useLabelElement={false}
-                i18n={i18n}
-            >
+        <div
+            className={classnames('adyen-checkout-ctp__otp-checkbox-container', {
+                'adyen-checkout-ctp__otp-checkbox-container--checked': checked
+            })}
+        >
+            <Field name={'clickToPayCookiesCheckbox'} addContextualElement={false} useLabelElement={false} i18n={i18n}>
                 <Checkbox
                     name={'clickToPayCookiesCheckbox'}
                     onInput={handleOnChange}
-                    label={i18n.get('ctp.otp.saveCookiesCheckbox')}
+                    label={i18n.get('ctp.otp.saveCookiesCheckbox.label')}
                     checked={checked}
+                    aria-describedby={'adyen-ctp-cookies-info'}
                 />
             </Field>
 
-            <p className="adyen-checkout-ctp__otp-checkbox-info">{i18n.get('ctp.otp.saveCookiesCheckbox.info')}</p>
-        </Fragment>
+            <p className="adyen-checkout-ctp__otp-checkbox-info">
+                {isTextTruncated ? (
+                    <Fragment>
+                        <span id="adyen-ctp-cookies-info">{i18n.get('ctp.otp.saveCookiesCheckbox.shorterInfo')} </span>
+                        <button className="adyen-checkout-ctp__otp-readmore-button" onClick={() => setIsTextTruncated(false)}>
+                            {i18n.get('readMore')}..
+                        </button>
+                    </Fragment>
+                ) : (
+                    <span id="adyen-ctp-cookies-info">{i18n.get('ctp.otp.saveCookiesCheckbox.information')}</span>
+                )}
+            </p>
+        </div>
     );
 }
 
