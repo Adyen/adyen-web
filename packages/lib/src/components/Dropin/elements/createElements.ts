0feeb53b11846c9bff496c7908ac03e800044f24
nicholas <nicholas.spong@adyen.com>
2023-11-24 16:32:12 +0100

    More informative warning in Dropin when classes for supported PMs have not been imported

--- packages/lib/src/components/Dropin/elements/createElements.ts
+++ packages/lib/src/components/Dropin/elements/createElements.ts
@@ -3,7 +3,7 @@ import { PaymentMethod, StoredPaymentMethod } from '../../../types/global-types'
 import { getComponentConfiguration } from './getComponentConfiguration';
 import { ICore } from '../../../core/types';
 import UIElement from '../../internal/UIElement/UIElement';
-import { PaymentMethodsConfiguration } from '../types';
+import { PaymentMethodsConfiguration, componentsMapToString } from '../types';
 
 /**
  * Returns a filtered (available) list of component Elements
@@ -30,7 +30,11 @@ const createElements = (
 
             if (!PaymentMethodElement) {
                 console.warn(
-                    `Dropin: '${paymentMethod.type}' component not found. Make sure to pass its Class to the Dropin 'paymentMethodComponents' parameter`
+                    `Dropin: You support the payment method '${
+                        paymentMethod.type
+                    }' but this component has not been configured. Make sure to import the Class  '${
+                        componentsMapToString[paymentMethod.type]
+                    }' and then pass it in the Dropin's 'paymentMethodComponents' config property if you wish to offer this payment method.`
                 );
                 return null;
             }
