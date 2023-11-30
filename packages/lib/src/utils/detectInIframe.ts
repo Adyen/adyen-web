db8bbe55c030c1be37cc64247e8aa90eb7205f1f
nicholas <nicholas.spong@adyen.com>
2023-11-30 14:17:41 +0100

    detectInIframe fn fix

--- packages/lib/src/utils/detectInIframe.ts
+++ packages/lib/src/utils/detectInIframe.ts
@@ -1,2 +1,9 @@
-// Returns true if the page is being run in an iframe
-export default () => window.location !== window.parent.location;
+//TODO - rename this module to detectInIframeInSameDomain
+
+// Returns true if the page is being run in an iframe (in a domain we have access to)
+export default () => {
+    if (window.parent.location instanceof Location) {
+        return window.location !== window.parent.location;
+    }
+    return false; // we cannot access window.parent.location - so consider us "not to be in an iframe" for the purpose of Redirects
+};
