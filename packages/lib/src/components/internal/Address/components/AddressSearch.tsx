9d04b6f7ee3c38e6433439496cdffa1c2ed8abfb
António Ferreira <m1aw@users.noreply.github.com>
2023-12-04 13:11:06 +0100

    Feature: Adds onAddressSelected to address lookup functionality (#2406)
    * feat: adds onAddresSelected
    
    * fix: change memo to callback
    
    * fix: makes some fnc to useCallback
    
    * feat: typings and blur on close
    
    * add: changeset
    
    * fix: typo in changset
    
    * fix: exported debounce
    
    * add: fix typings
    
    * add: unit test to address search
    
    * fix: assertion count
    
    * fix: sonar cloud code smells

--- packages/lib/src/components/internal/Address/components/AddressSearch.tsx
+++ packages/lib/src/components/internal/Address/components/AddressSearch.tsx
@@ -1,11 +1,12 @@
 import Field from '../../FormFields/Field';
-import { Fragment, h } from 'preact';
+import { h } from 'preact';
 import { AddressLookupItem } from '../types';
-import { useCallback, useEffect, useMemo, useState } from 'preact/hooks';
+import { useCallback, useEffect, useState, useMemo } from 'preact/hooks';
 import './AddressSearch.scss';
 import useCoreContext from '../../../../core/Context/useCoreContext';
 import { debounce } from '../utils';
 import Select from '../../FormFields/Select';
+import { AddressData } from '../../../../types';
 
 export type OnAddressLookupType = (
     value: string,
@@ -15,15 +16,37 @@ export type OnAddressLookupType = (
     }
 ) => Promise<void>;
 
+export type OnAddressSelectedType = (
+    value: string,
+    actions: {
+        resolve: (value: AddressLookupItem) => void;
+        reject: (reason?: any) => void;
+    }
+) => Promise<void>;
+
 interface AddressSearchProps {
     onAddressLookup?: OnAddressLookupType;
-    onSelect: any; //TODO
+    onAddressSelected?: OnAddressSelectedType;
+    onSelect: (addressItem: AddressData) => void;
     onManualAddress: any;
     externalErrorMessage: string;
     hideManualButton: boolean;
+    addressSearchDebounceMs?: number;
 }
 
-export default function AddressSearch({ onAddressLookup, onSelect, onManualAddress, externalErrorMessage, hideManualButton }: AddressSearchProps) {
+interface RejectionReason {
+    errorMessage: string;
+}
+
+export default function AddressSearch({
+    onAddressLookup,
+    onAddressSelected,
+    onSelect,
+    onManualAddress,
+    externalErrorMessage,
+    hideManualButton,
+    addressSearchDebounceMs
+}: Readonly<AddressSearchProps>) {
     const [formattedData, setFormattedData] = useState([]);
     const [originalData, setOriginalData] = useState([]);
 
@@ -32,20 +55,23 @@ export default function AddressSearch({ onAddressLookup, onSelect, onManualAddre
     const { i18n } = useCoreContext();
     const mapDataToSelect = data => data.map(({ id, name }) => ({ id, name }));
 
-    const onInput = useCallback(
-        async event => {
+    const handlePromiseReject = useCallback((reason: RejectionReason) => {
+        if (reason?.errorMessage) {
+            setErrorMessage(reason.errorMessage);
+        }
+    }, []);
+
+    const onTextInput = useCallback(
+        async (inputValue: string) => {
             new Promise<Array<AddressLookupItem>>((resolve, reject) => {
-                onAddressLookup(event, { resolve, reject });
+                onAddressLookup(inputValue, { resolve, reject });
             })
-                .then(data => {
-                    setOriginalData(data);
-                    setFormattedData(mapDataToSelect(data));
+                .then(searchArray => {
+                    setOriginalData(searchArray);
+                    setFormattedData(mapDataToSelect(searchArray));
                     setErrorMessage('');
                 })
-                .catch(reason => {
-                    setErrorMessage(reason);
-                    console.error('error', reason);
-                });
+                .catch(reason => handlePromiseReject(reason));
         },
         [onAddressLookup]
     );
@@ -55,44 +81,57 @@ export default function AddressSearch({ onAddressLookup, onSelect, onManualAddre
         setErrorMessage(externalErrorMessage);
     }, [externalErrorMessage]);
 
-    const onChange = event => {
+    const onSelectItem = async event => {
         if (!event.target.value) {
             setErrorMessage(i18n.get('address.errors.incomplete'));
             return;
         }
         const value = originalData.find(item => item.id === event.target.value);
-        onSelect(value);
-        setFormattedData([]);
+
+        // 1. in case we don't get a function just select item
+        if (typeof onAddressSelected !== 'function') {
+            onSelect(value);
+            setFormattedData([]);
+            return;
+        }
+
+        // 2. in case callback is provided, create and call onAddressSelected
+        new Promise<AddressLookupItem>((resolve, reject) => {
+            onAddressSelected(value, { resolve, reject });
+        })
+            .then(fullData => {
+                onSelect(fullData);
+                setFormattedData([]);
+            })
+            .catch(reason => handlePromiseReject(reason));
     };
 
-    const debounceInputHandler = useMemo(() => debounce(onInput), []);
+    const debounceInputHandler = useMemo(() => debounce(onTextInput, addressSearchDebounceMs), []);
 
     return (
-        <Fragment>
-            <div className={'adyen-checkout__address-search adyen-checkout__field-group'}>
-                <Field label={i18n.get('address')} classNameModifiers={['address-search']} errorMessage={errorMessage} name={'address-search'}>
-                    <Select
-                        name={'address-search'}
-                        className={'adyen-checkout__address-search__dropdown'}
-                        //placeholder={i18n.get('address.placeholder')}
-                        onInput={debounceInputHandler}
-                        items={formattedData}
-                        onChange={onChange}
-                        disableTextFilter={true}
-                    />
-                </Field>
-                {!hideManualButton && (
-                    <span className="adyen-checkout__address-search__manual-add">
-                        <button
-                            type="button"
-                            className="adyen-checkout__button adyen-checkout__button--inline adyen-checkout__button--link adyen-checkout__address-search__manual-add__button"
-                            onClick={onManualAddress}
-                        >
-                            {'+ ' + i18n.get('address.enterManually')}
-                        </button>
-                    </span>
-                )}
-            </div>
-        </Fragment>
+        <div className={'adyen-checkout__address-search adyen-checkout__field-group'}>
+            <Field label={i18n.get('address')} classNameModifiers={['address-search']} errorMessage={errorMessage} name={'address-search'}>
+                <Select
+                    name={'address-search'}
+                    className={'adyen-checkout__address-search__dropdown'}
+                    onInput={debounceInputHandler}
+                    items={formattedData}
+                    onChange={onSelectItem}
+                    disableTextFilter={true}
+                    blurOnClose={true}
+                />
+            </Field>
+            {!hideManualButton && (
+                <span className="adyen-checkout__address-search__manual-add">
+                    <button
+                        type="button"
+                        className="adyen-checkout__button adyen-checkout__button--inline adyen-checkout__button--link adyen-checkout__address-search__manual-add__button"
+                        onClick={onManualAddress}
+                    >
+                        {'+ ' + i18n.get('address.enterManually')}
+                    </button>
+                </span>
+            )}
+        </div>
     );
 }
