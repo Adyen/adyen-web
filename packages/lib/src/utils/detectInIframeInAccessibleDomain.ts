c31de29358cd282a99aaa93bebdf55530b916f91
nicholas <nicholas.spong@adyen.com>
2023-12-04 13:51:43 +0100

    Changed check for reading window.parent.location

--- packages/lib/src/utils/detectInIframeInAccessibleDomain.ts
+++ packages/lib/src/utils/detectInIframeInAccessibleDomain.ts
@@ -1,7 +1,10 @@
 // Returns true if the page is being run in an iframe (in a domain we have access to)
 export default () => {
-    if (window.parent.location instanceof Location) {
-        return window.location !== window.parent.location;
+    try {
+        if (window.parent.location.href) {
+            return window.location !== window.parent.location; // iframe check: locations will differ if we're in an iframe
+        }
+    } catch (e) {
+        return false; // we cannot access window.parent.location.href - so consider us "not to be in an iframe" for the purpose of Redirects
     }
-    return false; // we cannot access window.parent.location - so consider us "not to be in an iframe" for the purpose of Redirects
 };
