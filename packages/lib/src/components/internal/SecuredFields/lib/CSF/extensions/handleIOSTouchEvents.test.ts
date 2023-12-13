7fb380b3605eaaf150c0224451a3778efe8e440f
nicholas <nicholas.spong@adyen.com>
2023-12-13 18:49:48 +0100

    handleIOSTouchEvents.test.ts - second draft

--- packages/lib/src/components/internal/SecuredFields/lib/CSF/extensions/handleIOSTouchEvents.test.ts
+++ packages/lib/src/components/internal/SecuredFields/lib/CSF/extensions/handleIOSTouchEvents.test.ts
@@ -1,21 +1,18 @@
-// import { SecuredFields } from '../../types';
 import handleIOSTouchEvents from './handleIOSTouchEvents';
-// import { postMessageToAllIframes } from '../partials/postMessageToAllIframes';
-
-// jest.mock('../partials/postMessageToAllIframes');
-
-// const mockedPostMessageToAllIframes = postMessageToAllIframes as jest.Mock;
 
 const myCSF = {
     state: { type: 'card', registerFieldForIos: null },
-    props: { rootNode: 'div' },
+    props: { rootNode: document.createElement('div') },
     callbacks: {
         onTouchstartIOS: null
     },
+    config: { keypadFix: false },
     touchstartListener: handleIOSTouchEvents.touchstartListener,
+    touchendListener: handleIOSTouchEvents.touchendListener,
     handleTouchend: handleIOSTouchEvents.handleTouchend,
     hasGenuineTouchEvents: null,
-    postMessageToAllIframes: null
+    postMessageToAllIframes: null,
+    destroyTouchendListener: null
 };
 
 const makeElementWithAttribute = (elementType, attrName, attrValue) => {
@@ -25,23 +22,13 @@ const makeElementWithAttribute = (elementType, attrName, attrValue) => {
 };
 
 describe("Testing CSF's handleIOSTouchEvents' touchstartListener functionality", () => {
-    // const postMessageToAllIframesMock = jest.fn(obj => console.log('### handleIOSTouchEvents.test::Mock FN call to postMessageToAllIframes:: ', obj));
-
     beforeEach(() => {
-        // console.log = jest.fn(() => {});
+        console.log = jest.fn(() => {});
         myCSF.hasGenuineTouchEvents = false;
 
-        myCSF.postMessageToAllIframes = jest.fn(obj => {
-            console.log('### handleTab.test:::: postMessageToAllIframes called with', obj);
-        });
+        myCSF.postMessageToAllIframes = jest.fn(() => {});
 
-        myCSF.callbacks.onTouchstartIOS = jest.fn(obj => {
-            console.log('### handleIOSTouchEvents.test:::: onTouchstartIOS callback called with', obj);
-        });
-
-        // mockedPostMessageToAllIframes.mockReset();
-        // mockedPostMessageToAllIframes.mockImplementation(obj => postMessageToAllIframesMock(obj));
-        // postMessageToAllIframesMock.mockClear();
+        myCSF.callbacks.onTouchstartIOS = jest.fn(() => {});
     });
 
     test(
@@ -64,9 +51,6 @@ describe("Testing CSF's handleIOSTouchEvents' touchstartListener functionality",
         'Calling touchstartListener and passing it a span element will see that hasGenuineTouchEvents is set to true, and ' +
             'that postMessageToAllIframes & callbacks.onTouchstartIOS are both called with the expected objects',
         async () => {
-            // const myElement = document.createElement('span');
-            // myElement.setAttribute('data-id', 'mySpan');
-
             const myElement = makeElementWithAttribute('span', 'data-id', 'mySpan');
 
             // @ts-ignore - it's just a test!
@@ -83,9 +67,6 @@ describe("Testing CSF's handleIOSTouchEvents' touchstartListener functionality",
         'Calling touchstartListener and passing it an element that is not an input or a span will see that hasGenuineTouchEvents is set to true, but ' +
             'postMessageToAllIframes & callbacks.onTouchstartIOS will not be called',
         async () => {
-            // const myElement = document.createElement('div');
-            // myElement.setAttribute('data-id', 'myDiv');
-
             const myElement = makeElementWithAttribute('div', 'data-id', 'myDiv');
 
             // @ts-ignore - it's just a test!
@@ -101,7 +82,7 @@ describe("Testing CSF's handleIOSTouchEvents' touchstartListener functionality",
 
 describe("Testing CSF's handleIOSTouchEvents' handleTouchend functionality", () => {
     beforeEach(() => {
-        // console.log = jest.fn(() => {});
+        console.log = jest.fn(() => {});
         myCSF.state.registerFieldForIos = false;
     });
 
@@ -112,18 +93,54 @@ describe("Testing CSF's handleIOSTouchEvents' handleTouchend functionality", () 
     });
 });
 
-// describe("Testing CSF's handleIOSTouchEvents' touchendListener functionality", () => {
-//     beforeEach(() => {
-//         // console.log = jest.fn(() => {});
-//         myCSF.state.registerFieldForIos = false;
-//     });
-//
-//     test('Calling handleTouchend will set registerFieldForIos = true', async () => {
-//         const myElement = makeElementWithAttribute('input', 'name', 'myInput');
-//
-//         // @ts-ignore - it's just a test!
-//         myCSF.touchendListener(myElement);
-//
-//         expect(myCSF.state.registerFieldForIos).toEqual(true);
-//     });
-// });
+describe("Testing CSF's handleIOSTouchEvents' touchendListener functionality", () => {
+    beforeEach(() => {
+        console.log = jest.fn(() => {});
+        myCSF.state.registerFieldForIos = false;
+
+        myCSF.postMessageToAllIframes = jest.fn(() => {});
+
+        myCSF.destroyTouchendListener = jest.fn(() => {});
+    });
+
+    test('Calling handleTouchend and passing it an input element will...', async () => {
+        const myElement = makeElementWithAttribute('input', 'name', 'myInput');
+
+        // @ts-ignore - it's just a test!
+        myCSF.touchendListener({ target: myElement });
+
+        expect(myCSF.destroyTouchendListener).toHaveBeenCalled();
+
+        expect(myCSF.state.registerFieldForIos).toEqual(false);
+
+        expect(myCSF.postMessageToAllIframes).toHaveBeenCalledWith({ fieldType: 'webInternalElement', fieldClick: true });
+    });
+
+    test('Calling handleTouchend and passing it an element that is not an input when config.keypadFix = false, will...', async () => {
+        const myElement = makeElementWithAttribute('div', 'data-id', 'myInput');
+
+        // @ts-ignore - it's just a test!
+        myCSF.touchendListener({ target: myElement });
+
+        expect(myCSF.destroyTouchendListener).toHaveBeenCalled();
+
+        expect(myCSF.state.registerFieldForIos).toEqual(false);
+
+        expect(myCSF.postMessageToAllIframes).toHaveBeenCalledWith({ fieldType: 'webInternalElement', fieldClick: true });
+    });
+
+    test('Calling handleTouchend and passing it an element that is not an input when config.keypadFix = true, will...', async () => {
+        myCSF.config.keypadFix = true;
+
+        const myElement = makeElementWithAttribute('div', 'data-id', 'myInput');
+
+        // @ts-ignore - it's just a test!
+        myCSF.touchendListener({ target: myElement });
+
+        expect(myCSF.destroyTouchendListener).toHaveBeenCalled();
+
+        expect(myCSF.state.registerFieldForIos).toEqual(false);
+
+        expect(myCSF.postMessageToAllIframes).toHaveBeenCalledWith({ fieldType: 'webInternalElement', fieldClick: true });
+    });
+});
