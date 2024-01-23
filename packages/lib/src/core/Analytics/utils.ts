57ac845a942eeacda943dfc4d5697d196beb46df
nicholas <nicholas.spong@adyen.com>
2024-01-23 15:33:56 +0100

    Comments added regarding what properties are added to what analytics objects

--- packages/lib/src/core/Analytics/utils.ts
+++ packages/lib/src/core/Analytics/utils.ts
@@ -11,32 +11,34 @@ export const getUTCTimestamp = () => Date.now();
  *  "code", "errorType" & "message"
  *
  * Log objects have, in addition to the base props:
- *  "message" &
- *  "type" & "target" (e.g. when onSubmit is called after a pay button click), or,
- *  "type" & "subtype" (e.g. when an action is handled), or,
- *  "type" (e.g. logging during the 3DS2 process)
+ *  "message" & "type" &
+ *    "target" (e.g. when onSubmit is called after a pay button click), or,
+ *    "subtype" (e.g. when an action is handled)
  *
  * Info objects have, in addition to the base props:
  *   "type" & "target" &
- *   "isStoredPaymentMethod" & "brand" (when a storedCard is "selected")
- *   // TODO - NEW info events can also have validationErrorCode & validationErrorMessage props
+ *     "isStoredPaymentMethod" & "brand" (when a storedCard is "selected"), or,
+ *     "validationErrorCode" & "validationErrorMessage" (when the event is describing a validation error)
  *
  *  All objects can also have a "metadata" object of key-value pairs
  */
 export const createAnalyticsObject = (aObj: CreateAnalyticsObject): AnalyticsObject => ({
     timestamp: String(getUTCTimestamp()),
     component: aObj.component,
-    ...(aObj.event === 'error' && { code: aObj.code, errorType: aObj.errorType }), // only added if we have an error object
-    ...((aObj.event === 'error' || aObj.event === 'log') && { message: aObj.message }), // only added if we have an error, or log object
-    ...(aObj.event === 'log' && { type: aObj.type }), // only added if we have a log object
-    ...(aObj.event === 'log' && aObj.type === ANALYTICS_ACTION_STR && { subType: aObj.subtype }), // only added if we have a log object of Action type
-    ...(aObj.event === 'log' && aObj.type === ANALYTICS_SUBMIT_STR && { target: aObj.target }), // only added if we have a log object of Submit type
-    ...(aObj.event === 'info' && { type: aObj.type, target: aObj.target }), // only added if we have an info object
-    ...(aObj.event === 'info' && aObj.isStoredPaymentMethod && { isStoredPaymentMethod: aObj.isStoredPaymentMethod, brand: aObj.brand }), // only added if we have an info object about a storedPM
+    /** ERROR */
+    ...(aObj.event === 'error' && { code: aObj.code, errorType: aObj.errorType, message: aObj.message }), // error event
+    /** LOG */
+    ...(aObj.event === 'log' && { type: aObj.type, message: aObj.message }), // log event
+    ...(aObj.event === 'log' && aObj.type === ANALYTICS_ACTION_STR && { subType: aObj.subtype }), // only added if we have a log event of Action type
+    ...(aObj.event === 'log' && aObj.type === ANALYTICS_SUBMIT_STR && { target: aObj.target }), // only added if we have a log event of Submit type
+    /** INFO */
+    ...(aObj.event === 'info' && { type: aObj.type, target: aObj.target }), // info event
+    ...(aObj.event === 'info' && aObj.isStoredPaymentMethod && { isStoredPaymentMethod: aObj.isStoredPaymentMethod, brand: aObj.brand }), // only added if we have an info event about a storedPM
     ...(aObj.event === 'info' &&
         aObj.type === ANALYTICS_VALIDATION_ERROR_STR && {
             validationErrorCode: aObj.validationErrorCode,
             validationErrorMessage: aObj.validationErrorMessage
-        }),
+        }), // only added if we have an info event describing a validation error
+    /** All */
     ...(aObj.metadata && { metadata: aObj.metadata })
 });
