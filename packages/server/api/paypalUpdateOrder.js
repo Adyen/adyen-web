e1f4f0acf68b76876046c3fe563754b63a38a176
guilhermer <guilherme.ribeiro@adyen.com>
2024-02-13 15:10:02 +0100

    feat: clean up logs

--- packages/server/api/paypalUpdateOrder.js
+++ packages/server/api/paypalUpdateOrder.js
@@ -5,9 +5,5 @@ const { MERCHANT_ACCOUNT: merchantAccount } = require('../utils/config');
 
 module.exports = (res, request) => {
     const params = getPostParameters('paypal/updateOrder', { ...request });
-
-    console.log('## REQUEST');
-    console.log(params);
-
     post(params, (error, response, body) => handleCallback({ error, response, body }, res));
 };
