96c33a67d39e97d6d8bb8594c43e206aa4054ef3
Yu Long <longyu901009@gmail.com>
2024-02-19 17:28:01 +0100

    Fix the storybook advanced flow (#2555)

--- packages/lib/storybook/helpers/checkout-handlers.ts
+++ packages/lib/storybook/helpers/checkout-handlers.ts
@@ -1,7 +1,7 @@
 import { makePayment, makeDetailsCall, getPaymentMethods } from './checkout-api-calls';
 import UIElement from '../../src/components/internal/UIElement/UIElement';
 import Core from '../../src/core';
-
+// todo: we can make it a preact component in the storybook, so that we don't need to manipulate the DOM
 function displayResultMessage(isAuthorized: boolean, resultCode: string): void {
     const image = document.createElement('img');
     image.setAttribute(
