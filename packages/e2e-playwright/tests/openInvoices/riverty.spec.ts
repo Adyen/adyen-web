19d411601816a4d663c3d7ae58624112a64e6d7f
nicholas <nicholas.spong@adyen.com>
2024-03-20 14:55:07 +0100

    Fixing srPanel regression for OpenInvoices (and adding e2e tests)

---
+++ packages/e2e-playwright/tests/openInvoices/riverty.spec.ts
@@ -0,0 +1,81 @@
+import { test, expect } from '../../pages/openInvoices/openInvoices.fixture';
+
+import LANG from '../../../lib/src/language/locales/en-US.json';
+
+const INVALID_FORMAT_EXPECTS = LANG['invalid.format.expects'].replace('%{format}', `99999`);
+
+const BILLING_ADDRESS = LANG['billingAddress'];
+const DELIVERY_ADDRESS = LANG['deliveryAddress'];
+
+// [
+//     'Enter your first name-sr',
+//     'Enter your last name-sr',
+//     'Enter the Date of birth-sr',
+//     'Enter the Email address-sr',
+//     'Enter the Telephone number-sr',
+//     'Enter the Billing address Street-sr',
+//     'Enter the Billing address House number-sr',
+//     'Enter the Billing address Postal code-sr',
+//     'Enter the Billing address City-sr',
+//     'Enter the Delivery Address Recipient first name-sr',
+//     'Enter the Delivery Address Recipient last name-sr',
+//     'Enter the Delivery Address Street-sr',
+//     'Enter the Delivery Address House number-sr',
+//     'Enter the Delivery Address Postal code-sr',
+//     'Enter the Delivery Address City-sr',
+//     'You must agree with the terms & conditions-sr'
+// ]
+
+const expectedSRPanelTexts = [
+    `${LANG['firstName.invalid']}`,
+    `${LANG['lastName.invalid']}`,
+    `${LANG['dateOfBirth.invalid']}`,
+    `${LANG['shopperEmail.invalid']}`,
+    `${LANG['telephoneNumber.invalid']}`,
+    `${BILLING_ADDRESS} ${LANG['street']}: ${LANG['error.va.gen.01']}`,
+    `${BILLING_ADDRESS} ${LANG['houseNumberOrName']}: ${LANG['error.va.gen.01']}`,
+    `${BILLING_ADDRESS} ${LANG['postalCode']}: ${INVALID_FORMAT_EXPECTS}`,
+    `${BILLING_ADDRESS} ${LANG['city']}: ${LANG['error.va.gen.01']}`,
+    `${DELIVERY_ADDRESS} ${LANG['deliveryAddress.firstName']}: ${LANG['error.va.gen.01']}`,
+    `${DELIVERY_ADDRESS} ${LANG['deliveryAddress.lastName']}: ${LANG['error.va.gen.01']}`,
+    `${DELIVERY_ADDRESS} ${LANG['street']}: ${LANG['error.va.gen.01']}`,
+    `${DELIVERY_ADDRESS} ${LANG['houseNumberOrName']}: ${LANG['error.va.gen.01']}`,
+    `${DELIVERY_ADDRESS} ${LANG['postalCode']}: ${INVALID_FORMAT_EXPECTS}`,
+    `${DELIVERY_ADDRESS} ${LANG['city']}: ${LANG['error.va.gen.01']}`,
+    `${LANG['consent.checkbox.invalid']}`
+];
+
+test.describe('Test Riverty Component', () => {
+    test('#1 Test how Riverty component handles SRPanel messages', async ({ openInvoicesPage_riverty }) => {
+        const { openInvoices, page } = openInvoicesPage_riverty;
+
+        await openInvoices.isComponentVisible();
+
+        await openInvoices.rivertyDeliveryAddressCheckbox.click();
+
+        await openInvoicesPage_riverty.pay();
+
+        // Need to wait so that the expected elements can be found by locator.allInnerTexts
+        await page.waitForTimeout(100);
+
+        // Inspect SRPanel errors: all expected errors, in the expected order
+        await page
+            .locator('.adyen-checkout-sr-panel__msg')
+            .allInnerTexts()
+            .then(retrievedSRPanelTexts => {
+                console.log('### riverty.spec:::: retrievedSRPanelTexts', retrievedSRPanelTexts);
+                retrievedSRPanelTexts.forEach((retrievedText, index) => {
+                    // KEEP - handy for debugging test
+                    // console.log('\n### riverty.spec:::: retrievedText', retrievedText);
+                    // console.log('### riverty.spec:::: expectedTexts', expectedSRPanelTexts[index]);
+
+                    expect(retrievedText).toContain(expectedSRPanelTexts[index]);
+                });
+            });
+
+        // KEEP - handy for debugging test
+        // await expect(page.getByText('Enter your first name-sr', { exact: true })).toBeVisible();
+
+        await page.waitForTimeout(1000);
+    });
+});
