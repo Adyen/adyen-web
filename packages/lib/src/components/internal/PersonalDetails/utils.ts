52f41f9780a1f257f469d2b7ca63f852083f66fa
nicholas <nicholas.spong@adyen.com>
2024-03-20 11:32:17 +0100

    PersonalDetails no longer maps date-of-birth srPanel errors for OpenInvoices

--- packages/lib/src/components/internal/PersonalDetails/utils.ts
+++ packages/lib/src/components/internal/PersonalDetails/utils.ts
@@ -26,7 +26,6 @@ export const getFormattedData = data => {
 export const mapFieldKey = (key: string, i18n: Language): string => {
     switch (key) {
         case 'gender':
-        case 'dateOfBirth':
             return i18n.get(key);
         // We know that the translated error messages do contain a reference to the field they refer to, so we won't need to map them
         default:
