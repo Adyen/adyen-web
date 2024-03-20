baccd15544b24430eed5383321a14821aaf52bcd
nicholas <nicholas.spong@adyen.com>
2024-03-20 09:45:57 +0100

    Making generation of expected texts foe e2e tests as generic as possible

--- packages/e2e-playwright/tests/openInvoices/riverty.spec.ts
+++ packages/e2e-playwright/tests/openInvoices/riverty.spec.ts
@@ -2,26 +2,9 @@ import { test, expect } from '../../pages/openInvoices/openInvoices.fixture';
 
 import LANG from '../../../lib/src/language/locales/en-US.json';
 
-// const expectedSRPanelTexts = [
-//     'Enter your first name-sr',
-//     'Enter your last name-sr',
-//     'Date of birth: Enter a valid date of birth that indicates you are at least 18 years old-sr',
-//     'Invalid email address-sr',
-//     'Invalid telephone number-sr',
-//     'Billing address Street: Incomplete field-sr',
-//     'Billing address House number: Incomplete field-sr',
-//     'Billing address Postal code: Invalid format. Expected format: 99999-sr',
-//     'Billing address City: Incomplete field-sr',
-//     'Delivery Address Recipient first name: Incomplete field-sr',
-//     'Delivery Address Recipient last name: Incomplete field-sr',
-//     'Delivery Address Street: Incomplete field-sr',
-//     'Delivery Address House number: Incomplete field-sr',
-//     'Delivery Address Postal code: Invalid format. Expected format: 99999-sr',
-//     'Delivery Address City: Incomplete field-sr',
-//     'You must agree with the terms & conditions-sr'
-// ];
-
 const SR_PREFIX = '-sr';
+
+const INVALID_FORMAT_EXPECTS = LANG['invalidFormatExpects'].replace('%{format}', `99999${SR_PREFIX}`);
 
 const BILLING_ADDRESS = LANG['billingAddress'];
 const DELIVERY_ADDRESS = LANG['deliveryAddress'];
@@ -34,19 +17,19 @@ const expectedSRPanelTexts = [
     `${LANG['telephoneNumber.invalid']}${SR_PREFIX}`,
     `${BILLING_ADDRESS} ${LANG['street']}: ${LANG['error.va.gen.01']}${SR_PREFIX}`,
     `${BILLING_ADDRESS} ${LANG['houseNumberOrName']}: ${LANG['error.va.gen.01']}${SR_PREFIX}`,
-    `${BILLING_ADDRESS} ${LANG['postalCode']}: Invalid format. Expected format: 99999-sr`,
+    `${BILLING_ADDRESS} ${LANG['postalCode']}: ${INVALID_FORMAT_EXPECTS}`,
     `${BILLING_ADDRESS} ${LANG['city']}: ${LANG['error.va.gen.01']}${SR_PREFIX}`,
     `${DELIVERY_ADDRESS} ${LANG['deliveryAddress.firstName']}: ${LANG['error.va.gen.01']}${SR_PREFIX}`,
     `${DELIVERY_ADDRESS} ${LANG['deliveryAddress.lastName']}: ${LANG['error.va.gen.01']}${SR_PREFIX}`,
     `${DELIVERY_ADDRESS} ${LANG['street']}: ${LANG['error.va.gen.01']}${SR_PREFIX}`,
     `${DELIVERY_ADDRESS} ${LANG['houseNumberOrName']}: ${LANG['error.va.gen.01']}${SR_PREFIX}`,
-    `${DELIVERY_ADDRESS} ${LANG['postalCode']}: Invalid format. Expected format: 99999-sr`,
+    `${DELIVERY_ADDRESS} ${LANG['postalCode']}: ${INVALID_FORMAT_EXPECTS}`,
     `${DELIVERY_ADDRESS} ${LANG['city']}: ${LANG['error.va.gen.01']}${SR_PREFIX}`,
     `${LANG['consent.checkbox.invalid']}${SR_PREFIX}`
 ];
 
 test.describe('Test Riverty Component', () => {
-    test('#1 test how Riverty component handles SRPanel messages', async ({ openInvoicesPage_riverty }) => {
+    test('#1 Test how Riverty component handles SRPanel messages', async ({ openInvoicesPage_riverty }) => {
         const { openInvoices, page } = openInvoicesPage_riverty;
 
         await openInvoices.isComponentVisible();
@@ -64,6 +47,7 @@ test.describe('Test Riverty Component', () => {
             .allInnerTexts()
             .then(retrievedSRPanelTexts => {
                 retrievedSRPanelTexts.forEach((retrievedText, index) => {
+                    // KEEP - handy for debugging test
                     // console.log('\n### riverty.spec:::: retrievedText', retrievedText);
                     // console.log('### riverty.spec:::: expectedTexts', expectedSRPanelTexts[index]);
 
@@ -71,7 +55,7 @@ test.describe('Test Riverty Component', () => {
                 });
             });
 
-        // Inspect SRPanel errors
+        // KEEP - handy for debugging test
         // await expect(page.getByText('Enter your first name-sr', { exact: true })).toBeVisible();
 
         await page.waitForTimeout(1000);
