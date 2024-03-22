0a58048799aed1647d4223394385d0a60b059240
nicholas <nicholas.spong@adyen.com>
2024-03-22 10:03:24 +0100

    Added unit tests for OpenInvoice field mapping util

--- packages/lib/src/components/internal/OpenInvoice/utils.test.ts
+++ packages/lib/src/components/internal/OpenInvoice/utils.test.ts
@@ -1,5 +1,7 @@
-import { getActiveFieldsData, getInitialActiveFieldsets } from './utils';
+import { getActiveFieldsData, getInitialActiveFieldsets, mapFieldKey } from './utils';
 import { OpenInvoiceActiveFieldsets, OpenInvoiceStateData, OpenInvoiceVisibility } from './types';
+import Language from '../../../language';
+import { FIRST_NAME, LAST_NAME } from '../Address/constants';
 
 describe('OpenInvoice utils', () => {
     describe('getActiveFieldsData', () => {
@@ -58,6 +60,42 @@ describe('OpenInvoice utils', () => {
             };
 
             expect(getInitialActiveFieldsets(visibility).deliveryAddress).toBe(true);
+        });
+    });
+    describe('mapFieldKey functionality', () => {
+        const i18n = new Language('en-US');
+
+        const BILLING_ADDRESS = i18n.get('billingAddress');
+        const DELIVERY_ADDRESS = i18n.get('deliveryAddress');
+        const STREET = i18n.get('street');
+        const DATE_OF_BIRTH = i18n.get('dateOfBirth');
+        const RECIPIENT_FIRST_NAME = i18n.get('deliveryAddress.firstName');
+        const ZIP_CODE = i18n.get('zipCode');
+
+        const labelSpecs = {
+            firstName: 'deliveryAddress.firstName',
+            lastName: 'deliveryAddress.lastName',
+            postalCode: 'zipCode'
+        };
+
+        test('Should return expected strings from mapping function', () => {
+            // should split key and return translated string in expected format
+            expect(mapFieldKey('billingAddress:street', i18n, null)).toEqual(`${BILLING_ADDRESS} ${STREET}`);
+
+            // should split and use labels spec to return translated string in expected format
+            expect(mapFieldKey('deliveryAddress:firstName', i18n, labelSpecs)).toEqual(`${DELIVERY_ADDRESS} ${RECIPIENT_FIRST_NAME}`);
+
+            // key doesn't need splitting and shouldn't be mapped
+            expect(mapFieldKey('street', i18n, null)).toEqual(null);
+
+            // Not an address related field, but should be mapped
+            expect(mapFieldKey('dateOfBirth', i18n, null)).toEqual(DATE_OF_BIRTH);
+
+            // Not an address related field, should not be mapped
+            expect(mapFieldKey('firstName', i18n, null)).toEqual(null);
+
+            // should split and use labels spec to return translated string in expected format with country specific variation
+            expect(mapFieldKey('billingAddress:postalCode', i18n, labelSpecs)).toEqual(`${BILLING_ADDRESS} ${ZIP_CODE}`);
         });
     });
 });
