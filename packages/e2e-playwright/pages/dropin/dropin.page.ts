7e2187c47aedc9cbefcaee3133cac4646dd5903d
Yu Long <longyu901009@gmail.com>
2024-04-04 12:04:45 +0200

    Ci: run e2e against multiple api versions (#2627)
    * fix(e2e): locate pm based on api response
    
    (cherry picked from commit 9902c457f47549623b744c2428a1f44b445cf96d)

--- packages/e2e-playwright/pages/dropin/dropin.page.ts
+++ packages/e2e-playwright/pages/dropin/dropin.page.ts
@@ -1,25 +1,26 @@
-import { Locator, Page } from '@playwright/test';
+import { Page } from '@playwright/test';
 import { Dropin } from '../../models/dropin';
 
 class DropinPage {
     readonly page: Page;
 
     readonly dropin: Dropin;
-    // readonly payButton: Locator;
+    private _paymentMethods: Array<{ name: string; type: string }>;
 
     constructor(page: Page) {
         this.page = page;
-        this.dropin = new Dropin(page);
-        // this.payButton = page.getByRole('button', { name: /Pay/i });
+        this.dropin = new Dropin(page, this);
     }
 
     async goto(url?: string) {
         await this.page.goto('http://localhost:3024');
+        const response = await this.page.waitForResponse(response => response.url().includes('paymentMethods') && response.status() === 200);
+        this._paymentMethods = (await response.json()).paymentMethods.map(({ name, type }: { name: string; type: string }) => ({ name, type }));
     }
 
-    // async pay() {
-    //     await this.payButton.click();
-    // }
+    get paymentMethods() {
+        return this._paymentMethods;
+    }
 }
 
 export { DropinPage };
