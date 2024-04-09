8e8f3760a014c074d6929afca93e4880c5052612
Guilherme Ribeiro <guilhermemrr@gmail.com>
2024-04-09 10:28:59 -0300

    v6 - Fix for 'auto' bundle (#2633)

--- packages/lib/auto/auto.cjs
+++ packages/lib/auto/auto.cjs
@@ -1,3 +1,9 @@
+/**
+ * ATTENTION: Do not import anything that is not available inside the /dist folder .
+ * 'auto' uses the files that are packed when running 'npm pack'. Make sure that when adding another import, the file
+ * is available when generating the tarball that will be published on npm
+ */
+
 const library = require('../dist/cjs/index.cjs');
 const { AdyenCheckout, components } = library;
 
@@ -5,5 +11,6 @@ const { Dropin, ...Components } = components;
 const Classes = Object.keys(Components).map(key => Components[key]);
 
 AdyenCheckout.register(...Classes);
+AdyenCheckout.setBundleType('auto');
 
 module.exports = library;
