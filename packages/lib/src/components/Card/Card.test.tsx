642eaf2312d5100f339bab0c89d4435bf3d6c0c9
guilhermer <guilherme.ribeiro@adyen.com>
2024-04-15 14:21:40 +0200

    fix card test merge issue

--- packages/lib/src/components/Card/Card.test.tsx
+++ packages/lib/src/components/Card/Card.test.tsx
@@ -1,7 +1,7 @@
 import { h } from 'preact';
 import { CardElement } from './Card';
 import { render, screen } from '@testing-library/preact';
-import CoreProvider from '../../core/Context/CoreProvider';
+import { CoreProvider } from '../../core/Context/CoreProvider';
 import { Resources } from '../../core/Context/Resources';
 
 describe('Card', () => {
