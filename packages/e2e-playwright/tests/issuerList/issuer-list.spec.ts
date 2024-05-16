b2cc4ba237df5032dd07c204fbdf54814edfff74
nicholas <nicholas.spong@adyen.com>
2024-05-16 11:35:07 +0200

    Deleted iDEal e2e test in testcafe. Added TODO note to playwright test to test another issuerList component instead

--- packages/e2e-playwright/tests/issuerList/issuer-list.spec.ts
+++ packages/e2e-playwright/tests/issuerList/issuer-list.spec.ts
@@ -1,52 +1,60 @@
 import { test, expect } from '../../pages/issuerList/issuer-list.fixture';
-import { pressKeyboardToNextItem, pressKeyboardToSelectItem } from '../utils/keyboard';
+// import { pressKeyboardToNextItem, pressKeyboardToSelectItem } from '../utils/keyboard';
 
 test.describe('Issuer List', () => {
-    test('should select highlighted issuer and update pay button label', async ({ issuerListPage }) => {
-        const { issuerList } = issuerListPage;
-
-        await issuerList.selectHighlightedIssuer('Test Issuer 5');
-        await expect(issuerList.submitButton).toHaveText('Continue to Test Issuer 5');
-
-        await issuerList.selectHighlightedIssuer('Test Issuer 4');
-        await expect(issuerList.submitButton).toHaveText('Continue to Test Issuer 4');
-
-        await expect(issuerList.highlightedIssuerButtonGroup.getByRole('button', { pressed: true })).toHaveText('Test Issuer 4');
-    });
-
-    test('it should be able to filter and select using the keyboard', async ({ issuerListPage }) => {
-        const { issuerList, page } = issuerListPage;
-
-        await expect(issuerList.submitButton).toHaveText('Continue');
-
-        await issuerList.clickOnSelector();
-        await expect(issuerList.selectorList).toContainText('SNS');
-
-        await issuerList.typeOnSelectorField('Test');
-        await expect(issuerList.selectorList).not.toContainText('SNS');
-
-        await pressKeyboardToNextItem(page);
-        await pressKeyboardToNextItem(page);
-        await pressKeyboardToSelectItem(page);
-
-        await expect(issuerList.submitButton).toHaveText('Continue to Test Issuer 5');
-
-        // 1st press opens the dropdown
-        await pressKeyboardToNextItem(page);
-        // 2nd selects next items
-        await pressKeyboardToNextItem(page);
-        await pressKeyboardToSelectItem(page);
-
-        await expect(issuerList.submitButton).toHaveText('Continue to iDeal Test Issuer');
-    });
-
-    test('it should load a default when pressing enter', async ({ issuerListPage }) => {
-        const { issuerList, page } = issuerListPage;
-
-        await issuerList.clickOnSelector();
-        await issuerList.typeOnSelectorField('Test');
-        await pressKeyboardToSelectItem(page);
-
-        await expect(issuerList.submitButton).toHaveText('Continue to Test Issuer');
-    });
+    // TODO use this space to test another issuerList component
+    // test('should select highlighted issuer and update pay button label', async ({ issuerListPage }) => {
+    //     const { issuerList } = issuerListPage;
+    //
+    //     await issuerList.selectHighlightedIssuer('Test Issuer 5');
+    //     await expect(issuerList.submitButton).toHaveText('Continue to Test Issuer 5');
+    //
+    //     await issuerList.selectHighlightedIssuer('Test Issuer 4');
+    //     await expect(issuerList.submitButton).toHaveText('Continue to Test Issuer 4');
+    //
+    //     await expect(issuerList.highlightedIssuerButtonGroup.getByRole('button', { pressed: true })).toHaveText('Test Issuer 4');
+    // });
+    //
+    // test('it should be able to filter and select using the keyboard', async ({ issuerListPage }) => {
+    //     const { issuerList, page } = issuerListPage;
+    //
+    //     await expect(issuerList.submitButton).toHaveText('Continue');
+    //
+    //     await issuerList.clickOnSelector();
+    //     await expect(issuerList.selectorList).toContainText('SNS');
+    //
+    //     await issuerList.typeOnSelectorField('Test');
+    //     await expect(issuerList.selectorList).not.toContainText('SNS');
+    //
+    //     await pressKeyboardToNextItem(page);
+    //     await pressKeyboardToNextItem(page);
+    //     await pressKeyboardToSelectItem(page);
+    //
+    //     await expect(issuerList.submitButton).toHaveText('Continue to Test Issuer 5');
+    //
+    //     // 1st press opens the dropdown
+    //     await pressKeyboardToNextItem(page);
+    //     // 2nd selects next items
+    //     await pressKeyboardToNextItem(page);
+    //     await pressKeyboardToSelectItem(page);
+    //
+    //     await expect(issuerList.submitButton).toHaveText('Continue to iDeal Test Issuer');
+    // });
+    //
+    // test('it should load a default when pressing enter', async ({ issuerListPage }) => {
+    //     const { issuerList, page } = issuerListPage;
+    //
+    //     await issuerList.clickOnSelector();
+    //     await issuerList.typeOnSelectorField('Test');
+    //     await pressKeyboardToSelectItem(page);
+    //
+    //     await expect(issuerList.submitButton).toHaveText('Continue to Test Issuer');
+    // });
+    //
+    // 2DO - Following 2 tests existed in testcafe but not playwright
+    // test('should make an  payment', async t => {
+    // });
+    //
+    // test('should make an  payment using a highlighted issuer', async t => {
+    // });
 });
