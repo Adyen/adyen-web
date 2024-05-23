882d169c3eb3b5da00c490d522901fa92056954d
nicholas <nicholas.spong@adyen.com>
2024-05-23 11:31:35 +0200

    Adding IssuerList playwright e2e test

--- packages/e2e-playwright/tests/issuerList/issuer-list.spec.ts
+++ packages/e2e-playwright/tests/issuerList/issuer-list.spec.ts
@@ -1,60 +1,79 @@
 import { test, expect } from '../../pages/issuerList/issuer-list.fixture';
-// import { pressKeyboardToNextItem, pressKeyboardToSelectItem } from '../utils/keyboard';
+import { pressKeyboardToNextItem, pressKeyboardToSelectItem } from '../utils/keyboard';
 
 test.describe('Issuer List', () => {
-    // TODO use this space to test another issuerList component
-    // test('should select highlighted issuer and update pay button label', async ({ issuerListPage }) => {
-    //     const { issuerList } = issuerListPage;
-    //
-    //     await issuerList.selectHighlightedIssuer('Test Issuer 5');
-    //     await expect(issuerList.submitButton).toHaveText('Continue to Test Issuer 5');
-    //
-    //     await issuerList.selectHighlightedIssuer('Test Issuer 4');
-    //     await expect(issuerList.submitButton).toHaveText('Continue to Test Issuer 4');
-    //
-    //     await expect(issuerList.highlightedIssuerButtonGroup.getByRole('button', { pressed: true })).toHaveText('Test Issuer 4');
-    // });
-    //
-    // test('it should be able to filter and select using the keyboard', async ({ issuerListPage }) => {
-    //     const { issuerList, page } = issuerListPage;
-    //
-    //     await expect(issuerList.submitButton).toHaveText('Continue');
-    //
-    //     await issuerList.clickOnSelector();
-    //     await expect(issuerList.selectorList).toContainText('SNS');
-    //
-    //     await issuerList.typeOnSelectorField('Test');
-    //     await expect(issuerList.selectorList).not.toContainText('SNS');
-    //
-    //     await pressKeyboardToNextItem(page);
-    //     await pressKeyboardToNextItem(page);
-    //     await pressKeyboardToSelectItem(page);
-    //
-    //     await expect(issuerList.submitButton).toHaveText('Continue to Test Issuer 5');
-    //
-    //     // 1st press opens the dropdown
-    //     await pressKeyboardToNextItem(page);
-    //     // 2nd selects next items
-    //     await pressKeyboardToNextItem(page);
-    //     await pressKeyboardToSelectItem(page);
-    //
-    //     await expect(issuerList.submitButton).toHaveText('Continue to Test Issuer 4');
-    // });
-    //
-    // test('it should load a default when pressing enter', async ({ issuerListPage }) => {
-    //     const { issuerList, page } = issuerListPage;
-    //
-    //     await issuerList.clickOnSelector();
-    //     await issuerList.typeOnSelectorField('Test');
-    //     await pressKeyboardToSelectItem(page);
-    //
-    //     await expect(issuerList.submitButton).toHaveText('Continue to Test Issuer');
-    // });
-    //
-    // 2DO - Following 2 tests existed in testcafe but not playwright
-    // test('should make an  payment', async t => {
-    // });
-    //
-    // test('should make an  payment using a highlighted issuer', async t => {
-    // });
+    test('it should be able to filter and select using the keyboard', async ({ issuerListPage }) => {
+        const { issuerList, page } = issuerListPage;
+
+        await expect(issuerList.submitButton).toHaveText('Continue');
+
+        await issuerList.clickOnSelector();
+        await expect(issuerList.selectorList).toContainText('mTransfer'); // full list
+
+        await issuerList.typeOnSelectorField('Idea'); // filtered content
+        await expect(issuerList.selectorList).not.toContainText('mTransfer');
+
+        // select one of the filtered option
+        await pressKeyboardToNextItem(page); // Arrow down
+        await pressKeyboardToSelectItem(page); // Enter key
+
+        await expect(issuerList.submitButton).toHaveText('Continue to Idea Cloud');
+
+        // 1st press opens the dropdown
+        await pressKeyboardToNextItem(page);
+        // 2nd selects next item
+        await pressKeyboardToNextItem(page);
+        await pressKeyboardToSelectItem(page);
+
+        await expect(issuerList.submitButton).toHaveText('Continue to mRaty');
+    });
+
+    test('it should load a default, from the filtered items, when pressing enter', async ({ issuerListPage }) => {
+        const { issuerList, page } = issuerListPage;
+
+        await issuerList.clickOnSelector();
+        await issuerList.typeOnSelectorField('Nest');
+        await pressKeyboardToSelectItem(page);
+
+        await expect(issuerList.submitButton).toHaveText('Continue to Nest Bank');
+    });
+
+    test('it should have the expected data in state, ready for the /payments call', async ({ issuerListPage }) => {
+        const { issuerList, page } = issuerListPage;
+
+        // Open the drop down and select an item
+        await issuerList.clickOnSelector();
+        await pressKeyboardToNextItem(page); // Arrow down
+        await pressKeyboardToSelectItem(page); // Enter key
+
+        let issuerListData = await page.evaluate('window.dotpay.data');
+
+        // @ts-ignore
+        expect(issuerListData.paymentMethod).toEqual({
+            type: 'dotpay',
+            issuer: '73',
+            checkoutAttemptId: 'do-not-track'
+        });
+    });
+
+    test('should select highlighted issuer, update pay button label, and see the expected data in state', async ({ issuerListPage }) => {
+        const { issuerList, page } = issuerListPage;
+
+        await issuerList.selectHighlightedIssuer('BLIK');
+        await expect(issuerList.submitButton).toHaveText('Continue to BLIK');
+
+        await issuerList.selectHighlightedIssuer('Idea Cloud');
+        await expect(issuerList.submitButton).toHaveText('Continue to Idea Cloud');
+
+        await expect(issuerList.highlightedIssuerButtonGroup.getByRole('button', { pressed: true })).toHaveText('Idea Cloud');
+
+        let issuerListData = await page.evaluate('window.dotpay.data');
+
+        // @ts-ignore
+        expect(issuerListData.paymentMethod).toEqual({
+            type: 'dotpay',
+            issuer: '81',
+            checkoutAttemptId: 'do-not-track'
+        });
+    });
 });
