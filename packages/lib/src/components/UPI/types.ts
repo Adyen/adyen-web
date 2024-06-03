080f15b60625778a9bc48ee6a051f43a22fb82e6
Yu Long <longyu901009@gmail.com>
2024-06-03 17:42:56 +0200

    feat: add Upi intent flow (#2673)
    * Add translations
    
    * refactor: more tests for onChange
    
    * refactor: await component supports shouldRedirectAutomatically, use await for the upi intent apps instead of redirect
    
    * refactor(upi-app-list): disable selection on loading
    
    * fix logo path
    
    * remove app list for upi story

--- packages/lib/src/components/UPI/types.ts
+++ packages/lib/src/components/UPI/types.ts
@@ -2,18 +2,35 @@ import { UIElementProps } from '../types';
 
 export type UpiPaymentData = {
     paymentMethod: {
-        type: 'upi_qr' | 'upi_collect';
+        type: TX_VARIANT;
         virtualPaymentAddress?: string;
+        appId?: string;
     };
 };
 
 export enum UpiMode {
     Vpa = 'vpa',
-    QrCode = 'qrCode'
+    QrCode = 'qrCode',
+    Intent = 'intent'
 }
 
+export enum TX_VARIANT {
+    UpiCollect = 'upi_collect',
+    UpiQr = 'upi_qr',
+    UpiIntent = 'upi_intent'
+}
+
+export type App = { id: string; name: string; type?: TX_VARIANT };
+
 export interface UPIElementProps extends UIElementProps {
-    defaultMode: UpiMode;
+    defaultMode?: UpiMode;
+    // upi_intent
+    apps?: Array<App>;
+    /**
+     * Redirect url for upi intent apps
+     * @internal
+     */
+    url?: string;
     // Await
     paymentData?: string;
     // QR code
