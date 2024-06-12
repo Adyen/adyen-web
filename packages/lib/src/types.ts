bac8420786130cbf264c749462911088ce7c2e46
guilhermer <guilherme.ribeiro@adyen.com>
2024-06-12 15:26:43 +0200

    exporting uielement as part of the types

--- packages/lib/src/types.ts
+++ packages/lib/src/types.ts
@@ -4,3 +4,4 @@ export * from './core/types';
 export * from './types/global-types';
 
 export { default as AdyenCheckoutError } from './core/Errors/AdyenCheckoutError';
+export { default as UIElement } from './components/internal/UIElement';
