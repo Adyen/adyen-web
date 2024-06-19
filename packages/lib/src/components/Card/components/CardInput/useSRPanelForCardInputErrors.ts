8d5dbb1cf1432bf38735315eeaad4a00f79432c1
nicholas <nicholas.spong@adyen.com>
2024-06-19 12:54:40 +0200

    Adding precautionary try...catch

--- packages/lib/src/components/Card/components/CardInput/useSRPanelForCardInputErrors.ts
+++ packages/lib/src/components/Card/components/CardInput/useSRPanelForCardInputErrors.ts
@@ -28,62 +28,66 @@ const useSRPanelForCardInputErrors = ({ errors, props, isValidating, retrieveLay
     const mergedErrors = { ...errors, ...sfStateErrorsObj };
 
     useEffect(() => {
-        // Extract and then flatten billingAddress errors into a new object with *all* the field errors at top level
-        const { billingAddress: extractedAddressErrors, ...errorsWithoutAddress } = mergedErrors;
+        try {
+            // Extract and then flatten billingAddress errors into a new object with *all* the field errors at top level
+            const { billingAddress: extractedAddressErrors, ...errorsWithoutAddress } = mergedErrors;
 
-        const errorsForPanel = { ...errorsWithoutAddress, ...extractedAddressErrors };
+            const errorsForPanel = { ...errorsWithoutAddress, ...extractedAddressErrors };
 
-        // Pass dynamic props (errors, layout etc) to SRPanel via partial
-        const srPanelResp: SetSRMessagesReturnObject = setSRMessages?.({
-            errors: errorsForPanel,
-            isValidating: isValidating.current,
-            layout: retrieveLayout(),
-            // If we don't have country specific address labels, we might have a label related to a partialAddressSchema (i.e. zipCode)
-            countrySpecificLabels: specifications.getAddressLabelsForCountry(billingAddress?.country) ?? partialAddressSchema?.default?.labels
-        });
+            // Pass dynamic props (errors, layout etc) to SRPanel via partial
+            const srPanelResp: SetSRMessagesReturnObject = setSRMessages?.({
+                errors: errorsForPanel,
+                isValidating: isValidating.current,
+                layout: retrieveLayout(),
+                // If we don't have country specific address labels, we might have a label related to a partialAddressSchema (i.e. zipCode)
+                countrySpecificLabels: specifications.getAddressLabelsForCountry(billingAddress?.country) ?? partialAddressSchema?.default?.labels
+            });
 
-        // Store the array of sorted error objects separately so that we can use it to make comparisons between the old and new arrays
-        const currentErrorsSortedByLayout = srPanelResp?.currentErrorsSortedByLayout;
-        setSortedErrorList(currentErrorsSortedByLayout);
+            // Store the array of sorted error objects separately so that we can use it to make comparisons between the old and new arrays
+            const currentErrorsSortedByLayout = srPanelResp?.currentErrorsSortedByLayout;
+            setSortedErrorList(currentErrorsSortedByLayout);
 
-        /**
-         * Need extra actions after setting SRPanel messages in order to focus field (if required) and because we have some errors that are fired onBlur
-         */
-        switch (srPanelResp?.action) {
-            // A call to focus the first field in error will always follow the call to validate the whole form
-            case ERROR_ACTION_FOCUS_FIELD:
-                if (shouldMoveFocusSR) setFocusOnFirstField(isValidating.current, sfp, srPanelResp?.fieldToFocus);
-                // Remove 'showValidation' mode - allowing time for collation of all the fields in error whilst it is 'showValidation' mode (some errors come in a second render pass)
-                setTimeout(() => {
-                    isValidating.current = false;
-                }, 300);
-                break;
-            /** On blur scenario: not validating, i.e. trying to submit form, but there might be an error, either to set or to clear */
-            case ERROR_ACTION_BLUR_SCENARIO: {
-                const difference = getArrayDifferences<SortedErrorObject, string>(currentErrorsSortedByLayout, previousSortedErrors, 'field');
+            /**
+             * Need extra actions after setting SRPanel messages in order to focus field (if required) and because we have some errors that are fired onBlur
+             */
+            switch (srPanelResp?.action) {
+                // A call to focus the first field in error will always follow the call to validate the whole form
+                case ERROR_ACTION_FOCUS_FIELD:
+                    if (shouldMoveFocusSR) setFocusOnFirstField(isValidating.current, sfp, srPanelResp?.fieldToFocus);
+                    // Remove 'showValidation' mode - allowing time for collation of all the fields in error whilst it is 'showValidation' mode (some errors come in a second render pass)
+                    setTimeout(() => {
+                        isValidating.current = false;
+                    }, 300);
+                    break;
+                /** On blur scenario: not validating, i.e. trying to submit form, but there might be an error, either to set or to clear */
+                case ERROR_ACTION_BLUR_SCENARIO: {
+                    const difference = getArrayDifferences<SortedErrorObject, string>(currentErrorsSortedByLayout, previousSortedErrors, 'field');
 
-                const latestErrorMsg = difference?.[0];
+                    const latestErrorMsg = difference?.[0];
 
-                if (latestErrorMsg) {
-                    // Use the error code to look up whether error is actually a blur based one (most are but some SF ones aren't)
-                    const isBlurBasedError = lookupBlurBasedErrors(latestErrorMsg.errorCode);
+                    if (latestErrorMsg) {
+                        // Use the error code to look up whether error is actually a blur based one (most are but some SF ones aren't)
+                        const isBlurBasedError = lookupBlurBasedErrors(latestErrorMsg.errorCode);
 
-                    /**
-                     *  ONLY ADD BLUR BASED ERRORS TO THE ERROR PANEL - doing this step prevents the non-blur based errors from being read out twice
-                     *  (once from the aria-live, error panel & once from the aria-describedby element)
-                     */
-                    const latestSRError = isBlurBasedError ? latestErrorMsg.errorMessage : null;
-                    // console.log('### CardInput2::componentDidUpdate:: #2 (not validating) single error:: latestSRError', latestSRError);
-                    setSRMessagesFromStrings(latestSRError);
-                } else {
-                    // called when previousSortedErrors.length >= currentErrorsSortedByLayout.length
-                    // console.log('### CardInput2::componentDidUpdate:: #3(not validating) clearing errors:: NO latestErrorMsg');
-                    clearSRPanel();
+                        /**
+                         *  ONLY ADD BLUR BASED ERRORS TO THE ERROR PANEL - doing this step prevents the non-blur based errors from being read out twice
+                         *  (once from the aria-live, error panel & once from the aria-describedby element)
+                         */
+                        const latestSRError = isBlurBasedError ? latestErrorMsg.errorMessage : null;
+                        // console.log('### CardInput2::componentDidUpdate:: #2 (not validating) single error:: latestSRError', latestSRError);
+                        setSRMessagesFromStrings(latestSRError);
+                    } else {
+                        // called when previousSortedErrors.length >= currentErrorsSortedByLayout.length
+                        // console.log('### CardInput2::componentDidUpdate:: #3(not validating) clearing errors:: NO latestErrorMsg');
+                        clearSRPanel();
+                    }
+                    break;
                 }
-                break;
+                default:
+                    break;
             }
-            default:
-                break;
+        } catch (_) {
+            // We don't handle the error related to the sr panel, let it fail silently.
         }
     }, [errors]);
 
