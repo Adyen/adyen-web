0135a99b699909cdc155a7740a660f7ab89ded27
nicholas <nicholas.spong@adyen.com>
2024-06-20 12:09:59 +0200

    Added unit tests for searchObject util

--- packages/lib/src/components/internal/UIElement/utils.test.ts
+++ packages/lib/src/components/internal/UIElement/utils.test.ts
@@ -1,4 +1,4 @@
-import { sanitizeResponse } from './utils';
+import { sanitizeResponse, searchObject } from './utils';
 
 describe('components utils', () => {
     describe('getSanitizedResponse', () => {
@@ -17,6 +17,129 @@ describe('components utils', () => {
             expect(sanitizedResponse.resultCode).toBeTruthy();
             expect(sanitizedResponse.sessionResult).toBeTruthy();
             expect((sanitizedResponse as any).someBackendProperty).toBeUndefined();
+        });
+    });
+
+    describe.only('searchObject util', () => {
+        test('test that no results are found because there is nothing to search', () => {
+            const errors = null;
+
+            const result = searchObject(errors, 'age');
+
+            expect(result.length).toEqual(0);
+        });
+
+        test('test that no results are found because there is no matching entry', () => {
+            const errors = {
+                personalDetails: {
+                    firstName: null,
+                    lastName: null,
+                    telephoneNumber: null,
+                    shopperEmail: null
+                },
+                billingAddress: {
+                    country: null,
+                    street: null,
+                    houseNumberOrName: null,
+                    city: null,
+                    stateOrProvince: null,
+                    postalCode: null,
+                    firstName: null,
+                    lastName: null
+                }
+            };
+
+            const result = searchObject(errors, 'age');
+
+            expect(result.length).toEqual(0);
+        });
+
+        test('test that no results are found because there is not a "truthy" values', () => {
+            const errors = {
+                personalDetails: {
+                    firstName: null,
+                    lastName: null,
+                    telephoneNumber: null,
+                    shopperEmail: null
+                },
+                billingAddress: {
+                    country: null,
+                    street: null,
+                    houseNumberOrName: null,
+                    city: null,
+                    stateOrProvince: null,
+                    postalCode: null,
+                    firstName: null,
+                    lastName: null
+                }
+            };
+
+            const result = searchObject(errors, 'firstName');
+
+            expect(result.length).toEqual(0);
+        });
+
+        test('test that a result is found because there a "truthy" value: a boolean', () => {
+            const errors = {
+                personalDetails: {
+                    firstName: null,
+                    lastName: null,
+                    telephoneNumber: null,
+                    shopperEmail: null
+                },
+                billingAddress: {
+                    country: null,
+                    street: null,
+                    houseNumberOrName: null,
+                    city: null,
+                    stateOrProvince: null,
+                    postalCode: true,
+                    firstName: null,
+                    lastName: null
+                }
+            };
+
+            const result = searchObject(errors, 'postalCode');
+
+            expect(result.length).toEqual(1);
+        });
+
+        test('test that a result is found because there a "truthy" value: an object', () => {
+            const errors = {
+                personalDetails: {
+                    firstName: null,
+                    lastName: null,
+                    telephoneNumber: null,
+                    shopperEmail: { hasError: true }
+                },
+                billingAddress: {
+                    country: null,
+                    street: null,
+                    houseNumberOrName: null,
+                    city: null,
+                    stateOrProvince: null,
+                    postalCode: true,
+                    firstName: null,
+                    lastName: null
+                }
+            };
+
+            const result = searchObject(errors, 'shopperEmail');
+
+            expect(result.length).toEqual(1);
+        });
+
+        test('test that a result is found in a object without nesting', () => {
+            const errors = {
+                holderName: null,
+                socialSecurityNumber: true,
+                taxNumber: null,
+                billingAddress: null
+            };
+
+            const result = searchObject(errors, 'socialSecurityNumber');
+
+            expect(result.length).toEqual(1);
         });
     });
 });
