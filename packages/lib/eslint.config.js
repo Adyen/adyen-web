26e21f6b8bf8fd69e33dac9fe0c6d3acd6096326
guilhermer <guilherme.ribeiro@adyen.com>
2024-06-27 15:39:56 +0200

    cleanup package json

--- packages/lib/eslint.config.js
+++ packages/lib/eslint.config.js
@@ -42,14 +42,12 @@ const config = tseslint.config(
                 }
             ],
 
-            // maybe apply for test files?
             '@typescript-eslint/no-unsafe-assignment': 'off',
             '@typescript-eslint/no-unsafe-return': 'off',
             '@typescript-eslint/no-unsafe-call': 'off',
             '@typescript-eslint/no-unsafe-argument': 'off',
             '@typescript-eslint/no-unsafe-member-access': 'off',
             '@typescript-eslint/unbound-method': 'off',
-
             '@typescript-eslint/no-explicit-any': 'off',
             '@typescript-eslint/ban-types': 'off',
             '@typescript-eslint/ban-ts-comment': [
