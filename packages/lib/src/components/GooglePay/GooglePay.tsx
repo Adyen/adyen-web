4941341fa3ec4e2ff93cc0e131fe62c655148eaf
guilhermer <guilherme.ribeiro@adyen.com>
2024-07-10 11:47:52 +0200

    googlepay-adding-borderRadius-support

--- packages/lib/src/components/GooglePay/GooglePay.tsx
+++ packages/lib/src/components/GooglePay/GooglePay.tsx
@@ -139,6 +139,7 @@ class GooglePay extends UIElement<GooglePayProps> {
                     buttonSizeMode={this.props.buttonSizeMode}
                     buttonLocale={this.props.buttonLocale}
                     buttonRootNode={this.props.buttonRootNode}
+                    buttonRadius={this.props.buttonRadius}
                     paymentsClient={this.googlePay.paymentsClient}
                     onClick={this.submit}
                 />
