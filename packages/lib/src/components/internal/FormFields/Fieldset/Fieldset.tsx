504145c30ada4df5d202e9503e4ffd2ef3cbc972
nicholas <nicholas.spong@adyen.com>
2024-07-15 16:54:43 +0200

    Fieldset - ensure children are always wrapped in a containing div

--- packages/lib/src/components/internal/FormFields/Fieldset/Fieldset.tsx
+++ packages/lib/src/components/internal/FormFields/Fieldset/Fieldset.tsx
@@ -24,13 +24,13 @@ export default function Fieldset({ children, classNameModifiers = [], label, rea
         >
             {label && <legend className="adyen-checkout__fieldset__title">{i18n.get(label)}</legend>}
 
+            {/*propagate the uniqueId, if we've been given one*/}
             {props.uniqueId ? (
-                toChildArray(children).map((child: ComponentChild): ComponentChild => {
-                    const childProps = {
-                        ...(props.uniqueId && { uniqueId: props.uniqueId }) // propagate the uniqueId, if we've been given one
-                    };
-                    return cloneElement(child as VNode, childProps);
-                })
+                <div className="adyen-checkout__fieldset__fields">
+                    {toChildArray(children).map((child: ComponentChild): ComponentChild => {
+                        return cloneElement(child as VNode, { uniqueId: props.uniqueId });
+                    })}
+                </div>
             ) : (
                 <div className="adyen-checkout__fieldset__fields">{children}</div>
             )}
