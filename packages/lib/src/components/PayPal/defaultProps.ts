e0bd49c1f8508dd6bbad8273eaed4ac2503b9e43
Carsten Boensel <cboensel@paypal.com>
2024-09-02 17:02:18 +0200

    Fix PayLater not showing up (#2829)

--- packages/lib/src/components/PayPal/defaultProps.ts
+++ packages/lib/src/components/PayPal/defaultProps.ts
@@ -9,8 +9,8 @@ const defaultProps: Partial<PayPalConfiguration> = {
     style: {
         height: 50 // The height of the buttons is set to 50px to align with the default size of our Pay buttons.
     },
-    blockPayPalCreditButton: false,
-    blockPayPalPayLaterButton: false,
+    blockPayPalCreditButton: true, // If both Credit and Pay Later being enabled, Credit takes precedence.
+    blockPayPalPayLaterButton: false, // Pay Later is considered the more modern PayPal Credit offering.
     blockPayPalVenmoButton: false
 };
 
