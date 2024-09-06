db1a712106bfec763579ae659a80c0170e353258
sponglord <nicholas.spong@adyen.com>
2024-09-06 11:56:18 +0200

    Added ACH story (#2821)
    * Added ACH story
    
    * Updated comment

--- packages/lib/src/components/Ach/types.ts
+++ packages/lib/src/components/Ach/types.ts
@@ -1,11 +1,20 @@
 import { Placeholders } from './components/AchInput/types';
 import { UIElementProps } from '../internal/UIElement/types';
+import { AddressData } from '../../types/global-types';
 
 export interface AchConfiguration extends UIElementProps {
     storedPaymentMethodId?: string;
     holderNameRequired?: boolean;
     hasHolderName?: boolean;
     enableStoreDetails: boolean;
-    bankAccountNumber: string;
+    bankAccountNumber?: string; // Applies when a storedPM
     placeholders?: Placeholders;
+    /**
+     * Object that contains placeholder information that you can use to prefill fields.
+     * - merchant set config option
+     */
+    data?: {
+        holderName?: string;
+        billingAddress?: Partial<AddressData>;
+    };
 }
