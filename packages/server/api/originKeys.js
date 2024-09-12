635c5c6030ba9745d394a37793221b60d45b6ab1
antoniof <m1aw@users.noreply.github.com>
2024-09-12 11:39:48 +0200

    remove unsuded code

--- packages/server/api/originKeys.js
+++
@@ -1,8 +0,0 @@
-const makePostRequest = require('../utils/makePostRequest');
-const handleCallback = require('../utils/handleCallback');
-
-module.exports = async (res, request) => {
-    const originDomains = [`${request.protocol}://${request.headers.host}`];
-    const response = await makePostRequest('originKeys', { originDomains });
-    handleCallback(response, res);
-};
