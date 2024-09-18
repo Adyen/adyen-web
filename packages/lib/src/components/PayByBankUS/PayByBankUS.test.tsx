3f2c2ca317faaa5ee3eb66006d6da99a4aac88de
antoniof <m1aw@users.noreply.github.com>
2024-09-18 15:52:41 +0200

    fix tests

--- packages/lib/src/components/PayByBankUS/PayByBankUS.test.tsx
+++ packages/lib/src/components/PayByBankUS/PayByBankUS.test.tsx
@@ -1,40 +1,39 @@
 import { render, screen } from '@testing-library/preact';
-import Trustly from './PayByBankUS';
+import PayByBankUS from './PayByBankUS';
 
-describe('TrustlyElement', () => {
+describe('PayByBank US', () => {
     test('should render payment description by default', async () => {
-        const trustly = new Trustly(global.core, {
+        const pbb = new PayByBankUS(global.core, {
             i18n: global.i18n,
             loadingContext: 'test',
             modules: { resources: global.resources }
         });
 
-        render(trustly.render());
-        expect(await screen.findByText(/instant bank payment/i)).toBeTruthy();
-        expect(await screen.findByText(/pay directly from any of your bank accounts/i)).toBeTruthy();
-        expect(await screen.findByText(/no cards, no app download, no registration/i)).toBeTruthy();
+        render(pbb.render());
+        expect(await screen.findByText(/Use Pay by Bank to pay/i)).toBeTruthy();
+        expect(await screen.findByText(/By connecting your bank account/i)).toBeTruthy();
     });
 
     test('should render redirect button by default', async () => {
-        const trustly = new Trustly(global.core, {
+        const pbb = new PayByBankUS(global.core, {
             i18n: global.i18n,
             loadingContext: 'test',
             modules: { resources: global.resources }
         });
 
-        render(trustly.render());
-        expect(await screen.findByRole('button')).toHaveTextContent('Continue to trustly');
+        render(pbb.render());
+        expect(await screen.findByRole('button')).toHaveTextContent('Continue to');
     });
 
     test('should not render pay button if showPayButton is false', () => {
-        const trustly = new Trustly(global.core, {
+        const pbb = new PayByBankUS(global.core, {
             i18n: global.i18n,
             loadingContext: 'test',
             modules: { resources: global.resources },
             showPayButton: false
         });
 
-        render(trustly.render());
+        render(pbb.render());
         expect(screen.queryByRole('button')).not.toBeInTheDocument();
     });
 });
