3caaf43053cfbf21b1c30cc1151e3cb50cdd3e60
Guilherme Ribeiro <guilhermemrr@gmail.com>
2024-09-25 18:00:25 +0200

    v6 cherry-pick - Click to Pay: Adding Visa timeout logging (#2869)
    * cherry-picked code for v6
    
    * fix toString lint issue

--- packages/lib/src/components/internal/ClickToPay/errors/TimeoutError.ts
+++ packages/lib/src/components/internal/ClickToPay/errors/TimeoutError.ts
@@ -1,11 +1,32 @@
+interface TimeoutErrorProps {
+    source: string;
+    scheme: string;
+    isTimeoutTriggeredBySchemeSdk: boolean;
+}
+
 class TimeoutError extends Error {
-    constructor(message: string) {
-        super(message);
+    public scheme: string;
+    public source: string;
+    public isTimeoutTriggeredBySchemeSdk: boolean;
+
+    /** Currently populated only by Visa SDK if available */
+    public correlationId?: string;
+
+    constructor(options: TimeoutErrorProps) {
+        super(`ClickToPayService - Timeout during ${options.source}() of the scheme '${options.scheme}'`);
+
         this.name = 'TimeoutError';
+        this.source = options.source;
+        this.scheme = options.scheme;
+        this.isTimeoutTriggeredBySchemeSdk = options.isTimeoutTriggeredBySchemeSdk;
     }
 
-    toString() {
-        return `Message: ${this.message}`;
+    public setCorrelationId(correlationId: string): void {
+        this.correlationId = correlationId;
+    }
+
+    public toString() {
+        return this.message;
     }
 }
 
