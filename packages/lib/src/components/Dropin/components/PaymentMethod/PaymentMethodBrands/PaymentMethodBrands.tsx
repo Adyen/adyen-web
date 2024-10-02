bdc10ccc14caf97dfdfb8d4a737e8a3af76c9db1
António Ferreira <m1aw@users.noreply.github.com>
2024-10-02 09:52:15 +0200

    Whitelabel Pay by Bank in the US (#2861)
    * initial pay by bank us work
    
    * make config to always show logos
    
    * add changeset
    
    * revert yarn.lock
    
    * fix tests
    
    * stored pay by bank and code review changes
    
    * some more tests and code rivew fixes
    
    * translations
    
    * fixes css on small screens

--- packages/lib/src/components/Dropin/components/PaymentMethod/PaymentMethodBrands/PaymentMethodBrands.tsx
+++ packages/lib/src/components/Dropin/components/PaymentMethod/PaymentMethodBrands/PaymentMethodBrands.tsx
@@ -2,6 +2,7 @@ import { h } from 'preact';
 import { BrandConfiguration } from '../../../../Card/types';
 import PaymentMethodIcon from '../PaymentMethodIcon';
 import { getFullBrandName } from '../../../../Card/components/CardInput/utils';
+import { useCoreContext } from '../../../../../core/Context/CoreProvider';
 
 const prepareVisibleBrands = (allowedBrands: Array<BrandConfiguration>) => {
     const visibleBrands = allowedBrands.length <= 4 ? allowedBrands : allowedBrands.slice(0, 3);
@@ -15,10 +16,20 @@ interface PaymentMethodBrandsProps {
     brands: Array<BrandConfiguration>;
     excludedUIBrands?: Array<string>;
     isPaymentMethodSelected: boolean;
+    keepBrandsVisible?: boolean;
+    showOtherInsteafOfNumber?: boolean;
 }
 
-const PaymentMethodBrands = ({ brands, excludedUIBrands = [], isPaymentMethodSelected }: PaymentMethodBrandsProps) => {
-    if (isPaymentMethodSelected) {
+const PaymentMethodBrands = ({
+    brands,
+    excludedUIBrands = [],
+    isPaymentMethodSelected,
+    keepBrandsVisible = false,
+    showOtherInsteafOfNumber = false
+}: PaymentMethodBrandsProps) => {
+    const { i18n } = useCoreContext();
+
+    if (isPaymentMethodSelected && !keepBrandsVisible) {
         return null;
     }
 
@@ -30,7 +41,11 @@ const PaymentMethodBrands = ({ brands, excludedUIBrands = [], isPaymentMethodSel
             {visibleBrands.map(brand => (
                 <PaymentMethodIcon key={brand.name} altDescription={getFullBrandName(brand.name)} type={brand.name} src={brand.icon} />
             ))}
-            {leftBrandsAmount !== 0 && <span className="adyen-checkout__payment-method__brand-number">+{leftBrandsAmount}</span>}
+            {showOtherInsteafOfNumber ? (
+                <span className="adyen-checkout__payment-method__brand-number">+ {i18n.get('paymentMethodBrand.other')}</span>
+            ) : (
+                leftBrandsAmount !== 0 && <span className="adyen-checkout__payment-method__brand-number">+{leftBrandsAmount}</span>
+            )}
         </span>
     );
 };
