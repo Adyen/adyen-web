43ffef5920021eb3f74730129212383180bc632a
guilhermer <guilherme.ribeiro@adyen.com>
2024-10-07 14:00:18 +0200

    draft stepper

--- packages/lib/src/components/PayNow/components/PayNowIntroduction.tsx
+++ packages/lib/src/components/PayNow/components/PayNowIntroduction.tsx
@@ -12,11 +12,33 @@ const PayNowIntroduction = () => {
 
             <div className="adyen-checkout-paynow__introduction--mobile-only" data-testid="paynow-mobile-instructions">
                 <ContentSeparator />
-                <div>1. {i18n.get('paynow.mobileViewInstruction.step1')}</div>
-                <div>2. {i18n.get('paynow.mobileViewInstruction.step2')}</div>
-                <div>3. {i18n.get('paynow.mobileViewInstruction.step3')}</div>
-                <div>4. {i18n.get('paynow.mobileViewInstruction.step4')}</div>
-                <div>5. {i18n.get('paynow.mobileViewInstruction.step5')}</div>
+                <ol className="c-timeline">
+                    <li className="c-timeline__item">
+                        <div className="c-timeline__content">
+                            <p className="c-timeline__desc">Take a screenshot of the QR code.</p>
+                        </div>
+                    </li>
+                    <li className="c-timeline__item">
+                        <div className="c-timeline__content">
+                            <p className="c-timeline__desc">Open the PayNow bank or payment app.</p>
+                        </div>
+                    </li>
+                    <li className="c-timeline__item">
+                        <div className="c-timeline__content">
+                            <p className="c-timeline__desc">Select the option to scan a QR code.</p>
+                        </div>
+                    </li>
+                    <li className="c-timeline__item">
+                        <div className="c-timeline__content">
+                            <p className="c-timeline__desc">Choose the option to upload a QR and select the screenshot.</p>
+                        </div>
+                    </li>
+                    <li className="c-timeline__item">
+                        <div className="c-timeline__content">
+                            <p className="c-timeline__desc">Complete the transaction.</p>
+                        </div>
+                    </li>
+                </ol>
             </div>
         </div>
     );
