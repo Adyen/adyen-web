e720c4b3d0fa0b341e7aa05072a39b1399ba6993
nicholas <nicholas.spong@adyen.com>
2024-10-07 16:04:00 +0200

    Boleto & Doku vouchers use util fn to extract common props

--- packages/lib/src/components/Boleto/components/BoletoVoucherResult/BoletoVoucherResult.tsx
+++ packages/lib/src/components/Boleto/components/BoletoVoucherResult/BoletoVoucherResult.tsx
@@ -5,27 +5,29 @@ import { useCoreContext } from '../../../../core/Context/CoreProvider';
 import './BoletoVoucherResult.scss';
 import { VoucherDetail } from '../../../internal/Voucher/types';
 import useImage from '../../../../core/Context/useImage';
+import { extractCommonPropsForVoucher } from '../../../internal/Voucher/utils';
 
 const BoletoVoucherResult = props => {
     const { i18n, loadingContext } = useCoreContext();
     const getImage = useImage();
-    const { reference, expiresAt, totalAmount, paymentMethodType, downloadUrl, onActionHandled, originalAction } = props;
+    const { reference, expiresAt, downloadUrl } = props;
     const barcodeReference = reference.replace(/[^\d]/g, '').replace(/^(\d{4})(\d{5})\d{1}(\d{10})\d{1}(\d{10})\d{1}(\d{15})$/, '$1$5$2$3$4');
     const barcodeUrl = `${loadingContext}barcode.shtml?data=${barcodeReference}&barcodeType=BT_Int2of5A&fileType=png`;
 
+    const paymentMethodType = 'boletobancario'; // overwrite the bank specific type of boleto, e.g. 'boletobancario_santander', to a more generic form
+
     return (
         <Voucher
-            reference={reference}
-            paymentMethodType={'boletobancario'}
+            {...extractCommonPropsForVoucher({
+                props: { ...props, paymentMethodType },
+                i18n,
+                introKey: 'voucher.introduction',
+                getImage: getImage()
+            })}
             barcode={barcodeUrl}
-            introduction={i18n.get('voucher.introduction')}
-            imageUrl={getImage()(paymentMethodType)}
-            amount={totalAmount && i18n.amount(totalAmount.value, totalAmount.currency)}
             voucherDetails={[{ label: i18n.get('voucher.expirationDate'), value: i18n.date(expiresAt) }] as VoucherDetail[]}
             downloadUrl={downloadUrl}
             copyBtn
-            onActionHandled={onActionHandled}
-            originalAction={originalAction}
         />
     );
 };
