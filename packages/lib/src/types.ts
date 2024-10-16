60b39429ff50ea01654f5eee38d863fc497252d5
nicholas <nicholas.spong@adyen.com>
2024-10-16 11:16:16 +0200

    Export CustomTranslations type

--- packages/lib/src/types.ts
+++ packages/lib/src/types.ts
@@ -2,6 +2,7 @@ export * from './index';
 export * from './components/types';
 export * from './core/types';
 export * from './types/global-types';
+export { CustomTranslations } from './language/types';
 
 export { default as AdyenCheckoutError } from './core/Errors/AdyenCheckoutError';
 export { default as UIElement } from './components/internal/UIElement';
