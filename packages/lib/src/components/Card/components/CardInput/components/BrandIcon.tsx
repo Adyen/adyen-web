e6478e7041e010fcb268e3ff619ba2d5b6b61113
nicholas <nicholas.spong@adyen.com>
2024-11-14 11:48:53 +0100

    Add fix to reshow card icon if previously it failed to load

--- packages/lib/src/components/Card/components/CardInput/components/BrandIcon.tsx
+++ packages/lib/src/components/Card/components/CardInput/components/BrandIcon.tsx
@@ -7,13 +7,19 @@ export default function BrandIcon({ brand, brandsConfiguration = {} }: BrandIcon
     const getImage = useImage();
     const imageName = brand === 'card' ? 'nocard' : brand;
     const imageUrl = brandsConfiguration[brand]?.icon ?? getCardImageUrl(imageName, getImage);
+
     const handleError = e => {
         e.target.style.cssText = 'display: none';
+    };
+
+    const handleLoad = e => {
+        e.target.style.cssText = 'display: block';
     };
 
     return (
         <img
             className="adyen-checkout-card-input__icon adyen-checkout__card__cardNumber__brandIcon"
+            onLoad={handleLoad}
             onError={handleError}
             alt={getFullBrandName(brand)}
             src={imageUrl}
