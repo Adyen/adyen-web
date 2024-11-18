d0dbb20e3a1d306467e22880634df2fd53c7db0d
Yu Long <longyu901009@gmail.com>
2024-11-18 14:12:56 +0100

    Add playwright tests dualBranding and avs (#2952)
    * test(e2e): update models and add bcmc tests
    
    * test: added card avs tests
    
    * refactor: solve conflicts
    
    * test(avs.spec.ts): added address lookup
    
    * refactor(avs.spec.ts): move the urls

--- packages/e2e-playwright/tests/e2e/card/avs.spec.ts
+++ packages/e2e-playwright/tests/e2e/card/avs.spec.ts
@@ -1,83 +1,101 @@
-import { test as base, expect } from '@playwright/test';
+import { test, expect } from '../../../fixtures/card.fixture';
 import { PAYMENT_RESULT, REGULAR_TEST_CARD, TEST_CVC_VALUE, TEST_DATE_VALUE, TEST_POSTCODE } from '../../utils/constants';
-import { CardWithAvs } from '../../../models/card-avs';
 import { URL_MAP } from '../../../fixtures/URL_MAP';
 
-type Fixture = {
-    cardAvsPage: CardWithAvs;
-};
+test.describe('Card payments with address lookup', () => {
+    test('should make a successful card payment', async ({ cardWithAvs }) => {
+        await cardWithAvs.goto(URL_MAP.addressLookupUrl);
+        await cardWithAvs.fillCardNumber(REGULAR_TEST_CARD);
+        await cardWithAvs.fillExpiryDate(TEST_DATE_VALUE);
+        await cardWithAvs.fillCvc(TEST_CVC_VALUE);
 
-const test = base.extend<Fixture>({
-    cardAvsPage: async ({ page }, use) => {
-        const cardAvcPage = new CardWithAvs(page);
-        await cardAvcPage.goto(URL_MAP.cardWithPartialAvs);
-        await use(cardAvcPage);
-    }
+        await cardWithAvs.billingAddress.searchAddressAndChooseTheFirst('1');
+        await cardWithAvs.pay();
+        await cardWithAvs.paymentResult.waitFor({ state: 'visible' });
+        await expect(cardWithAvs.paymentResult).toContainText(PAYMENT_RESULT.authorised);
+    });
 });
-
-test.describe('Card payments with address lookup', () => {});
 
 test.describe('Card payments with partial avs', () => {
     test.describe('When fill in a valid the post code', () => {
-        test('should make a successful card payment', async ({ cardAvsPage }) => {
-            await cardAvsPage.fillCardNumber(REGULAR_TEST_CARD);
-            await cardAvsPage.fillExpiryDate(TEST_DATE_VALUE);
-            await cardAvsPage.fillCvc(TEST_CVC_VALUE);
-            await cardAvsPage.fillInPostCode(TEST_POSTCODE);
-            await cardAvsPage.pay();
-            await cardAvsPage.paymentResult.waitFor({ state: 'visible' });
-            await expect(cardAvsPage.paymentResult).toContainText(PAYMENT_RESULT.authorised);
+        test('should make a successful card payment', async ({ cardWithAvs }) => {
+            await cardWithAvs.goto(URL_MAP.cardWithPartialAvs);
+            await cardWithAvs.fillCardNumber(REGULAR_TEST_CARD);
+            await cardWithAvs.fillExpiryDate(TEST_DATE_VALUE);
+            await cardWithAvs.fillCvc(TEST_CVC_VALUE);
+            await cardWithAvs.billingAddress.fillInPostCode(TEST_POSTCODE);
+            await cardWithAvs.pay();
+            await cardWithAvs.paymentResult.waitFor({ state: 'visible' });
+            await expect(cardWithAvs.paymentResult).toContainText(PAYMENT_RESULT.authorised);
         });
     });
 
-    test.describe('When fill in an invalid post code ', () => {
-        // in the fixture, do not provide country code
-        test('should not submit the payment', async ({ page }) => {
-            // fill in card number
-            // fill in expiry date
-            // fill in cvc
-            // fill in post code
-            // click pay btn
-            // expect to see error message under the post code field
+    test.describe('When not fill in a post code ', () => {
+        test('should not submit the payment', async ({ cardWithAvs }) => {
+            await cardWithAvs.goto(URL_MAP.cardWithPartialAvs);
+            await cardWithAvs.fillCardNumber(REGULAR_TEST_CARD);
+            await cardWithAvs.fillExpiryDate(TEST_DATE_VALUE);
+            await cardWithAvs.fillCvc(TEST_CVC_VALUE);
+            await cardWithAvs.pay();
+            await expect(cardWithAvs.billingAddress.postalCodeError).toContainText('Enter the zip code');
         });
     });
 });
 
 test.describe('Card payments with full avs', () => {
     test.describe('When fill in the valid address data', () => {
-        test('should make a successful card payment', async ({ page }) => {
-            // fill in card number
-            // fill in expiry date
-            // fill in cvc
-            // fill in address ...
-            // click pay btn
-            // expect to see success result
+        test('should make a successful card payment', async ({ cardWithAvs }) => {
+            await cardWithAvs.goto(URL_MAP.fullAvsWithoutPrefilledDataUrl);
+            await cardWithAvs.fillCardNumber(REGULAR_TEST_CARD);
+            await cardWithAvs.fillExpiryDate(TEST_DATE_VALUE);
+            await cardWithAvs.fillCvc(TEST_CVC_VALUE);
+
+            await cardWithAvs.billingAddress.selectCountry({ name: 'United States' });
+            await cardWithAvs.billingAddress.fillInStreet('Test address');
+            await cardWithAvs.billingAddress.fillInCity('Test city');
+            await cardWithAvs.billingAddress.selectState({ name: 'Florida' });
+            await cardWithAvs.billingAddress.fillInPostCode('12345');
+            await cardWithAvs.pay();
+            await cardWithAvs.paymentResult.waitFor({ state: 'visible' });
+            await expect(cardWithAvs.paymentResult).toContainText(PAYMENT_RESULT.authorised);
         });
     });
 
     test.describe('When fill in the invalid address data', () => {
-        test('should not submit the payment', async ({ page }) => {
-            // fill in card number
-            // fill in expiry date
-            // fill in cvc
-            // skip required fields in address section
-            // click pay btn
-            // expect to see error message
+        test('should not submit the payment', async ({ cardWithAvs }) => {
+            await cardWithAvs.goto(URL_MAP.fullAvsWithoutPrefilledDataUrl);
+            await cardWithAvs.fillCardNumber(REGULAR_TEST_CARD);
+            await cardWithAvs.fillExpiryDate(TEST_DATE_VALUE);
+            await cardWithAvs.fillCvc(TEST_CVC_VALUE);
+
+            await cardWithAvs.billingAddress.selectCountry({ name: 'United States' });
+            await cardWithAvs.pay();
+            await expect(cardWithAvs.billingAddress.streetInputError).toContainText('Enter the address');
+            await expect(cardWithAvs.billingAddress.cityError).toContainText('Enter the city');
+            await expect(cardWithAvs.billingAddress.stateError).toContainText('Enter the state');
+            await expect(cardWithAvs.billingAddress.postalCodeError).toContainText('Enter the zip code');
         });
     });
 
     test.describe('When switching to a different delivery country', () => {
-        test('should make a successful card payment', async ({ page }) => {
-            // prefilled wrong address
-            // user sees error msg
-            // change the country code
-            // submit payment successfully
+        test('should make a successful card payment', async ({ cardWithAvs }) => {
+            const url =
+                '/iframe.html?globals=&args=componentConfiguration.billingAddressAllowedCountries:!undefined;componentConfiguration.data.billingAddress.postalCode:A9A9A9&id=cards-card--with-avs&viewMode=story';
+            await cardWithAvs.goto(url);
+            await expect(cardWithAvs.billingAddress.postalCodeError).toContainText('Invalid format. Expected format');
+            await cardWithAvs.billingAddress.selectCountry({ name: 'Japan' });
+            await cardWithAvs.fillCardNumber(REGULAR_TEST_CARD);
+            await cardWithAvs.fillExpiryDate(TEST_DATE_VALUE);
+            await cardWithAvs.fillCvc(TEST_CVC_VALUE);
+            await cardWithAvs.pay();
+            await cardWithAvs.paymentResult.waitFor({ state: 'visible' });
+            await expect(cardWithAvs.paymentResult).toContainText(PAYMENT_RESULT.authorised);
         });
-        test('should not submit the payment', async ({ page }) => {
-            // prefilled correct address
-            // change the country code
-            // user sees error msg
-            // user cannot submit payment
+
+        test('should not submit the payment', async ({ cardWithAvs }) => {
+            await cardWithAvs.goto(URL_MAP.fullAvsWithPrefilledDataUrl);
+            await cardWithAvs.billingAddress.selectCountry({ name: 'Canada' });
+            await expect(cardWithAvs.billingAddress.postalCodeError).toContainText('Invalid format. Expected format');
         });
     });
 });
