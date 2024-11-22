b824b29318863c085e92eaf8f3c63e3a33d28fdb
sponglord <nicholas.spong@adyen.com>
2024-11-22 17:23:07 +0100

    Add fix to reshow card icon if previously it failed to load (#2955)
    * Add fix to reshow card icon if previously it failed to load
    
    * Use useState and avoid setting inline styles
    
    * Added changeset file

---
+++ .changeset/strong-boxes-suffer.md
@@ -0,0 +1,5 @@
+---
+'@adyen/adyen-web': patch
+---
+
+Add fix to reshow card icon if previously it had failed to load
--- packages/lib/src/components/Card/components/CardInput/components/BrandIcon.tsx
+++ packages/lib/src/components/Card/components/CardInput/components/BrandIcon.tsx
@@ -2,21 +2,29 @@ import { h } from 'preact';
 import { getCardImageUrl, getFullBrandName } from '../utils';
 import { BrandIconProps } from './types';
 import useImage from '../../../../../core/Context/useImage';
+import { useState } from 'preact/hooks';
+import classNames from 'classnames';
 
 export default function BrandIcon({ brand, brandsConfiguration = {} }: BrandIconProps) {
     const getImage = useImage();
     const imageName = brand === 'card' ? 'nocard' : brand;
     const imageUrl = brandsConfiguration[brand]?.icon ?? getCardImageUrl(imageName, getImage);
-    const handleError = e => {
-        e.target.style.cssText = 'display: none';
+
+    const [hasLoaded, setHasLoaded] = useState(false);
+
+    const handleError = () => {
+        setHasLoaded(false);
     };
 
-    return (
-        <img
-            className="adyen-checkout-card-input__icon adyen-checkout__card__cardNumber__brandIcon"
-            onError={handleError}
-            alt={getFullBrandName(brand)}
-            src={imageUrl}
-        />
-    );
+    const handleLoad = () => {
+        setHasLoaded(true);
+    };
+
+    const fieldClassnames = classNames({
+        'adyen-checkout-card-input__icon': true,
+        'adyen-checkout__card__cardNumber__brandIcon': true,
+        'adyen-checkout-card-input__icon--hidden': !hasLoaded
+    });
+
+    return <img className={fieldClassnames} onLoad={handleLoad} onError={handleError} alt={getFullBrandName(brand)} src={imageUrl} />;
 }
