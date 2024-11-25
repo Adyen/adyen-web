8d4296b89179f476feb4836608214c358c3907ef
antoniof <m1aw@users.noreply.github.com>
2024-11-25 17:08:20 +0100

    payto segmented controller

---
+++ packages/lib/src/components/PayTo/components/PayIDInput.tsx
@@ -0,0 +1,17 @@
+import { h } from 'preact';
+
+export default function PayIDInput() {
+    // const { i18n } = useCoreContext();
+
+    // TODO type this
+    // const { handleChangeFor, triggerValidation, data, valid, errors } = useForm<any>({
+    //     schema: ['beneficiaryId']
+    // });
+    //
+    // const [status, setStatus] = useState<string>('ready');
+
+    // this.setStatus = setStatus;
+    // this.showValidation = triggerValidation;
+
+    return <p>PayIDInput.tsx</p>;
+}
--- packages/lib/src/components/PayTo/components/PayToInput.tsx
+++ packages/lib/src/components/PayTo/components/PayToInput.tsx
@@ -1,5 +1,31 @@
 import { h } from 'preact';
 import LoadingWrapper from '../../internal/LoadingWrapper';
+import SegmentedControl from '../../internal/SegmentedControl';
+import { useState } from 'preact/hooks';
+import { SegmentedControlOptions } from '../../internal/SegmentedControl/SegmentedControl';
+import PayIDInput from './PayIDInput';
+import BSBInput from './BSBInput';
+
+const inputOptions: SegmentedControlOptions<any> = [
+    {
+        value: 'payid-option',
+        label: 'PayID',
+        htmlProps: {
+            id: 'payid-option', // TODO move this to i18n
+            'aria-controls': 'payid-input',
+            'aria-expanded': true // TODO move this logic to segmented controller
+        }
+    },
+    {
+        value: 'bsb-option',
+        label: 'BSB and account number', // TODO move this to i18n
+        htmlProps: {
+            id: 'bsb-option',
+            'aria-controls': 'bsb-input',
+            'aria-expanded': false // TODO move this logic to segmented controller
+        }
+    }
+];
 
 export default function PayToInput() {
     // const { i18n } = useCoreContext();
@@ -14,9 +40,14 @@ export default function PayToInput() {
     // this.setStatus = setStatus;
     // this.showValidation = triggerValidation;
 
+    const defaultOption = inputOptions[0].value;
+    const [selectedInput, setSelectedInput] = useState<string>(defaultOption);
+
     return (
         <LoadingWrapper>
-            <div>Input goes here</div>
+            <SegmentedControl selectedValue={selectedInput} options={inputOptions} onChange={setSelectedInput} />
+            {selectedInput === 'payid-option' && <PayIDInput />}
+            {selectedInput === 'bsb-option' && <BSBInput />}
         </LoadingWrapper>
     );
 }
