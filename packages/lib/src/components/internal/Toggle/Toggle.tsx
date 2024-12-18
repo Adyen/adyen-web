3970ca560a86485df889b9375759db5aeedfaae2
guilhermer <guilherme.ribeiro@adyen.com>
2024-12-18 13:25:15 -0300

    using bento css. added different stories and mixins.

--- packages/lib/src/components/internal/Toggle/Toggle.tsx
+++ packages/lib/src/components/internal/Toggle/Toggle.tsx
@@ -1,30 +1,46 @@
 import { h } from 'preact';
+import { useCallback, useMemo } from 'preact/hooks';
 import cx from 'classnames';
+import uuid from '../../../utils/uuid';
 import './Toggle.scss';
 
 interface ToggleProps {
     label?: string;
     labelPosition?: 'before' | 'after';
+    ariaLabel?: string;
+    description?: string;
     checked: boolean;
-    onChange?: () => void;
     disabled?: boolean;
     readonly?: boolean;
+    onChange?(checked: boolean): void;
 }
 
-const Toggle = ({ label, labelPosition, checked, disabled, readonly, onChange }: ToggleProps) => {
+const Toggle = ({ label, labelPosition = 'after', ariaLabel, description, checked, disabled = false, readonly = false, onChange }: ToggleProps) => {
+    const descriptionId = useMemo(() => (description ? `toggle-description-${uuid()}` : null), [description]);
+    const computedAriaLabel = useMemo(() => ariaLabel ?? label, [ariaLabel, label]);
+
     const conditionalClasses = cx({
         'adyen-checkout-toggle--label-first': labelPosition === 'before',
         'adyen-checkout-toggle--disabled': disabled,
         'adyen-checkout-toggle--readonly': readonly
     });
 
+    const onInputChange = useCallback(
+        (event: Event) => {
+            onChange((event.target as HTMLInputElement).checked);
+        },
+        [onChange]
+    );
+
     return (
         <label className={`adyen-checkout-toggle ${conditionalClasses}`}>
             <input
+                disabled={disabled}
                 checked={checked}
-                aria-label={label}
-                onChange={onChange}
-                aria-describedby={''}
+                onChange={onInputChange}
+                aria-label={computedAriaLabel}
+                aria-readonly={readonly}
+                aria-describedby={descriptionId}
                 role="switch"
                 type="checkbox"
                 className="adyen-checkout-toggle__input"
@@ -33,15 +49,7 @@ const Toggle = ({ label, labelPosition, checked, disabled, readonly, onChange }:
             <span aria-hidden={true} className="adyen-checkout-toggle__track">
                 <span className="adyen-checkout-toggle__handle">
                     {checked && (
-                        <svg
-                            data-v-dd0561f5=""
-                            role="img"
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="16"
-                            height="16"
-                            fill="none"
-                            className="ui-assets-icons-16"
-                        >
+                        <svg role="img" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none">
                             <path
                                 fill="#00112C"
                                 d="M12.0608 6.00011L11.0001 4.93945L7.00011 8.93945L5.00011 6.93945L3.93945 8.00011L7.00011 11.0608L12.0608 6.00011Z"
@@ -53,8 +61,14 @@ const Toggle = ({ label, labelPosition, checked, disabled, readonly, onChange }:
 
             {label && (
                 <span className="adyen-checkout-toggle__label-container">
-                    <span className="adyen-checkout-toggle__label-text">{label}</span>
-                    <span className="adyen-checkout-toggle__description"></span>
+                    <span className="adyen-checkout-toggle__label-text" data-testid="inner-label">
+                        {label}
+                    </span>
+                    {description && (
+                        <span data-testid="description" className="adyen-checkout-toggle__description" id={descriptionId}>
+                            {description}
+                        </span>
+                    )}
                 </span>
             )}
         </label>
--- packages/lib/storybook/stories/internals/Toggle.stories.tsx
+++ packages/lib/storybook/stories/internals/Toggle.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from '@storybook/preact';
 import Toggle from '../../../src/components/internal/Toggle';
+import { useState } from 'preact/hooks';
 
 const meta: Meta = {
     title: 'Internals/Toggle',
@@ -8,7 +9,53 @@ const meta: Meta = {
 
 export const Default: StoryObj = {
     render: () => {
-        return <Toggle checked={true} onChange={() => console.log('change')} label={'Save your info with Fastlane for faster checkouts'} />;
+        const [checked, setChecked] = useState<boolean>(false);
+        return <Toggle checked={checked} onChange={setChecked} label={'Save your profile'} />;
+    },
+    parameters: {
+        controls: { exclude: ['useSessions', 'countryCode', 'shopperLocale', 'amount', 'showPayButton'] }
+    }
+};
+
+export const Disabled: StoryObj = {
+    render: () => {
+        return <Toggle checked={false} disabled label={'Save your profile'} />;
+    },
+    parameters: {
+        controls: { exclude: ['useSessions', 'countryCode', 'shopperLocale', 'amount', 'showPayButton'] }
+    }
+};
+
+export const Readonly: StoryObj = {
+    render: () => {
+        return <Toggle checked={false} readonly label={'Save your profile'} />;
+    },
+    parameters: {
+        controls: { exclude: ['useSessions', 'countryCode', 'shopperLocale', 'amount', 'showPayButton'] }
+    }
+};
+
+export const ToggleOnly: StoryObj = {
+    render: () => {
+        const [checked, setChecked] = useState<boolean>(true);
+        return <Toggle checked={checked} onChange={setChecked} />;
+    },
+    parameters: {
+        controls: { exclude: ['useSessions', 'countryCode', 'shopperLocale', 'amount', 'showPayButton'] }
+    }
+};
+
+export const WithDescription: StoryObj = {
+    render: () => {
+        const [checked, setChecked] = useState<boolean>(true);
+        return (
+            <Toggle
+                checked={checked}
+                onChange={setChecked}
+                label={'Save your profile'}
+                description={'Save your profile for faster checkouts next time you buy our products'}
+            />
+        );
     },
     parameters: {
         controls: { exclude: ['useSessions', 'countryCode', 'shopperLocale', 'amount', 'showPayButton'] }
