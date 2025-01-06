3d1f4190c7ea15f306edb825776b6a9aea73b065
guilhermer <guilherme.ribeiro@adyen.com>
2025-01-06 12:13:56 -0300

    testing that updateWithAction is called

--- packages/lib/src/components/Klarna/KlarnaPayments.test.tsx
+++ packages/lib/src/components/Klarna/KlarnaPayments.test.tsx
@@ -1,9 +1,15 @@
 import { render, screen, waitFor } from '@testing-library/preact';
 import KlarnaPayments from './KlarnaPayments';
 import Dropin from '../Dropin';
+import { PaymentAction } from '../../types/global-types';
 
 describe('KlarnaPayments', () => {
-    const coreProps = { name: 'Klarna', i18n: global.i18n, loadingContext: 'test', modules: { resources: global.resources } };
+    const coreProps = {
+        name: 'Klarna',
+        i18n: global.i18n,
+        loadingContext: 'test',
+        modules: { resources: global.resources, analytics: global.analytics }
+    };
     const renderKlarna = props => {
         const KlarnaPaymentsEle = new KlarnaPayments(global.core, {
             ...coreProps,
@@ -52,5 +58,62 @@ describe('KlarnaPayments', () => {
         klarna.onComplete();
 
         expect(onAdditionalDetailsMock).toHaveBeenCalled();
+    });
+
+    describe('when handling action', () => {
+        test('should use updateWithAction when action type is "sdk"', async () => {
+            const widgetAction: PaymentAction = {
+                paymentData: 'Ab02b4c0!...',
+                paymentMethodType: 'klarna_paynow',
+                type: 'sdk',
+                sdkData: {
+                    client_token: 'eyJhbGciOiJ...',
+                    payment_method_category: 'pay_over_time'
+                }
+            };
+
+            const klarna = new KlarnaPayments(global.core, {
+                ...coreProps,
+                type: 'klarna_paynow',
+                onSubmit(state, component, actions) {
+                    actions.resolve({
+                        resultCode: 'Pending',
+                        action: widgetAction
+                    });
+                }
+            });
+            const spy = jest.spyOn(klarna, 'handleAction');
+
+            render(klarna.render());
+            klarna.submit();
+
+            await waitFor(() => expect(spy).toHaveBeenCalledWith(widgetAction), { interval: 100 });
+        });
+
+        test('should NOT use updateWithAction when action type is NOT "sdk"', async () => {
+            const action: PaymentAction = {
+                paymentMethodType: 'klarna_paynow',
+                type: 'redirect',
+                url: 'https://checkoutshopper-test.adyen.com/checkoutshopper/checkoutPaymentRedirect?redirectData=X3XtfGC9%21H4s...',
+                method: 'GET'
+            };
+
+            const klarna = new KlarnaPayments(global.core, {
+                ...coreProps,
+                type: 'klarna_paynow',
+                onSubmit(state, component, actions) {
+                    actions.resolve({
+                        resultCode: 'Pending',
+                        action
+                    });
+                }
+            });
+            const spy = jest.spyOn(klarna, 'handleAction');
+
+            render(klarna.render());
+            klarna.submit();
+
+            await waitFor(() => expect(spy).not.toHaveBeenCalled(), { interval: 100 });
+        });
     });
 });
