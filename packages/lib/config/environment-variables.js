13d273747b2e57c53af861980f42baa590102d4b
guilhermer <guilherme.ribeiro@adyen.com>
2025-02-07 11:59:33 +0100

    fix for node 22

--- packages/lib/config/environment-variables.js
+++ packages/lib/config/environment-variables.js
@@ -1,4 +1,4 @@
-import packageJson from '../package.json' assert { type: 'json' };
+import packageJson from '../package.json' with { type: 'json' };
 import { loadEnv } from 'vite';
 import * as path from 'path';
 
