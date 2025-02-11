e87dac20f716749a517720de2e3dd0a3bd2270b4
guilhermer <guilherme.ribeiro@adyen.com>
2025-02-11 13:56:45 +0100

    added challengewindowsize type to googlepay

--- packages/lib/src/components/GooglePay/types.ts
+++ packages/lib/src/components/GooglePay/types.ts
@@ -16,6 +16,19 @@ export interface GooglePayConfiguration extends UIElementProps {
     isExpress?: boolean;
 
     /**
+     * Defines the size of the challenge Component
+     *
+     * 01: [250px, 400px]
+     * 02: [390px, 400px]
+     * 03: [500px, 600px]
+     * 04: [600px, 400px]
+     * 05: [100%, 100%]
+     *
+     * @defaultValue '02'
+     */
+    challengeWindowSize?: '01' | '02' | '03' | '04' | '05';
+
+    /**
      * @see https://developers.google.com/pay/api/web/reference/request-objects#IsReadyToPayRequest
      * @defaultValue false
      */
