edf27bf60b053f31c4d5213adbe09bcf314d7db2
nicholas <nicholas.spong@adyen.com>
2025-02-21 12:54:35 +0100

    Deleting unneeded placeholder files

--- packages/e2e-playwright/tests/ui/customCard/branding/customcard.dualBranding.clientScripts.js
+++
@@ -1,7 +0,0 @@
-/**
- * Set cartebancaire as a brand since the test dual brand card is visa/cb
- */
-window.cardConfig = {
-    type: 'scheme',
-    brands: ['mc', 'visa', 'amex', 'cartebancaire', 'bcmc', 'maestro']
-};
