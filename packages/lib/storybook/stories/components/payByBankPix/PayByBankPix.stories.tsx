ce13219e4c8c40a4ff977b33acca034d81c4331c
Yu Long <longyu901009@gmail.com>
2025-03-03 16:35:32 +0100

    [phase 1] Pix biometrics enrollment (#3157)
    * refactor: await add pollStatus callback. Added enrollment poll status function. Add onAction callback in the core because the action returned by makeDetailsCall needs to be handled
    
    * refactor: type definition
    
    * feat(core): added afterAdditionalDetails callback to expose the action UIElement
    
    * refactor: added IssuerListIntroduction and AwaitLogoContainer
    
    * refactor: use useImage
    
    * refactor: added test and update endpoint

---
+++ packages/lib/storybook/stories/components/payByBankPix/PayByBankPix.stories.tsx
@@ -0,0 +1,103 @@
+import { MetaConfiguration, PaymentMethodStoryProps, StoryConfiguration } from '../../types';
+import { ComponentContainer } from '../../ComponentContainer';
+import { Checkout } from '../../Checkout';
+import PayByBankPix from '../../../../src/components/PayByBankPix';
+import { PayByBankPixConfiguration } from '../../../../src/components/PayByBankPix/types';
+import { http, HttpResponse } from 'msw';
+import SimulatedIssuer from './SimulatedIssuer';
+import {
+    mockPaymentsResponseMerchantPage,
+    mockPaymentsResponseSimulateHostedPage,
+    mockPendingStatusSimulateHostedPage,
+    mockReceivedStatusSimulateHostedPage,
+    mockSubmitDetailsResponseSimulateHostedPage
+} from './mocks';
+import { SimulatedHostedPage } from './SimulatedHostedPage';
+import { getSearchParameter } from '../../../utils/get-query-parameters';
+
+type PixBiometricStory = StoryConfiguration<PayByBankPixConfiguration>;
+
+const meta: MetaConfiguration<PayByBankPixConfiguration> = {
+    title: 'Components/PayByBankPix'
+};
+
+const render = ({ componentConfiguration, ...checkoutConfig }: PaymentMethodStoryProps<PayByBankPixConfiguration>) => (
+    <>
+        <h1>Merchant page</h1>
+        <Checkout checkoutConfig={checkoutConfig}>
+            {checkout => <ComponentContainer element={new PayByBankPix(checkout, componentConfiguration)} />}
+        </Checkout>
+    </>
+);
+
+export const MerchantPage: PixBiometricStory = {
+    render,
+    args: {
+        useSessions: false,
+        countryCode: 'BR',
+        amount: 0,
+        componentConfiguration: { _isAdyenHosted: false }
+    },
+    parameters: {
+        msw: {
+            handlers: [
+                http.post('https://localhost:3020/payments', () => {
+                    return HttpResponse.json(mockPaymentsResponseMerchantPage);
+                }),
+                http.post('/details', () => {
+                    return HttpResponse.json();
+                })
+            ]
+        }
+    }
+};
+
+export const SimulateHostedPage: PixBiometricStory = {
+    render: props => <SimulatedHostedPage {...props} />,
+    args: {
+        useSessions: false,
+        countryCode: 'BR',
+        amount: 0,
+        redirectResult: getSearchParameter('redirectResult'),
+        componentConfiguration: {
+            _isAdyenHosted: true,
+            issuers: [
+                {
+                    id: '0b919e9b-bee0-4549-baa3-bb6d003575ce',
+                    name: 'Iniciador Mock Bank'
+                }
+            ],
+            onChange: data => {
+                console.log({ data });
+            }
+        }
+    },
+    parameters: {
+        msw: {
+            handlers: [
+                http.post('https://localhost:3020/payments', () => {
+                    return HttpResponse.json(mockPaymentsResponseSimulateHostedPage);
+                }),
+                http.get(
+                    'https://checkoutshopper-test.adyen.com/checkoutshopper/utility/v1/pixpaybybank/registration-options/enrollment123?clientKey=test_L6HTEOAXQBCZJHKNU4NLN6EI7IE6VRRW',
+                    () => {
+                        return HttpResponse.json(
+                            getSearchParameter('pollStatus') === 'pending'
+                                ? mockPendingStatusSimulateHostedPage
+                                : mockReceivedStatusSimulateHostedPage
+                        );
+                    }
+                ),
+                http.post('/details', () => {
+                    return HttpResponse.json(mockSubmitDetailsResponseSimulateHostedPage);
+                })
+            ]
+        }
+    }
+};
+
+export const SimulateIssuerPage: PixBiometricStory = {
+    render: () => <SimulatedIssuer />
+};
+
+export default meta;
