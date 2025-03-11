8c9fc83bad9d34275cdaf66cc0145271ddceab54
Guilherme Ribeiro <guilhermemrr@gmail.com>
2025-03-11 15:54:38 +0100

    Apple Pay - Third-party browser support (#3130)
    * loading applepay sdk and using its element to render
    
    * parsing version properly
    
    * adding comment to new prop
    
    * added applepaycapabilities. adjusted isavailable
    
    * added test to utils
    
    * added tests for applepayservice
    
    * added tests for applepaysdkloader
    
    * tests for isAvailable
    
    * added more tests
    
    * changed import to type
    
    * split applepay utils functions in different files
    
    * changeset
    
    * cleanup

--- packages/lib/src/types/custom.d.ts
+++ packages/lib/src/types/custom.d.ts
@@ -1,12 +1,40 @@
+import { ApplePayButtonStyle, ApplePayButtonType, ApplePayWebConfiguration } from '../components/ApplePay/types';
+
+declare module 'preact' {
+    namespace JSX {
+        interface IntrinsicElements {
+            'apple-pay-button': {
+                buttonstyle: ApplePayButtonStyle;
+                type: ApplePayButtonType;
+                locale: string;
+                onclick(): void;
+            };
+        }
+    }
+}
+
 declare module '*.scss' {
     const content: { [className: string]: string };
     export default content;
 }
 
-interface Window {
-    AdyenWeb: any;
-    VISA_SDK?: {
-        buildClientProfile?(srciDpaId?: string): any;
-        correlationId?: string;
-    };
+declare global {
+    interface Window {
+        /**
+         * ApplePaySession added by ApplePaySDK
+         */
+        ApplePaySession?: ApplePaySession;
+
+        ApplePayWebOptions?: {
+            set(config: ApplePayWebConfiguration): void;
+            focusApplePayCodeWindow(): void;
+            closeApplePayCodeWindow(): void;
+        };
+
+        AdyenWeb: any;
+        VISA_SDK?: {
+            buildClientProfile?(srciDpaId?: string): any;
+            correlationId?: string;
+        };
+    }
 }
