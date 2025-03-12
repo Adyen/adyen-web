6cc7078d99e68f33197db03abd1884653d65ec0b
Guilherme Ribeiro <guilhermemrr@gmail.com>
2025-03-12 15:48:37 +0100

    Typescript - Fixing triple slash directives on build file (#3178)
    * adding triple slash directives
    
    * changeset

---
+++ packages/lib/scripts/add-type-references.cjs
@@ -0,0 +1,26 @@
+/**
+ * Typescript for some reason is not adding the triple-slash directives at the generated type definition file.
+ * This scripts adds them manually after the types are generated
+ */
+
+const fs = require('fs');
+const path = './dist/temp-types/lib/src/types.d.ts';
+
+const references = ['/// <reference types="googlepay" />', '/// <reference types="applepayjs" />'];
+
+if (fs.existsSync(path)) {
+    let content = fs.readFileSync(path, 'utf8');
+
+    // Ensure references are added at the top
+    references.forEach(ref => {
+        if (!content.includes(ref)) {
+            content = ref + '\n' + content;
+        }
+    });
+
+    fs.writeFileSync(path, content, 'utf8');
+    console.log('Added references to googlepay and applepayjs types.');
+} else {
+    console.error(`Error: ${path} not found. Ensure TypeScript declaration files are generated.`);
+    process.exit(1);
+}
