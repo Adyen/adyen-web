20179f8d59a689fec86084f5b9991436e4f70b09
nicholas <nicholas.spong@adyen.com>
2025-03-18 16:29:23 +0100

    Card icons stop restricting focus area of inputs

--- packages/lib/src/components/Card/components/CardInput/components/CVC.tsx
+++ packages/lib/src/components/Card/components/CardInput/components/CVC.tsx
@@ -47,6 +47,10 @@ export default function CVC(props: CVCProps) {
 
     const fieldLabel = cvcPolicy !== CVC_POLICY_OPTIONAL ? label : i18n.get('creditCard.securityCode.label.optional');
 
+    const handleIconClick = () => {
+        onFocusField(ENCRYPTED_SECURITY_CODE);
+    };
+
     return (
         <Field
             label={fieldLabel}
@@ -68,7 +72,7 @@ export default function CVC(props: CVCProps) {
         >
             <DataSfSpan encryptedFieldType={ENCRYPTED_SECURITY_CODE} className={cvcClassnames} />
 
-            <CVCHint frontCVC={frontCVC} fieldLabel={fieldLabel} />
+            <CVCHint frontCVC={frontCVC} fieldLabel={fieldLabel} onClick={handleIconClick} />
         </Field>
     );
 }
