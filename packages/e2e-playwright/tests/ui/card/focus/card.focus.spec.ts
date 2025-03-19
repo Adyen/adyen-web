1561701d310a4effaba099392ae4b8e00d2b6280
nicholas <nicholas.spong@adyen.com>
2025-03-19 16:21:02 +0100

    Extended test to cover clicking on labels

--- packages/e2e-playwright/tests/ui/card/focus/card.focus.spec.ts
+++ packages/e2e-playwright/tests/ui/card/focus/card.focus.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '../../../../fixtures/card.fixture';
 import { URL_MAP } from '../../../../fixtures/URL_MAP';
 
-test('#1 Clicking on card icons should focus the respective inputs', async ({ card, page }) => {
+test('#1 Clicking on card icons should focus the respective inputs', async ({ card }) => {
     await card.goto(URL_MAP.card);
 
     await card.selectBrand('card'); // select the generic "no card" icon
@@ -14,5 +14,18 @@ test('#1 Clicking on card icons should focus the respective inputs', async ({ ca
 
     await card.selectCVCIcon();
 
+    await expect(card.cvcInput).toBeFocused();
+});
+
+test('#2 Clicking on card labels should focus the respective inputs', async ({ card }) => {
+    await card.goto(URL_MAP.card);
+
+    await card.cardNumberLabelElement.click();
+    await expect(card.cardNumberInput).toBeFocused();
+
+    await card.expiryDateLabelElement.click();
+    await expect(card.expiryDateInput).toBeFocused();
+
+    await card.cvcLabelElement.click();
     await expect(card.cvcInput).toBeFocused();
 });
