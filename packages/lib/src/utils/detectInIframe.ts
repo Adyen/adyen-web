d6e5df5b05119eeddc5c2c8233967c236c28d29f
guilhermer <guilherme.ribeiro@adyen.com>
2025-03-24 11:19:07 +0100

    implemented review comment

--- packages/lib/src/utils/detectInIframe.ts
+++ packages/lib/src/utils/detectInIframe.ts
@@ -2,9 +2,16 @@
  * Returns true if the Checkout is hosted within an iframe, false otherwise.
  */
 const detectInIframe = () => {
-    try {
-        return window.self !== window.top;
-    } catch (e) {
+    if (typeof window !== 'undefined') {
+        try {
+            return window.self !== window.top;
+        } catch (e) {
+            // In some cross-origin scenarios, accessing window.top might throw an error.
+            // In such cases, it is likely in an iframe.
+            return true;
+        }
+    } else {
+        // SSR
         return false;
     }
 };
