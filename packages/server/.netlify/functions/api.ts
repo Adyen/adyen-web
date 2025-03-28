c6a503a034af60711181d360a97196ab3591172b
antoniof <m1aw@users.noreply.github.com>
2025-03-28 18:08:35 +0100

    test netlify deployment

---
+++ packages/server/.netlify/functions/api.ts
@@ -0,0 +1,13 @@
+// YOUR_BASE_DIRECTORY/netlify/functions/api.ts
+
+import express, { Router } from 'express';
+import serverless from 'serverless-http';
+
+const api = express();
+
+const router = Router();
+router.get('/hello', (req, res) => res.send('Hello World!'));
+
+api.use('/api/', router);
+
+export const handler = serverless(api);
