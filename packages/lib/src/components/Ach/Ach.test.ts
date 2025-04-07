c27821c8b572835261b2cd4d6f1bf41f9b74d090
guilhermer <guilherme.ribeiro@adyen.com>
2025-04-07 14:53:42 +0200

    test file

---
+++ packages/lib/src/components/Ach/Ach.test.ts
@@ -0,0 +1,19 @@
+describe('ACH', () => {
+    describe('Default component', () => {
+        test('should submit the payment');
+
+        test('should submit the payment with the store consent given');
+
+        test('should not submit the payment if the account number does not match');
+
+        test('should not submit the payment if the account number is invalid');
+
+        test('should not submit the payment if the routing number is invalid');
+
+        test('should hide the holder name if configuration is set to hide it');
+    });
+
+    describe('Stored component', () => {
+        test('should submit the payment');
+    });
+});
