43676ccb17f9e50328de49ca4c1286cc8dbde978
Yu Long <longyu901009@gmail.com>
2025-04-09 16:35:18 +0200

    [phase 2]: refactor the code & add payment draft flow (#3174)
    * refactor: initial version pix stored pm flow
    
    * refactor: improvements on enrollment flow
    
    * refactor: move business logic to ui element
    
    * refactor: type and share component
    
    * refactor: extract `deviceId` from `riskSignals` object before sending to backend
    
    * refactor: some improvements and added unit test
    
    * refactor: added to dropin supportedStoredPaymentMethods
    
    * refactor: fix sonarqube issues
    
    * refactor: send `riskSignals` in the payments call for the payment flow
    
    * refactor: improve the enrollment screen
    
    * refactor: improve the code and better typing

--- packages/lib/src/components/PayByBankPix/components/Payment/Payment.tsx
+++ packages/lib/src/components/PayByBankPix/components/Payment/Payment.tsx
@@ -1,22 +1,90 @@
-import { h } from 'preact';
-import { useState } from 'preact/hooks';
+import { h, Fragment } from 'preact';
+import { useState, useEffect, useRef } from 'preact/hooks';
+import { useCoreContext } from '../../../../core/Context/CoreProvider';
+import { PaymentProps } from './types';
+import PaymentDetails from '../../../internal/Voucher';
+import useImage from '../../../../core/Context/useImage';
+import PayButton from '../../../internal/PayButton';
+import './Payment.scss';
+import getAuthorizationStatus from './getAuthorizationStatus';
+import PayByBankPixAwait from '../Enrollment/components/PayByBankPixAwait';
 
-function Payment({ payButton }) {
+function Payment({
+    onPay,
+    type,
+    countdownTime,
+    receiver,
+    amount,
+    txVariant,
+    issuer,
+    setComponentRef,
+    enrollmentId,
+    initiationId,
+    clientKey,
+    onAuthorize,
+    onError
+}: PaymentProps) {
+    const { i18n, loadingContext } = useCoreContext();
+    const getImage = useImage();
     const [status, setStatus] = useState('ready');
-    this.setStatus = setStatus;
+    const [authorizationOptions, setAuthorizationOptions] = useState<string>(null);
     const buttonModifiers = ['standalone'];
+    const logos = [{ name: 'open-finance', alt: i18n.get('paybybankpix.await.logoAlt.openFinance'), src: `${getImage()('open-finance')}` }];
+    const details = [
+        { label: i18n.get('paybybankpix.payment.receiver.label'), value: receiver },
+        { label: i18n.get('paybybankpix.payment.paymentDate.label'), value: i18n.date(new Date().toString()) },
+        { label: i18n.get('paybybankpix.payment.paymentMethod.label'), value: 'Pix Open Finance' }
+    ];
+    const self = useRef({
+        setStatus
+    });
 
-    return (
-        <div>
-            {
-                // todo: add screen
-            }
-            {payButton({
-                status,
-                label: 'Dummy',
-                classNameModifiers: buttonModifiers
-            })}
-        </div>
+    const pollStatus = async () => {
+        if (authorizationOptions) return;
+
+        const response = await getAuthorizationStatus({ enrollmentId, initiationId, clientKey, loadingContext });
+        if (response.authorizationOptions) {
+            setAuthorizationOptions(response.authorizationOptions);
+        }
+        return response;
+    };
+
+    useEffect(() => {
+        setComponentRef(self.current);
+    }, [setComponentRef]);
+
+    useEffect(() => {
+        if (authorizationOptions) {
+            onAuthorize(authorizationOptions);
+        }
+    }, [authorizationOptions]);
+
+    return type === 'await' ? (
+        <PayByBankPixAwait
+            logos={logos}
+            type={txVariant}
+            countdownTime={countdownTime}
+            clientKey={clientKey}
+            onError={onError}
+            awaitText={i18n.get('paybybankpix.await.fetchDetails')}
+            pollStatus={pollStatus}
+        ></PayByBankPixAwait>
+    ) : (
+        <Fragment>
+            <PaymentDetails
+                issuerImageUrl={getImage()(issuer)}
+                paymentMethodType={txVariant}
+                amount={i18n.amount(amount.value, amount.currency)}
+                voucherDetails={details}
+            ></PaymentDetails>
+            <PayButton
+                classNameModifiers={buttonModifiers}
+                label={i18n.get('paybybankpix.storedPayment.payButton.label')}
+                status={status}
+                amount={amount}
+                onClick={onPay}
+            />
+        </Fragment>
     );
 }
 
--- packages/lib/src/components/internal/IssuerList/types.ts
+++ packages/lib/src/components/internal/IssuerList/types.ts
@@ -2,11 +2,6 @@ import { PayButtonProps } from '../PayButton/PayButton';
 import { ComponentChildren } from 'preact';
 import { AnalyticsEvent } from '../../../core/Analytics/AnalyticsEvent';
 
-// public "functions"
-export interface IIssuerList {
-    showValidation: () => {};
-}
-
 export interface IssuerListProps {
     items: IssuerItem[];
     showPayButton: boolean;
