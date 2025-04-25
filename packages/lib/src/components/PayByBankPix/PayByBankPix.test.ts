f3abb83aceb1128f0e5b376a407ddb04790d07d5
Yu Long <longyu901009@gmail.com>
2025-04-25 11:06:21 +0200

    Revert "refactor: use `pixChallenge` for action type"
    This reverts commit 7cf8d2f50f9ac9d46e08314b6b974cbc9c33ac00.

--- packages/lib/src/components/PayByBankPix/PayByBankPix.test.ts
+++ packages/lib/src/components/PayByBankPix/PayByBankPix.test.ts
@@ -166,7 +166,7 @@ describe('PayByBankPix', () => {
                 ...coreProps,
                 modules: { ...coreProps.modules, srPanel: new SRPanel(global.core) },
                 _isAdyenHosted: true,
-                type: 'pixChallenge'
+                type: 'await'
             });
             render(payByBankPixElement.render());
             expect(await screen.findByText('Waiting for your confirmation...')).toBeInTheDocument();
@@ -177,7 +177,7 @@ describe('PayByBankPix', () => {
                 ...coreProps,
                 modules: { ...coreProps.modules, srPanel: new SRPanel(global.core) },
                 _isAdyenHosted: true,
-                type: 'pixChallenge',
+                type: 'await',
                 enrollmentId: 'mock-enrollment-id',
                 clientKey: 'mock-client-key'
             });
@@ -205,7 +205,7 @@ describe('PayByBankPix', () => {
                 ...coreProps,
                 modules: { ...coreProps.modules, srPanel: new SRPanel(global.core) },
                 _isAdyenHosted: true,
-                type: 'pixChallenge',
+                type: 'await',
                 enrollmentId: 'mock-enrollment-id',
                 clientKey: 'mock-client-key'
             });
@@ -283,7 +283,7 @@ describe('PayByBankPix', () => {
                 storedPaymentMethodId: 'mock-stored-payment-method-id',
                 receiver: 'mock-receiver',
                 amount: { value: 100, currency: 'BRL' },
-                type: 'pixChallenge'
+                type: 'await'
             });
             render(payByBankPixElement.render());
             expect(await screen.findByText('Fetching details...')).toBeInTheDocument();
@@ -300,7 +300,7 @@ describe('PayByBankPix', () => {
                 enrollmentId: 'mock-enrollment-id',
                 initiationId: 'mock-initiationId-id',
                 clientKey: 'mock-client-key',
-                type: 'pixChallenge'
+                type: 'await'
             });
             render(payByBankPixElement.render());
             await waitFor(() =>
@@ -332,7 +332,7 @@ describe('PayByBankPix', () => {
                 enrollmentId: 'mock-enrollment-id',
                 initiationId: 'mock-initiationId-id',
                 clientKey: 'mock-client-key',
-                type: 'pixChallenge'
+                type: 'await'
             });
             render(payByBankPixElement.render());
             await waitFor(() => expect(mockCreateCredential).toHaveBeenCalledWith('mock-authorization-options'));
