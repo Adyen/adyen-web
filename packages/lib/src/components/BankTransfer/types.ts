95e623e78e0134013bc162e1168cc516bfedc1a6
Yu Long <yu.long@adyen.com>
2025-06-06 17:42:13 +0200

    feat(bank-transfer): UI improvements

--- packages/lib/src/components/BankTransfer/types.ts
+++ packages/lib/src/components/BankTransfer/types.ts
@@ -1,14 +1,16 @@
 import { UIElementProps } from '../internal/UIElement/types';
+import { BankTransferInputProps } from './components/BankTransferInput/types';
 
-export interface BankTransferConfiguration extends UIElementProps {
-    reference?: string;
+export type BankTransferConfiguration = UIElementProps &
+    Partial<Omit<BankTransferInputProps, 'onChange' | 'ref'>> & {
+        reference?: string;
 
-    /**
-     * Show/hide email address field
-     * @default true
-     */
-    showEmailAddress?: boolean;
-}
+        /**
+         * Show/hide email address field
+         * @default true
+         */
+        showEmailAddress?: boolean;
+    };
 
 export interface BankTransferState extends UIElementProps {
     data: {
