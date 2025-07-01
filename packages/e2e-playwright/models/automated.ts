67628d1590a1daf4c42f3713e3b6f0c94d49fc5e
antoniof <m1aw@users.noreply.github.com>
2025-07-01 14:59:35 +0200

    a11y improvements

--- packages/e2e-playwright/models/automated.ts
+++ packages/e2e-playwright/models/automated.ts
@@ -7,7 +7,7 @@ export class Automated extends Base {
     constructor(page: Page) {
         super(page);
 
-        this.payButton = this.page.locator('.adyen-checkout__button--pay');
+        this.payButton = this.page.locator('#component-root');
     }
 
     async goto(url: string) {
@@ -15,7 +15,25 @@ export class Automated extends Base {
         await this.isComponentVisible();
     }
 
+    private isInternalStory() { 
+        const storyUrl = this.page.url();
+        // Check if is 'internal' component story
+        return storyUrl && storyUrl.includes('internals-');
+    }
+
+    protected a11yComponentSelector() {
+        if(this.isInternalStory()){
+            return ('#storybook-root');
+        }
+        return '#component-root';
+    }
+
     async isComponentVisible() {
+        if(this.isInternalStory()){
+            // Becasue these don't have paybutton wait 3s - it was trial and error
+            // another way of doing this is welcome
+            return this.page.waitForTimeout(3000);
+        }
         await this.payButton.waitFor({ state: 'visible' });
     }
 }
