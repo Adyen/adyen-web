aba1988cb33532db902130d9671117cfb81aa840
antoniof <m1aw@users.noreply.github.com>
2025-08-01 20:19:40 +0200

    fix import

--- packages/server/.netlify/functions/server.js
+++ packages/server/.netlify/functions/server.js
@@ -1,9 +1,10 @@
 const serverless = require('serverless-http');
 const express = require('express');
+const adyenWebServer = require('./index');
 
 const app = express();
 
-const adyenWebExpress  = adyenWebServer(app);
+const adyenWebExpress = adyenWebServer(app);
 
 const handler = serverless(adyenWebExpress);
 module.exports.funcName = async (context, req) => {
