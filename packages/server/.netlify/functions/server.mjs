6e97c568a61ba43bd7fb2945d50169cd5f3b1a63
antoniof <m1aw@users.noreply.github.com>
2025-08-01 19:37:24 +0200

    remove imports

--- packages/server/.netlify/functions/server.mjs
+++ packages/server/.netlify/functions/server.mjs
@@ -1,12 +1,8 @@
 import serverless from 'serverless-http';
 import express from 'express';
 import { createRequire } from 'module';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
 
 // Setup require for CommonJS modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 const require = createRequire(import.meta.url);
 
 // Import the Express app factory from index.js
