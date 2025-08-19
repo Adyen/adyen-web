becaa280eb2c440486a0e069f34cf8c13efd0421
nicholas <nicholas.spong@adyen.com>
2025-08-19 11:40:14 +0200

    Stop Dropin adding an onAdditionalDetails prop in its own handleAction fn

--- packages/lib/src/components/Dropin/Dropin.tsx
+++ packages/lib/src/components/Dropin/Dropin.tsx
@@ -167,7 +167,10 @@ class DropinElement extends UIElement<DropinConfiguration> implements IDropin {
         const paymentAction: UIElement = this.core.createFromAction(action, {
             ...props,
             elementRef: this.elementRef, // maintain elementRef for 3DS2 flow
-            onAdditionalDetails: this.handleAdditionalDetails,
+            // onAdditionalDetails: this.handleAdditionalDetails,
+            // TODO - check how this flow works if we comment out this line
+            //  - do we still need the extra clause in actionTypes? - A: Yes
+            //  - But do all the other scenarios & tests pass with this line commented out? e2e tests pass, manual Dropin/card & storedCard works (with 3DS2)
             isDropin: true
         });
 
