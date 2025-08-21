852ac6c855dd07554ded45ff9a7cf4bbfe4331cb
nicholas <nicholas.spong@adyen.com>
2025-08-21 15:31:53 +0200

    Bump sf version to 6.0.0

--- packages/lib/src/components/internal/SecuredFields/lib/constants.ts
+++ packages/lib/src/components/internal/SecuredFields/lib/constants.ts
@@ -15,7 +15,7 @@ export const ENCRYPTED_SECURITY_CODE_4_DIGITS = 'encryptedSecurityCode4digits';
 
 export const GIFT_CARD = 'giftcard';
 
-export const SF_VERSION = '5.5.3';
+export const SF_VERSION = '6.0.0';
 
 export const DEFAULT_CARD_GROUP_TYPES = ['amex', 'mc', 'visa'];
 
