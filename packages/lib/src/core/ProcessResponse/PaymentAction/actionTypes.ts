ae069138a37518ee99117eba0c13d34ac1a87fc6
nicholas <nicholas.spong@adyen.com>
2025-08-21 13:20:41 +0200

    Removed unused code

--- packages/lib/src/core/ProcessResponse/PaymentAction/actionTypes.ts
+++ packages/lib/src/core/ProcessResponse/PaymentAction/actionTypes.ts
@@ -51,25 +51,13 @@ const actionTypes = {
          */
         const paymentData = action.subtype === 'fingerprint' || props.isMDFlow ? action.paymentData : action.authorisationToken;
 
-        // let mappedOnComplete: any = props.onComplete; // usual way
-        // if (props.isMDFlow) {
-        //     mappedOnComplete = props.originalOptions.onComplete;
-        // }
-        // if (props.isDropin) {
-        //     mappedOnComplete = (state: any) => {
-        //         // If we were in a dropin, we need to pass the dropin ref, not the 3DS2 component ref
-        //         props.onComplete(state, props.elementRef);
-        //     };
-        // }
-
         const config = {
             // Props common to both flows
             core: core,
             token: action.token,
             paymentData,
             onActionHandled: props.onActionHandled,
-            // onComplete: mappedOnComplete,
-            onComplete: props.isMDFlow ? props.originalOptions.onComplete : props.onComplete, // new way, allowing for isMDFlow
+            onComplete: props.isMDFlow ? props.originalOptions.onComplete : props.onComplete,
             onError: props.onError,
             isDropin: !!props.isDropin,
             loadingContext: props.loadingContext,
