b8795e380c444741a3b57e8efc11bdb437dbf6a6
John Ayeni <john.ayeni@adyen.com>
2025-09-22 07:01:13 -0500

    feat(stored-cards): add correct failure state for stored bancontact card

--- packages/e2e-playwright/tests/e2e/dropin/storedCard/stored-card-bancontact.spec.ts
+++ packages/e2e-playwright/tests/e2e/dropin/storedCard/stored-card-bancontact.spec.ts
@@ -17,6 +17,20 @@ test.describe('Stored Bancontact card', () => {
 
         await bcmc.threeDs2Challenge.submit();
 
+        await expect(bcmc.paymentResult).toContainText(PAYMENT_RESULT.success);
+    });
+
+    test('should decline the payment after filling in the wrong 3ds challenge password', async ({ dropinWithSession, page }) => {
+        await dropinWithSession.goto(BCMC_URL);
+        const { paymentMethodDetailsLocator } = await dropinWithSession.selectFirstStoredPaymentMethod('bcmc', getCardNumberLast4(BCMC_CARD));
+
+        const bcmc = new BCMC(page, paymentMethodDetailsLocator);
+
+        await bcmc.pay({ name: /pay €259\.00/i });
+        await bcmc.threeDs2Challenge.fillInPassword('dummy');
+
+        await bcmc.threeDs2Challenge.submit();
+
         await expect(bcmc.paymentResult).toContainText(PAYMENT_RESULT.fail);
     });
 });
