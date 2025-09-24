dd783b9c769f5b0976607e9b242bcf5c5b293618
antoniof <m1aw@users.noreply.github.com>
2025-09-24 14:40:03 +0200

    fix paths on netlify

--- packages/server/api/translations.js
+++ packages/server/api/translations.js
@@ -5,7 +5,11 @@ module.exports = (res, req) => {
     const locale = req.params.locale;
 
     try {
-        const jsonString = fs.readFileSync(path.resolve(__dirname, `../translations/${locale}.json`), { encoding: 'utf-8' });
+        const translationPath = process.env.NETLIFY 
+            ? path.resolve(process.cwd(), `packages/server/translations/${locale}.json`)
+            : path.resolve(__dirname, `../translations/${locale}.json`);
+        
+        const jsonString = fs.readFileSync(translationPath, { encoding: 'utf-8' });
         res.json(JSON.parse(jsonString));
     } catch (error) {
         console.log(`ERROR: Localhost Server /translations endpoint error - ${error.message}`);
