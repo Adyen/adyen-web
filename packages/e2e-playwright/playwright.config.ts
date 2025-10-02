7d980f3f27bbb5c3a5e4d9528f6df675bf8a0160
John Ayeni <john.ayeni@adyen.com>
2025-10-02 04:45:57 -0500

    chore(playwright): retry e2e tests 2 times on CI

--- packages/e2e-playwright/playwright.config.ts
+++ packages/e2e-playwright/playwright.config.ts
@@ -30,7 +30,7 @@ const config: PlaywrightTestConfig = {
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
-    retries: process.env.CI ? 1 : 0,
+    retries: process.env.CI ? 2 : 0,
     /* Opt out of parallel tests on CI. Use default locally */
     workers: process.env.CI ? 4 : undefined,
 
