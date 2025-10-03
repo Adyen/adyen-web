0180dd5b93d19f916d4bd27af035c73db1f6a9bd
John Ayeni <john.ayeni@adyen.com>
2025-10-03 09:21:56 -0500

    chore(pix): update translations and pix styles

--- packages/lib/src/components/Pix/components/PixQRDetails.tsx
+++ packages/lib/src/components/Pix/components/PixQRDetails.tsx
@@ -9,14 +9,19 @@ const PixQRDetails = () => {
     const { qrCodeData, qrCodeImage, percentage, timeToPay, onTick, countdownTime, onQRCodeLoad, onTimeUp, handleCopy } = useQRLoaderDetails();
 
     return (
-        <div className="adyen-checkout__pix__qr_details">
+        <div className="adyen-checkout__qr-loader--pix__qr_details">
             <QRImage src={qrCodeImage} onLoad={onQRCodeLoad} />
-            <div className="adyen-checkout__pix__qr_details__timer">
+            <div className="adyen-checkout__qr-loader--pix__qr_details__timer">
                 <QRProgressbar percentage={percentage} />
                 <QRCountdown countdownTime={countdownTime} timeToPay={timeToPay} onTick={onTick} onCompleted={onTimeUp} />
             </div>
             <Field name="pix-code" useLabelElement={false}>
-                <InputText className="adyen-checkout__pix__qr_details__code" value={qrCodeData} readonly aria-label={i18n.get('pix.code.label')} />
+                <InputText
+                    className="adyen-checkout__qr-loader--pix__qr_details__code"
+                    value={qrCodeData}
+                    readonly
+                    aria-label={i18n.get('pix.code.label')}
+                />
             </Field>
             <QRCodeCopyButton copyLabel={i18n.get('pix.code.copy.label')} copiedLabel={i18n.get('pix.code.copied.label')} handleCopy={handleCopy} />
         </div>
