38c49a762e7cfd8e7e4fd3cf5f72824a4a36ceed
John Ayeni <john.ayeni@adyen.com>
2025-10-20 04:55:41 -0500

    test(qr-loader): fix types for ref

--- packages/lib/src/components/internal/QRLoader/types.ts
+++ packages/lib/src/components/internal/QRLoader/types.ts
@@ -33,7 +33,7 @@ export interface QRLoaderProps {
     copyBtn?: boolean;
     onActionHandled?: (rtnObj: ActionHandledReturnObject) => void;
     onSubmitAnalytics?: (aObj: AnalyticsEvent) => void;
-    ref?: ComponentMethodsRef;
+    ref?: ComponentMethodsRef | ((ref: QRLoaderProps) => void);
     children?: ComponentChildren;
 }
 
