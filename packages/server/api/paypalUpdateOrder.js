7dafab6db850426294ab9b51a7398027984892ef
John Ayeni <john.ayeni@adyen.com>
2025-11-04 10:48:24 -0600

    chore(server): remove unused var

--- packages/server/api/paypalUpdateOrder.js
+++ packages/server/api/paypalUpdateOrder.js
@@ -1,6 +1,5 @@
 const makePostRequest = require('../utils/makePostRequest');
 const handleCallback = require('../utils/handleCallback');
-const { MERCHANT_ACCOUNT: merchantAccount } = require('../utils/config');
 
 module.exports = async (res, request) => {
     const response = await makePostRequest('paypal/updateOrder', { ...request });
