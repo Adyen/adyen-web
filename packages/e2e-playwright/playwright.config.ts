e6c43afc2b1676e7bfa5f4a29158743f09982df5
nicholas <nicholas.spong@adyen.com>
2025-11-05 14:55:03 +0100

    Changing config to allow .only and to stop retries

--- packages/e2e-playwright/playwright.config.ts
+++ packages/e2e-playwright/playwright.config.ts
@@ -28,9 +28,9 @@ const config: PlaywrightTestConfig = {
     /* Run tests in files in parallel */
     fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
-    forbidOnly: !!process.env.CI,
+    // forbidOnly: !!process.env.CI,
     /* Retry on CI only */
-    retries: process.env.CI ? 2 : 0,
+    retries: process.env.CI ? 0 : 0,
     /* Opt out of parallel tests on CI. Use default locally */
     workers: process.env.CI ? 4 : undefined,
 
