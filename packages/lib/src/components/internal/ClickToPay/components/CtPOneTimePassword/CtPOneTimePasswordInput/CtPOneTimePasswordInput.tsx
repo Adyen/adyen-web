bd541463567b5d0f543d17adfe1644a4b26a046d
antoniof <m1aw@users.noreply.github.com>
2025-11-14 17:53:43 +0100

    fixes missing error messages for upi and ctp

--- packages/lib/src/components/internal/ClickToPay/components/CtPOneTimePassword/CtPOneTimePasswordInput/CtPOneTimePasswordInput.tsx
+++ packages/lib/src/components/internal/ClickToPay/components/CtPOneTimePassword/CtPOneTimePasswordInput/CtPOneTimePasswordInput.tsx
@@ -8,6 +8,7 @@ import useForm from '../../../../../../utils/useForm';
 import Field from '../../../../FormFields/Field';
 import './CtPOneTimePasswordInput.scss';
 import InputText from '../../../../FormFields/InputText';
+import { getErrorMessage } from '../../../../../../utils/getErrorMessage';
 
 type OnChangeProps = { data: CtPOneTimePasswordInputDataState; valid; errors; isValid: boolean };
 
@@ -99,12 +100,19 @@ const CtPOneTimePasswordInput = (props: CtPOneTimePasswordInputProps): h.JSX.Ele
         props.onChange({ data, valid, errors, isValid });
     }, [data, valid, errors]);
 
+
+    const errorMessageLogic = () => {
+        if(!isOtpFielDirty) return null;
+        return resendOtpError || props.errorMessage || getErrorMessage(i18n, errors.otp, i18n.get('ctp.otp.fieldLabel'));
+    }
+    const errorMessage = errorMessageLogic(); 
+
     return (
         <div className={'adyen-checkout-ctp__otp-field-wrapper'}>
             <Field
                 name="oneTimePassword"
                 label={i18n.get('ctp.otp.fieldLabel')}
-                errorMessage={isOtpFielDirty ? resendOtpError || props.errorMessage || !!errors.otp : null}
+                errorMessage={errorMessage}
                 classNameModifiers={['otp']}
             >
                 <InputText
