d02a98f35f353e2b08560ab07f090ea7702c0528
John Ayeni <john.ayeni@adyen.com>
2025-11-18 09:44:23 -0600

    chore: fix issues with package dependency updates

--- packages/lib/src/utils/amount-util.test.ts
+++ packages/lib/src/utils/amount-util.test.ts
@@ -1,25 +1,4 @@
-import { isValidCurrencyCode, getCurrencyCode, getLocalisedAmount, getLocalisedPercentage, getDivider } from './amount-util';
-
-describe('isValidCurrencyCode', () => {
-    test('should return false for empty values', () => {
-        expect(isValidCurrencyCode('')).toBe(false);
-        expect(isValidCurrencyCode(null)).toBe(false);
-        expect(isValidCurrencyCode('0')).toBe(false);
-    });
-
-    // In the past this currency code was an exception, test that everything still works
-    test('should return true for BYN', () => {
-        expect(isValidCurrencyCode('BYN')).toBe(true);
-    });
-
-    test('should return true for existing keys', () => {
-        expect(isValidCurrencyCode('USD')).toBe(true);
-    });
-
-    test('should return false for non existing keys', () => {
-        expect(isValidCurrencyCode('FAKE')).toBe(false);
-    });
-});
+import { getLocalisedAmount, getDivider } from './amount-util';
 
 describe('getDivider', () => {
     test('should return the divider for a currency', () => {
@@ -27,26 +6,6 @@ describe('getDivider', () => {
         expect(getDivider('JPY')).toBe(1);
         expect(getDivider('BHD')).toBe(1000);
         expect(getDivider('MRO')).toBe(10);
-    });
-});
-
-describe('getCurrencyCode', () => {
-    test('should return false for empty values', () => {
-        expect(getCurrencyCode('')).toBe(false);
-        expect(getCurrencyCode(null)).toBe(false);
-        expect(getCurrencyCode('0')).toBe(false);
-    });
-
-    test('should return a currency symbol for existing keys', () => {
-        expect(getCurrencyCode('USD')).toBe('$');
-        expect(getCurrencyCode('BYN')).toBe('Br');
-    });
-
-    test('should return a false for non existing keys', () => {
-        expect(getCurrencyCode('FAKE')).toBe(false);
-        expect(getCurrencyCode('123')).toBe(false);
-        expect(getCurrencyCode(undefined)).toBe(false);
-        expect(getCurrencyCode('true')).toBe(false);
     });
 });
 
@@ -77,16 +36,5 @@ describe('getLocalisedAmount', () => {
 
     test('should return an amount if no locale is passed', () => {
         expect(getLocalisedAmount(1000, 'undefined', 'undefined')).toBe('1000');
-    });
-});
-
-describe('getLocalisedPercentage', () => {
-    test('should return a percentage formatted string', () => {
-        expect(getLocalisedPercentage(1000, 'en-US')).toBe('10%');
-        expect(getLocalisedPercentage(100, 'en-US')).toBe('1%');
-        expect(getLocalisedPercentage(10, 'en-US')).toBe('0.1%');
-        expect(getLocalisedPercentage(1, 'en-US')).toBe('0.01%');
-        expect(getLocalisedPercentage(1234, 'en-US')).toBe('12.34%');
-        expect(getLocalisedPercentage(-2500, 'en-US')).toBe('-25%');
     });
 });
