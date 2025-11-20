87f034228a6ff9d9df5e0ab8f9066617bd1d5753
Yu Long <yu.long@adyen.com>
2025-11-20 13:11:27 +0100

    fix(Img): optionally call `onError` callback

--- packages/lib/src/components/internal/Img/Img.test.tsx
+++ packages/lib/src/components/internal/Img/Img.test.tsx
@@ -1,18 +1,30 @@
-import { shallow } from 'enzyme';
 import { h } from 'preact';
+import { fireEvent, render, screen } from '@testing-library/preact';
 import Img from './Img';
+import { ImgProps } from './types';
 
 describe('Image', () => {
-    const getWrapper = (props = {}) => shallow(<Img {...props} />);
+    const renderImg = (props: ImgProps) => render(<Img {...props} />);
 
     test('renders a component', () => {
-        const wrapper = getWrapper();
-        expect(wrapper.is('img')).toBe(true);
-        expect(wrapper.hasClass('adyen-checkout__image')).toBe(true);
+        renderImg({ alt: 'test image' });
+        const image = screen.getByRole('img', { name: 'test image' });
+        expect(image).toBeInTheDocument();
+        expect(image).toHaveClass('adyen-checkout__image');
     });
 
     test('has passed className', () => {
-        const wrapper = getWrapper({ className: 'abc123' });
-        expect(wrapper.find('img').hasClass('abc123')).toBe(true);
+        renderImg({ alt: 'test image', className: 'abc123' });
+        expect(screen.getByRole('img')).toHaveClass('abc123');
+    });
+
+    test('calls onError callback', () => {
+        const onErrorMock = jest.fn();
+        renderImg({ alt: 'test image', src: 'invalid.jpg', onError: onErrorMock });
+
+        const image = screen.getByRole('img');
+        fireEvent.error(image);
+
+        expect(onErrorMock).toHaveBeenCalledTimes(1);
     });
 });
