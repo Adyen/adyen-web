8c709a63484fbd1709fe9070778f2e15980fe80d
John Ayeni <john.ayeni@adyen.com>
2025-12-24 11:50:25 -0600

    chore: add config and tests for screenshots on playwright

--- packages/lib/src/components/Swish/Swish.stories.tsx
+++ packages/lib/src/components/Swish/Swish.stories.tsx
@@ -1,4 +1,5 @@
 import { h } from 'preact';
+import { http, HttpResponse } from 'msw';
 import { MetaConfiguration, PaymentMethodStoryProps, StoryConfiguration } from '../../../storybook/types';
 import { QRLoaderConfiguration } from '../../types';
 import { ComponentContainer } from '../../../storybook/components/ComponentContainer';
@@ -11,6 +12,16 @@ const meta: MetaConfiguration<QRLoaderConfiguration> = {
     title: 'Components/Swish'
 };
 
+const createSwishMockHandlers = () => [
+    http.post('https://checkoutshopper-test.adyen.com/checkoutshopper/services/PaymentInitiation/v1/status', () => {
+        return HttpResponse.json({
+            payload: '',
+            resultCode: 'pending',
+            type: 'pending'
+        });
+    })
+];
+
 const render = ({ componentConfiguration, ...checkoutConfig }: PaymentMethodStoryProps<QRLoaderConfiguration>) => (
     <Checkout checkoutConfig={checkoutConfig}>{checkout => <ComponentContainer element={new Swish(checkout, componentConfiguration)} />}</Checkout>
 );
@@ -20,6 +31,32 @@ export const Default: SwishStory = {
     args: {
         countryCode: 'SE'
     }
+};
+
+export const QRCodeScreen: SwishStory = {
+    args: {
+        countryCode: 'SE'
+    },
+    parameters: {
+        msw: {
+            handlers: createSwishMockHandlers()
+        }
+    },
+    render: ({ componentConfiguration, ...checkoutConfig }) => (
+        <Checkout checkoutConfig={checkoutConfig}>
+            {checkout => (
+                <ComponentContainer
+                    element={
+                        new Swish(checkout, {
+                            paymentData: 'Ab02b4c0....J86s=',
+                            qrCodeData: 'testqrcode',
+                            ...componentConfiguration
+                        })
+                    }
+                />
+            )}
+        </Checkout>
+    )
 };
 
 export default meta;
