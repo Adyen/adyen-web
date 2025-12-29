b51b2f710a62275d33ba1887e06a29a796fc983b
ribeiroguilherme <guilhermemrr@gmail.com>
2025-12-29 11:22:26 -0300

    PayButton - Fixed tests and adjusted hook usage in the component

--- packages/lib/src/core/Context/AmountProvider.tsx
+++ packages/lib/src/core/Context/AmountProvider.tsx
@@ -2,9 +2,15 @@ import { h, createContext, Fragment } from 'preact';
 import { useContext, useEffect, useImperativeHandle, useMemo, useState } from 'preact/hooks';
 import type { ComponentChildren, RefObject } from 'preact';
 import type { PaymentAmount } from '../../types';
+import { isAmountValid } from '../../utils/amount-util';
 
-interface AmountProviderProps {
-    amount: PaymentAmount;
+interface AmountContextValue {
+    amount?: PaymentAmount;
+    secondaryAmount?: PaymentAmount;
+}
+
+export interface AmountProviderProps {
+    amount?: PaymentAmount;
     secondaryAmount?: PaymentAmount;
     providerRef: RefObject<AmountProviderRef>;
     children: ComponentChildren;
@@ -14,35 +20,34 @@ export interface AmountProviderRef {
     update(newAmount: PaymentAmount, newSecondaryAmount?: PaymentAmount): void;
 }
 
-const AmountContext = createContext<
-    | {
-          amount: PaymentAmount;
-          secondaryAmount?: PaymentAmount;
-      }
-    | undefined
->(undefined);
+const AmountContext = createContext<AmountContextValue | undefined>(undefined);
 
-const useAmount = (): PaymentAmount => {
+const useAmount = (): { amount: PaymentAmount | undefined; isZeroAuth: boolean; isAmountValid: boolean } => {
     const context = useContext(AmountContext);
     if (!context) {
         throw new Error('useAmount must be used within an AmountProvider');
     }
-    return context.amount;
+
+    return {
+        amount: context.amount,
+        isZeroAuth: context.amount?.value === 0,
+        isAmountValid: isAmountValid(context.amount)
+    };
 };
 
-const useSecondaryAmount = (): PaymentAmount => {
+const useSecondaryAmount = (): { secondaryAmount: PaymentAmount | undefined } => {
     const context = useContext(AmountContext);
     if (!context) {
         throw new Error('useAmount must be used within an AmountProvider');
     }
-    return context.secondaryAmount;
+    return { secondaryAmount: context.secondaryAmount };
 };
 
 const AmountProvider = ({ amount, secondaryAmount, providerRef, children }: AmountProviderProps) => {
     const context = useContext(AmountContext);
 
     const [updatedAmount, setUpdatedAmount] = useState<PaymentAmount>(amount);
-    const [updatedSecondaryAmount, setUpdatedSecondaryAmount] = useState<PaymentAmount>(secondaryAmount);
+    const [updatedSecondaryAmount, setUpdatedSecondaryAmount] = useState<PaymentAmount | undefined>(secondaryAmount);
 
     useEffect(() => {
         setUpdatedAmount(amount);
