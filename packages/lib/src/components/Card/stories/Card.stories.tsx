dd266948f3a37ddc6f09d6b1363aee805ba6f334
ribeiroguilherme <guilhermemrr@gmail.com>
2025-12-30 11:29:16 -0300

    ClickToPay - Removed amount from CTP Provider in order to use from the new hook

--- packages/lib/src/components/Card/stories/Card.stories.tsx
+++ packages/lib/src/components/Card/stories/Card.stories.tsx
@@ -173,6 +173,12 @@ export const WithClickToPay: CardStory = {
     render: createCardComponent,
     args: {
         componentConfiguration: {
+            configuration: {
+                visaSrciDpaId: '8e6e347c-254e-863f-0e6a-196bf2d9df02',
+                visaSrcInitiatorId: 'B9SECVKIQX2SOBQ6J9X721dVBBKHhJJl1nxxVbemHGn5oB6S8',
+                mcDpaId: '6d41d4d6-45b1-42c3-a5d0-a28c0e69d4b1_dpa2',
+                mcSrcClientId: '6d41d4d6-45b1-42c3-a5d0-a28c0e69d4b1'
+            },
             clickToPayConfiguration: {
                 shopperEmail: 'gui.ctp@adyen.com',
                 merchantDisplayName: 'Adyen Merchant Name'
