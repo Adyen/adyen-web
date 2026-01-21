49bdc29cc65dca3bf5e2ca02e5a3d5e18b7cc712
antoniof <m1aw@users.noreply.github.com>
2026-01-21 17:38:11 +0100

    refactor: move formater address logic to validator

--- packages/lib/src/components/internal/Address/validate.formats.ts
+++ packages/lib/src/components/internal/Address/validate.formats.ts
@@ -1,7 +1,7 @@
 import { FormatterContext } from '../../../utils/Formatters/types';
 import { CountryFormatRules, FormatRules } from '../../../utils/Validator/types';
 import { Formatter } from '../../../utils/useForm/types';
-import { getFormattingRegEx, INVALID_CHARS_REGEX } from '../../../utils/validator-utils';
+import { getFormattingRegEx, stripInvalidChars } from '../../../utils/validator-utils';
 
 const asCountryFormatRules = <T extends CountryFormatRules>(rules: T): T & CountryFormatRules => rules;
 
@@ -15,7 +15,7 @@ const createFormatByDigits = (digits: number): Formatter => {
     };
 };
 
-const formattingFn = (val: string) => val.replace(INVALID_CHARS_REGEX, '');
+const defaultFormattingFn = (val: string) => val;
 
 export const addressFormatters: FormatRules = {
     postalCode: {
@@ -33,13 +33,13 @@ export const addressFormatters: FormatRules = {
         }
     },
     street: {
-        formatterFn: formattingFn
+        formatterFn: defaultFormattingFn
     },
     houseNumberOrName: {
-        formatterFn: formattingFn
+        formatterFn: defaultFormattingFn
     },
     city: {
-        formatterFn: formattingFn
+        formatterFn: defaultFormattingFn
     }
 };
 
@@ -112,8 +112,7 @@ export const countrySpecificFormatters = asCountryFormatRules({
     },
     GB: {
         postalCode: {
-            // Disallow invalid chars & set to maxlength
-            formatterFn: val => val.replace(INVALID_CHARS_REGEX, '').substring(0, 8),
+            formatterFn: val => stripInvalidChars(val).substring(0, 8),
             format: 'AA99 9AA or A99 9AA or A9 9AA',
             maxlength: 8
         }
