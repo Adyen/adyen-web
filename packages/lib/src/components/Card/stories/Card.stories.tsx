9c083dd5068b9a5f91843bd9390a63d918094508
ribeiroguilherme <guilhermemrr@gmail.com>
2026-02-06 11:43:45 -0300

    excluding ctp from visual tests

--- packages/lib/src/components/Card/stories/Card.stories.tsx
+++ packages/lib/src/components/Card/stories/Card.stories.tsx
@@ -171,6 +171,7 @@ export const WithMockedFastlane: CardStory = {
 
 export const WithClickToPay: CardStory = {
     render: createCardComponent,
+    tags: ['no-automated-visual-test'],
     args: {
         componentConfiguration: {
             configuration: {
