1f9ea3dfaeece97be952d722da10f7f3dfdc2798
ribeiroguilherme <guilhermemrr@gmail.com>
2026-03-06 09:22:04 -0600

    cleaned up dual brand test

--- packages/lib/src/components/Card/components/CardInput/CardInput.dualBrandUI.test.tsx
+++ packages/lib/src/components/Card/components/CardInput/CardInput.dualBrandUI.test.tsx
@@ -60,55 +60,25 @@ describe('CardNumber and the dual branding UI', () => {
         const { container } = renderCardInput(<CardInput {...cardInputRequiredProps} />);
 
         /* eslint-disable testing-library/no-node-access, testing-library/no-container */
-        // Expected card fields
         expect(container.querySelector('[data-cse="encryptedCardNumber"]')).toBeTruthy();
         expect(container.querySelector('[data-cse="encryptedExpiryDate"]')).toBeTruthy();
         expect(container.querySelector('[data-cse="encryptedSecurityCode"]')).toBeTruthy();
-
-        // No dual branding UI
-        expect(container.querySelector('.adyen-checkout__fieldset--dual-brand-switcher')).toBeNull();
         /* eslint-enable testing-library/no-node-access, testing-library/no-container */
+
+        expect(screen.queryByText('Card Brand')).not.toBeInTheDocument();
     });
 
     test('Renders a CardInput with dual branding UI radio button elements', () => {
-        const { container } = renderCardInput(<CardInput {...cardInputRequiredProps} />);
+        renderCardInput(<CardInput {...cardInputRequiredProps} />);
 
         void act(() => {
             cardInputRef.processBinLookupResponse(dualBrandResp, false);
         });
 
-        /* eslint-disable testing-library/no-node-access, testing-library/no-container */
-        //  Dual branding UI visible
-        const dualBrandEl = container.querySelector('.adyen-checkout__fieldset--dual-brand-switcher');
-        expect(dualBrandEl).toBeTruthy();
+        expect(screen.getByText('Card Brand')).toBeInTheDocument();
 
-        // title
-        expect(dualBrandEl.querySelector('.adyen-checkout__fieldset__title')).toBeTruthy();
-
-        // subTitle
-        expect(dualBrandEl.querySelector('legend')).toBeTruthy();
-        /* eslint-enable testing-library/no-node-access, testing-library/no-container */
-
-        // radio group
-        expect(screen.getByRole('radiogroup')).toBeTruthy();
-
-        // buttons
-        expect(screen.getAllByRole('radio')).toHaveLength(2);
-
-        // check images
-        const radioLabels = screen.getAllByRole('radio');
-
-        /* eslint-disable testing-library/no-node-access */
-        const firstLabel = radioLabels[0].closest('.adyen-checkout__radio_group__input-wrapper');
-        const secondLabel = radioLabels[1].closest('.adyen-checkout__radio_group__input-wrapper');
-
-        expect(firstLabel.querySelector('img').getAttribute('alt')).toEqual('VISA');
-        expect(secondLabel.querySelector('img').getAttribute('alt')).toEqual('cartebancaire');
-
-        // check texts
-        expect(firstLabel.textContent).toContain('VISA');
-        expect(secondLabel.textContent).toContain('Carte Bancaire');
-        /* eslint-enable testing-library/no-node-access */
+        expect(screen.getByRole('radio', { name: /VISA/i })).toBeVisible();
+        expect(screen.getByRole('radio', { name: /Carte Bancaire/i })).toBeVisible();
     });
 
     test('Dual branding UI is not hidden when the card number is in error', () => {
@@ -118,10 +88,9 @@ describe('CardNumber and the dual branding UI', () => {
             cardInputRef.processBinLookupResponse(dualBrandResp, false);
         });
 
-        /* eslint-disable testing-library/no-node-access, testing-library/no-container */
-        //  Dual branding UI visible
-        expect(container.querySelector('.adyen-checkout__fieldset--dual-brand-switcher')).toBeTruthy();
+        expect(screen.getByText('Card Brand')).toBeInTheDocument();
 
+        /* eslint-disable testing-library/no-node-access, testing-library/no-container */
         // 3 error fields - all hidden
         expect(container.querySelectorAll('.adyen-checkout-contextual-text--error.adyen-checkout-contextual-text--hidden')).toHaveLength(3);
 
@@ -131,9 +100,8 @@ describe('CardNumber and the dual branding UI', () => {
 
         // 3 error fields all visible
         expect(container.querySelectorAll('.adyen-checkout-contextual-text--error.adyen-checkout-contextual-text--hidden')).toHaveLength(0);
-
-        //  Dual branding UI still visible
-        expect(container.querySelector('.adyen-checkout__fieldset--dual-brand-switcher')).toBeTruthy();
         /* eslint-enable testing-library/no-node-access, testing-library/no-container */
+
+        expect(screen.getByText('Card Brand')).toBeInTheDocument();
     });
 });
