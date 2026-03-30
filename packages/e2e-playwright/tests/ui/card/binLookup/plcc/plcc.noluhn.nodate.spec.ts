b92e65a740a7af407f00954a526b4512f8189534
nicholas <nicholas.spong@adyen.com>
2026-03-30 12:46:55 +0200

    Fixing e2e failing due to binLookup change

--- packages/e2e-playwright/tests/ui/card/binLookup/plcc/plcc.noluhn.nodate.spec.ts
+++ packages/e2e-playwright/tests/ui/card/binLookup/plcc/plcc.noluhn.nodate.spec.ts
@@ -1,11 +1,7 @@
 import { test, expect } from '../../../../../fixtures/card.fixture';
 import { getStoryUrl } from '../../../../utils/getStoryUrl';
-import { PLCC_NO_LUHN_OPTIONAL_DATE_WOULD_FAIL_LUHN, TEST_CVC_VALUE } from '../../../../utils/constants';
+import { PLCC_NO_LUHN_HIDDEN_DATE_WOULD_FAIL_LUHN, TEST_CVC_VALUE } from '../../../../utils/constants';
 import { URL_MAP } from '../../../../../fixtures/URL_MAP';
-import LANG from '../../../../../../server/translations/en-US.json';
-
-const DATE_LABEL = LANG['creditCard.expiryDate.label'];
-const DATE_LABEL_OPTIONAL = `${LANG['creditCard.expiryDate.label']} ${LANG['field.title.optional']}`;
 
 test.describe('Testing binLookup endpoint for a response that should indicate a luhn check is not required)', () => {
     test('Test a PLCC card, that does not require a date, becomes valid with a number that fails the luhn check', async ({ card, page }) => {
@@ -17,17 +13,14 @@ test.describe('Testing binLookup endpoint for a response that should indicate a 
         await card.isComponentVisible();
 
         // Number that identifies as plcc, with no luhn required, but that also fails luhn
-        await card.typeCardNumber(PLCC_NO_LUHN_OPTIONAL_DATE_WOULD_FAIL_LUHN);
+        await card.typeCardNumber(PLCC_NO_LUHN_HIDDEN_DATE_WOULD_FAIL_LUHN);
 
         // Confirm plcc brand
         let brandingIconSrc = await card.brandingIcon.getAttribute('src');
         expect(brandingIconSrc).toContain('synchrony_plcc.svg');
 
-        // Confirm date is optional
-        // ...with "optional" text
-        await expect(card.expiryDateLabelText).toHaveText(DATE_LABEL_OPTIONAL);
-        // ...and optional class
-        await expect(card.expiryDateField).toHaveClass(/adyen-checkout__field__exp-date--optional/);
+        // Confirm date is hidden
+        await expect(card.expiryDateField).not.toBeVisible();
 
         // Fill cvc
         await card.typeCvc(TEST_CVC_VALUE);
@@ -47,7 +40,8 @@ test.describe('Testing binLookup endpoint for a response that should indicate a 
         expect(brandingIconSrc).toContain('nocard.svg');
 
         // Confirm date is required again
-        await expect(card.expiryDateLabelText).toHaveText(DATE_LABEL);
+        // await expect(card.expiryDateLabelText).toHaveText(DATE_LABEL);
+        await expect(card.expiryDateField).toBeVisible();
 
         // PM is not valid
         cardValid = await page.evaluate('window.component.isValid');
