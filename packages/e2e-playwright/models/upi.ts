a8e1aaee713ffc09fea997cfc2bd8dd87e063eae
vagrant <guilherme.scotti@adyen.com>
2026-03-31 08:55:55 -0500

    fix screenshot tests

--- packages/e2e-playwright/models/upi.ts
+++ packages/e2e-playwright/models/upi.ts
@@ -8,11 +8,13 @@ class UPI extends Base {
     readonly mandateInfo: Locator;
     readonly appDropdown: Locator;
     readonly errorAlert: Locator;
+    readonly qrCodeIntent: Locator;
 
     constructor(public readonly page: Page) {
         super(page);
 
         this.appList = this.page.getByRole('radiogroup');
+        this.qrCodeIntent = this.page.locator('#upi-area-qr-code');
         this.intentArea = this.page.locator('#upi-area-intent');
         this.qrCodeImage = this.page.getByAltText('Scan QR code');
         this.mandateInfo = this.page.locator('.adyen-checkout__alert-message--info');
