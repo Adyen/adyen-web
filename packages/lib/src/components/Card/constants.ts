866296637424f8a33ebc80b2d21e51e24b386113
nicholas <nicholas.spong@adyen.com>
2026-04-07 13:42:30 +0200

    Add mechanism to force brand to local brand, if we are working from our regEx "best guess"

--- packages/lib/src/components/Card/constants.ts
+++ packages/lib/src/components/Card/constants.ts
@@ -1,1 +1,9 @@
 export const DUAL_BRANDS_THAT_NEED_SELECTION_MECHANISM = ['cartebancaire', 'bcmc', 'dankort'] as const;
+
+export const COUNTRIES_THAT_REQUIRE_SELECTION_MECHANISM = ['fr', 'be', 'dk'] as const;
+
+export const DUAL_BRAND_MAP = {
+    fr: 'cartebancaire',
+    be: 'bcmc',
+    dk: 'dankort'
+};
