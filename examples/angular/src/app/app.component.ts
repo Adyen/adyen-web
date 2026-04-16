f60e318489a09d1b6c0121296304706ac813750b
John Ayeni <john.ayeni@adyen.com>
2026-04-16 09:12:32 -0500

    chore: remove examples folder

--- examples/angular/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-@Component({
-    selector: 'app-root',
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html'
-})
-export class AppComponent {}
