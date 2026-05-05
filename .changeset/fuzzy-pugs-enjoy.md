0ed81c960994895c63879aefcc30792d03283dd2
vagrant <guilherme.scotti@adyen.com>
2026-05-05 09:16:36 -0500

    fix(PayByBankUS): remove dropin brand-icon CSS overrides breaking 320px reflow

---
+++ .changeset/fuzzy-pugs-enjoy.md
@@ -0,0 +1,5 @@
+---
+'@adyen/adyen-web': patch
+---
+
+Fixed: remove payByBank css override
---
+++ packages/e2e-playwright/tests/e2e/dropin/sessions/paybybankUS.spec.ts
@@ -0,0 +1,28 @@
+import dotenv from 'dotenv';
+import { test } from '../../../../fixtures/dropin.fixture';
+import { MOBILE_USER_AGENT, SMALL_A11Y_MOBILE_VIEWPORT, TAGS } from '../../../utils/constants';
+import { URL_MAP } from '../../../../fixtures/URL_MAP';
+import { toHaveScreenshot } from '../../../utils/assertions';
+
+dotenv.config();
+
+test.describe('Dropin - Sessions - PayByBankUS', () => {
+    test.describe('Brand list (Small Mobile)', () => {
+        test.use({
+            viewport: SMALL_A11Y_MOBILE_VIEWPORT,
+            userAgent: MOBILE_USER_AGENT
+        });
+
+        test(
+            'should render all brand icons in the dropin list at 320px width',
+            { tag: [TAGS.SCREENSHOT] },
+            async ({ dropinWithSession, browserName }) => {
+                await dropinWithSession.goto(URL_MAP.dropinWithSession);
+
+                const payByBankHeader = dropinWithSession.getPaymentMethodHeader('Pay by bank');
+
+                await toHaveScreenshot(payByBankHeader.rootElement, browserName, 'paybybankus-payment-method-header-mobile-320.png');
+            }
+        );
+    });
+});
