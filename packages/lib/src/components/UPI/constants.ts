4d6837be9cd62b0734a70cd66af2fe5236cffb9d
guilhermesc <guilherme.scotti@adyen.com>
2026-05-11 14:42:44 +0200

    remove comment

--- packages/lib/src/components/UPI/constants.ts
+++ packages/lib/src/components/UPI/constants.ts
@@ -4,7 +4,7 @@ export const UPI_MODE = {
     QR_CODE: 'qrCode' as UpiMode,
     INTENT: 'intent' as UpiMode
 } as const;
-// temporary limit of primary apps to show, rollback to 4 when ready
+
 export const MAX_PRIMARY_APPS = 4;
 
 export const A11Y = {
