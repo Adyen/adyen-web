d30f765a0e9faeb6cfdc6f25a804c0e17ee0b89f
guilhermesc <guilherme.scotti@adyen.com>
2026-05-20 14:43:15 +0200

    Create an E2E test for 3DS flow

--- packages/e2e-playwright/models/emi.ts
+++ packages/e2e-playwright/models/emi.ts
@@ -1,17 +1,42 @@
 import { Locator, Page } from '@playwright/test';
 import { Base } from './base';
+import { Card } from './card';
+import { ThreeDs2Challenge } from './threeds2Challenge';
 
 class EMI extends Base {
-    readonly cardNumberField: Locator;
-    readonly holderNameField: Locator;
+    readonly card: Card;
     readonly errorFields: Locator;
+    readonly threeDs2Challenge: ThreeDs2Challenge;
 
     constructor(public readonly page: Page) {
         super(page);
-        this.cardNumberField = this.page.locator('.adyen-checkout__card__cardNumber__input, [data-cse="encryptedCardNumber"]').first();
-        this.holderNameField = this.page.locator('.adyen-checkout__card__holderName__input').first();
+        this.card = new Card(page);
         this.errorFields = this.page.locator('.adyen-checkout__field--error');
+        this.threeDs2Challenge = this.card.threeDs2Challenge;
+    }
 
+    get cardNumberField(): Locator {
+        return this.card.cardNumberField;
+    }
+
+    get holderNameField(): Locator {
+        return this.card.holderNameField;
+    }
+
+    async isComponentVisible() {
+        await this.card.isComponentVisible();
+    }
+
+    async typeCardNumber(cardNumber: string) {
+        await this.card.typeCardNumber(cardNumber);
+    }
+
+    async typeExpiryDate(expiryDate: string) {
+        await this.card.typeExpiryDate(expiryDate);
+    }
+
+    async typeCvc(cvc: string) {
+        await this.card.typeCvc(cvc);
     }
 }
 
