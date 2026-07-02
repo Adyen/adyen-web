319ae9bd112a630b12d38f1cc7f9118a124b0b8c
nicholas <nicholas.spong@adyen.com>
2026-07-02 14:44:18 +0200

    Fixed type for first argument in on3DS2RedirectFlowComplete function

--- packages/lib/src/components/ThreeDS2/types.ts
+++ packages/lib/src/components/ThreeDS2/types.ts
@@ -166,7 +166,7 @@ export type ThreeDS2ConfigProps = {
     readonly token: string;
     readonly paymentData: string;
     readonly onActionHandled?: (rtnObj: ActionHandledReturnObject) => void;
-    readonly on3DS2RedirectFlowComplete?: (state: ChallengeResolveData, component: UIElement) => void;
+    readonly on3DS2RedirectFlowComplete?: (state: ChallengeResolveData | FingerprintResolveData, component: UIElement) => void;
     readonly onAdditionalDetails?: (state: AdditionalDetailsData, component: UIElement, actions: AdditionalDetailsActions) => void;
     readonly onError?: (error: AdyenCheckoutError, element?: UIElement) => void;
     readonly isDropin?: boolean;
