d53db4ecceae39203279360ecf57d3b183d953aa
guilhermesc <guilherme.scotti@adyen.com>
2026-07-08 13:57:04 +0200

    EMI E2E tests will run on Advanced flow

--- packages/e2e-playwright/tests/e2e/dropin/advanced/emi.spec.ts
+++ packages/e2e-playwright/tests/e2e/dropin/advanced/emi.spec.ts
@@ -3,13 +3,13 @@ import { URL_MAP } from '../../../../fixtures/URL_MAP';
 import { EMI } from '../../../../models/emi';
 
 test.describe('Dropin - Sessions - EMI', () => {
-    test('should display EMI tile with subtitle and embedded card form', async ({ dropinWithSession, page }) => {
-        await dropinWithSession.goto(URL_MAP.dropinSessionsIndia);
+    test('should display EMI tile with subtitle and embedded card form', async ({ dropin, page }) => {
+        await dropin.goto(URL_MAP.dropinWithAdvancedIndia);
 
-        const emiPaymentMethodHeader = dropinWithSession.getPaymentMethodHeader('EMI');
+        const emiPaymentMethodHeader = dropin.getPaymentMethodHeader('EMI');
         await expect(emiPaymentMethodHeader.rootElement).toBeVisible();
 
-        await dropinWithSession.selectNonStoredPaymentMethod('emi');
+        await dropin.selectNonStoredPaymentMethod('emi');
 
         const emi = new EMI(page);
 
