2dea31c9a0da931ebebf75d886012e9494436cdf
johna <john.ayeni@adyen.com>
2026-09-18 14:32:40 +0200

    chore: update paypal story and fix redirect result story

--- packages/lib/src/components/PayPal/stories/PayPalV6.stories.tsx
+++ packages/lib/src/components/PayPal/stories/PayPalV6.stories.tsx
@@ -24,7 +24,7 @@ export const Default: Story = {
         componentConfiguration: {
             usePayPalV6: {
                 presentationModeOptions: {
-                    presentationMode: 'redirect'
+                    presentationMode: 'auto'
                 },
                 style: {
                     paypal: {
